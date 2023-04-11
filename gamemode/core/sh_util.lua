6b20f7147c041ba1c30bd25a4ea932d191018960
Leonheart <33399712+bleonheart@users.noreply.github.com>
2023-04-11 16:17:45 +0100

    Added Compatibility to Util

--- gamemode/core/sh_util.lua
+++ gamemode/core/sh_util.lua
@@ -1,260 +1,300 @@
 -- This file includes utility functions that are pretty isolated.
-
 -- Includes a file from the prefix.
+function ix.util.Include(fileName, state)
+    lia.util.include(fileName, state)
+end
+
 function lia.util.include(fileName, state)
-	if (!fileName) then
-		error("[Lilia] No file name specified for including.")
-	end
+    if not fileName then
+        error("[Lilia] No file name specified for including.")
+    end
 
-	-- Only include server-side if we're on the server.
-	if ((state == "server" or fileName:find("sv_")) and SERVER) then
-		return include(fileName)
-	-- Shared is included by both server and client.
-	elseif (state == "shared" or fileName:find("sh_")) then
-		if (SERVER) then
-			-- Send the file to the client if shared so they can run it.
-			AddCSLuaFile(fileName)
-		end
+    -- Only include server-side if we're on the server.
+    if (state == "server" or fileName:find("sv_")) and SERVER then
+        return include(fileName)
+    elseif state == "shared" or fileName:find("sh_") then
+        -- Shared is included by both server and client.
+        if SERVER then
+            -- Send the file to the client if shared so they can run it.
+            AddCSLuaFile(fileName)
+        end
 
-		return include(fileName)
-	-- File is sent to client, included on client.
-	elseif (state == "client" or fileName:find("cl_")) then
-		if (SERVER) then
-			AddCSLuaFile(fileName)
-		else
-			return include(fileName)
-		end
-	end
+        return include(fileName)
+    elseif state == "client" or fileName:find("cl_") then
+        -- File is sent to client, included on client.
+        if SERVER then
+            AddCSLuaFile(fileName)
+        else
+            return include(fileName)
+        end
+    end
+end
+
+function ix.util.IncludeDir(directory, fromLua, recursive)
+    lia.util.includeDir(directory, fromLua, recursive)
 end
 
 -- Include files based off the prefix within a directory.
 function lia.util.includeDir(directory, fromLua, recursive)
-	-- By default, we include relatively to Lilia.
-	local baseDir = "lilia"
+    -- By default, we include relatively to Lilia.
+    local baseDir = "lilia"
 
-	-- If we're in a schema, include relative to the schema.
-	if (SCHEMA and SCHEMA.folder and SCHEMA.loading) then
-		baseDir = SCHEMA.folder.."/schema/"
-	else
-		baseDir = baseDir.."/gamemode/"
-	end
+    -- If we're in a schema, include relative to the schema.
+    if SCHEMA and SCHEMA.folder and SCHEMA.loading then
+        baseDir = SCHEMA.folder .. "/schema/"
+    else
+        baseDir = baseDir .. "/gamemode/"
+    end
 
-	if recursive then
-		local function AddRecursive(folder)
-			local files, folders = file.Find(folder.."/*", "LUA")
+    if recursive then
+        local function AddRecursive(folder)
+            local files, folders = file.Find(folder .. "/*", "LUA")
 
-			if (!files) then
-				MsgN("Warning! This folder is empty!")
-				return
-			end
+            if not files then
+                MsgN("Warning! This folder is empty!")
 
-			for k, v in pairs(files) do
-				lia.util.include(folder .. "/" .. v)
-			end
+                return
+            end
 
-			for k, v in pairs(folders) do
-				AddRecursive(folder .. "/" .. v)
-			end
-		end
-		AddRecursive((fromLua and "" or baseDir)..directory)
-	else
-		-- Find all of the files within the directory.
-		for k, v in ipairs(
-			file.Find((fromLua and "" or baseDir)..directory.."/*.lua", "LUA")
-		) do
-			-- Include the file from the prefix.
-			lia.util.include(directory.."/"..v)
-		end
-	end
+            for k, v in pairs(files) do
+                lia.util.include(folder .. "/" .. v)
+            end
+
+            for k, v in pairs(folders) do
+                AddRecursive(folder .. "/" .. v)
+            end
+        end
+
+        AddRecursive((fromLua and "" or baseDir) .. directory)
+    else
+        -- Find all of the files within the directory.
+        for k, v in ipairs(file.Find((fromLua and "" or baseDir) .. directory .. "/*.lua", "LUA")) do
+            -- Include the file from the prefix.
+            lia.util.include(directory .. "/" .. v)
+        end
+    end
 end
 
 -- Returns the address:port of the server.
 function lia.util.getAddress()
-	ErrorNoHalt(
-		"lia.util.getAddress() is deprecated, use game.GetIPAddress()\n"
-	)
-	return game.GetIPAddress()
+    ErrorNoHalt("lia.util.getAddress() is deprecated, use game.GetIPAddress()\n")
+
+    return game.GetIPAddress()
 end
 
 -- Returns a table of admin players
+function ix.util.GetAdmins(isSuper)
+    lia.util.getAdmins(isSuper)
+end
+
 function lia.util.getAdmins(isSuper)
-	local admins = {}
+    local admins = {}
 
-	for k, v in ipairs(player.GetAll()) do
-		if (isSuper) then
-			if (v:IsSuperAdmin()) then
-				admins[#admins + 1] = v
-			end
-		else
-			if (v:IsAdmin()) then
-				admins[#admins + 1] = v
-			end
-		end
-	end
+    for k, v in ipairs(player.GetAll()) do
+        if isSuper then
+            if v:IsSuperAdmin() then
+                admins[#admins + 1] = v
+            end
+        else
+            if v:IsAdmin() then
+                admins[#admins + 1] = v
+            end
+        end
+    end
 
-	return admins
+    return admins
 end
 
 -- Returns true if a string is a 32-bit SteamID.
+function ix.util.IsSteamID(value)
+    lia.util.isSteamID(value)
+end
+
 function lia.util.isSteamID(value)
-	if (string.match(value, "STEAM_(%d+):(%d+):(%d+)")) then
-		return true
-	end
-	return false
+    if string.match(value, "STEAM_(%d+):(%d+):(%d+)") then return true end
+
+    return false
+end
+
+function ix.util.FindPlayer(identifier, bAllowPatterns)
+    lia.util.findPlayer(identifier, allowPatterns)
 end
 
 -- Finds a player by matching their name or steam id.
 function lia.util.findPlayer(identifier, allowPatterns)
-	if (lia.util.isSteamID(identifier)) then
-		return player.GetBySteamID(identifier)
-	end
+    if lia.util.isSteamID(identifier) then return player.GetBySteamID(identifier) end
 
-	if (!allowPatterns) then
-		identifier = string.PatternSafe(identifier)
-	end
+    if not allowPatterns then
+        identifier = string.PatternSafe(identifier)
+    end
 
-	for k, v in ipairs(player.GetAll()) do
-		if (lia.util.stringMatches(v:Name(), identifier)) then
-			return v
-		end
-	end
+    for k, v in ipairs(player.GetAll()) do
+        if lia.util.stringMatches(v:Name(), identifier) then return v end
+    end
+end
+
+function ix.util.GridVector(vec, gridSize)
+    lia.util.gridVector(vec, gridSize)
 end
 
 function lia.util.gridVector(vec, gridSize)
-	if (gridSize <= 0) then
-		gridSize = 1
-	end
+    if gridSize <= 0 then
+        gridSize = 1
+    end
 
-	for i = 1, 3 do
-		vec[i] = vec[i] / gridSize
-		vec[i] = math.Round(vec[i])
-		vec[i] = vec[i] * gridSize
-	end
+    for i = 1, 3 do
+        vec[i] = vec[i] / gridSize
+        vec[i] = math.Round(vec[i])
+        vec[i] = vec[i] * gridSize
+    end
 
-	return vec
+    return vec
 end
 
-
 function lia.util.getAllChar()
-	local charTable = {}
+    local charTable = {}
 
-	for k, v in ipairs(player.GetAll()) do
-		if (v:getChar()) then
-			table.insert(charTable, v:getChar():getID())
-		end
-	end
+    for k, v in ipairs(player.GetAll()) do
+        if v:getChar() then
+            table.insert(charTable, v:getChar():getID())
+        end
+    end
 
-	return charTable
+    return charTable
 end
 
 -- Misc. player stuff.
 do
-	local playerMeta = FindMetaTable("Player")
+    local playerMeta = FindMetaTable("Player")
+    -- Default list of weapons that are always raised.
+    ALWAYS_RAISED = ALWAYS_RAISED or {}
+    ALWAYS_RAISED["weapon_physgun"] = true
+    ALWAYS_RAISED["gmod_tool"] = true
+    ALWAYS_RAISED["lia_poshelper"] = true
 
-	-- Default list of weapons that are always raised.
-	ALWAYS_RAISED = ALWAYS_RAISED or {}
-	ALWAYS_RAISED["weapon_physgun"] = true
-	ALWAYS_RAISED["gmod_tool"] = true
-	ALWAYS_RAISED["lia_poshelper"] = true
+    -- Returns how many seconds the player has played on the server in total.
+    if SERVER then
+        function playerMeta:GetPlayTime()
+            self:getPlayTime()
+        end
 
-	-- Returns how many seconds the player has played on the server in total.
+        function playerMeta:getPlayTime()
+            local diff = os.time(lia.util.dateToNumber(self.lastJoin)) - os.time(lia.util.dateToNumber(self.firstJoin))
 
-	if (SERVER) then
-		function playerMeta:getPlayTime()
-			local diff = os.time(lia.util.dateToNumber(self.lastJoin))
-				- os.time(lia.util.dateToNumber(self.firstJoin))
-			return diff + (RealTime() - (self.liaJoinTime or RealTime()))
-		end
-	else
-		function playerMeta:getPlayTime()
-			local diff = os.time(lia.util.dateToNumber(lia.lastJoin))
-				- os.time(lia.util.dateToNumber(lia.firstJoin))
-			return diff + (RealTime() - lia.joinTime or 0)
-		end
-	end
+            return diff + (RealTime() - (self.liaJoinTime or RealTime()))
+        end
+    else
+        function playerMeta:GetPlayTime()
+            self:getPlayTime()
+        end
 
-	-- Returns true if the player is moving at least the running speed.
-	local vectorLength2D = FindMetaTable("Vector").Length2D
+        function playerMeta:getPlayTime()
+            local diff = os.time(lia.util.dateToNumber(lia.lastJoin)) - os.time(lia.util.dateToNumber(lia.firstJoin))
 
-	function playerMeta:isRunning()
-		return vectorLength2D(self:GetVelocity()) > (self:GetWalkSpeed() + 10)
-	end
+            return diff + (RealTime() - lia.joinTime or 0)
+        end
+    end
 
-	-- Checks if the player has a female model.
-	function playerMeta:isFemale()
-		local model = self:GetModel():lower()
+    -- Returns true if the player is moving at least the running speed.
+    local vectorLength2D = FindMetaTable("Vector").Length2D
 
-		return model:find("female")
-			or model:find("alyx")
-			or model:find("mossman")
-			or lia.anim.getModelClass(model) == "citizen_female"
-	end
+    function playerMeta:isRunning()
+        return vectorLength2D(self:GetVelocity()) > (self:GetWalkSpeed() + 10)
+    end
 
-	-- Returns a good position in front of the player for an entity.
-	function playerMeta:getItemDropPos()
-		-- Start a trace.
-		local data = {}
-			data.start = self:GetShootPos()
-			data.endpos = self:GetShootPos() + self:GetAimVector()*86
-			data.filter = self
-		local trace = util.TraceLine(data)
-			data.start = trace.HitPos
-			data.endpos = data.start + trace.HitNormal*46
-			data.filter = {}
-		trace = util.TraceLine(data)
+    function playerMeta:IsRunning()
+        self:isRunning()
+    end
 
-		return trace.HitPos
-	end
+    -- Checks if the player has a female model.
+    function playerMeta:IsFemale()
+        self:isFemale()
+    end
 
-	if (SERVER) then
-		-- Removes a player's weapon and restricts interactivity.
-		function playerMeta:setRestricted(state, noMessage)
-			if (state) then
-				self:setNetVar("restricted", true)
+    function playerMeta:isFemale()
+        local model = self:GetModel():lower()
 
-				if (noMessage) then
-					self:setLocalVar("restrictNoMsg", true)
-				end
+        return model:find("female") or model:find("alyx") or model:find("mossman") or lia.anim.getModelClass(model) == "citizen_female"
+    end
 
-				self.liaRestrictWeps = self.liaRestrictWeps or {}
+    -- Returns a good position in front of the player for an entity.
+    function playerMeta:GetItemDropPos()
+        self:getItemDropPos()
+    end
 
-				for k, v in ipairs(self:GetWeapons()) do
-					self.liaRestrictWeps[k] = v:GetClass()
-				end
-				timer.Simple(0, function()
-					self:StripWeapons()
-				end)
-				hook.Run("OnPlayerRestricted", self)
-			else
-				self:setNetVar("restricted")
+    function playerMeta:getItemDropPos()
+        -- Start a trace.
+        local data = {}
+        data.start = self:GetShootPos()
+        data.endpos = self:GetShootPos() + self:GetAimVector() * 86
+        data.filter = self
+        local trace = util.TraceLine(data)
+        data.start = trace.HitPos
+        data.endpos = data.start + trace.HitNormal * 46
+        data.filter = {}
+        trace = util.TraceLine(data)
 
-				if (self:getLocalVar("restrictNoMsg")) then
-					self:setLocalVar("restrictNoMsg")
-				end
+        return trace.HitPos
+    end
 
-				if (self.liaRestrictWeps) then
-					for k, v in ipairs(self.liaRestrictWeps) do
-						self:Give(v)
-					end
+    if SERVER then
+        function playerMeta:setRestricted(state, noMessage)
+            self:SetRestricted(state, noMessage)
+        end
 
-					self.liaRestrictWeps = nil
-				end
+        -- Removes a player's weapon and restricts interactivity.
+        function playerMeta:setRestricted(state, noMessage)
+            if state then
+                self:setNetVar("restricted", true)
 
-				hook.Run("OnPlayerUnRestricted", self)
-			end
-		end
-	end
+                if noMessage then
+                    self:setLocalVar("restrictNoMsg", true)
+                end
+
+                self.liaRestrictWeps = self.liaRestrictWeps or {}
+
+                for k, v in ipairs(self:GetWeapons()) do
+                    self.liaRestrictWeps[k] = v:GetClass()
+                end
+
+                timer.Simple(0, function()
+                    self:StripWeapons()
+                end)
+
+                hook.Run("OnPlayerRestricted", self)
+            else
+                self:setNetVar("restricted")
+
+                if self:getLocalVar("restrictNoMsg") then
+                    self:setLocalVar("restrictNoMsg")
+                end
+
+                if self.liaRestrictWeps then
+                    for k, v in ipairs(self.liaRestrictWeps) do
+                        self:Give(v)
+                    end
+
+                    self.liaRestrictWeps = nil
+                end
+
+                hook.Run("OnPlayerUnRestricted", self)
+            end
+        end
+    end
 end
 
 lia.util.cachedMaterials = lia.util.cachedMaterials or {}
 
+function ix.util.GetMaterial(materialPath)
+    lia.util.getMaterial(materialPath)
+end
+
 -- Returns a single cached copy of a material or creates it if it doesn't exist.
 function lia.util.getMaterial(materialPath)
-	-- Cache the material.
-	lia.util.cachedMaterials[materialPath] =
-		lia.util.cachedMaterials[materialPath]
-		or Material(materialPath)
-	return lia.util.cachedMaterials[materialPath]
+    -- Cache the material.
+    lia.util.cachedMaterials[materialPath] = lia.util.cachedMaterials[materialPath] or Material(materialPath)
+
+    return lia.util.cachedMaterials[materialPath]
 end
 
 lia.util.includeDir("lilia/gamemode/core/util", true)
