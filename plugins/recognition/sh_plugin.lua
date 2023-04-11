7295baede228ce1abeea1647bca3585613252d9a
Leonheart <33399712+bleonheart@users.noreply.github.com>
2023-04-11 21:28:40 +0100

    Fixed Typos

--- plugins/recognition/sh_plugin.lua
+++ plugins/recognition/sh_plugin.lua
@@ -3,6 +3,7 @@ PLUGIN.author = "Cheesenot/Leonheart#7476"
 PLUGIN.desc = "Adds the ability to recognize people // You can also allow auto faction recognition."
 lia.util.include("sv_plugin.lua")
 lia.util.include("cl_plugin.lua")
+--[[
 PLUGIN.noRecognise = {
     [FACTION_CITIZEN] = true,
-}
+}]]
