720e3aee3cdeac6184b7b93a030e06a55c7e6eff
Leonheart <33399712+bleonheart@users.noreply.github.com>
2023-04-11 13:53:57 +0100

    Update sh_plugin.lua

--- plugins/frameworkcompatibility/sh_plugin.lua
+++ plugins/frameworkcompatibility/sh_plugin.lua
@@ -1,4 +1,4 @@
-
 PLUGIN.name = "Framework Compatibility"
 PLUGIN.author = "Cheesenot/Leonheart#7476"
 PLUGIN.desc = "Adds Compatibility for DarkRP, Nutscript and Helix"
+lia.util.include("sv_plugin.lua)
