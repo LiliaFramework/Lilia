d79197eeaaee4afdda3cf966d872eabf04d824b4
Leonheart <33399712+bleonheart@users.noreply.github.com>
2023-04-11 14:36:20 +0100

    Finished DarkRP Money & Notification Compatibility

--- plugins/frameworkcompatibility/sv_plugin.lua
+++ plugins/frameworkcompatibility/sv_plugin.lua
@@ -1,1 +1,53 @@
+DarkRP = {}
 nut = lia or {}
+local playerMeta = FindMetaTable("Player")
+local entityMeta = FindMetaTable("Entity")
+local vehicleMeta = FindMetaTable("Vehicle")
+
+-----------------DARKP ----------------
+function playerMeta:canAfford(x)
+    return self:getChar():hasMoney(x)
+end
+
+function playerMeta:addMoney(x)
+    if x < 0 then
+        self:getChar():takeMoney(x)
+    else
+        self:getChar():giveMoney(x)
+    end
+end
+
+function playerMeta:getDarkRPVar(var)
+    if var ~= "money" then
+        self:ChatPrint("Tried To Fetch Improper Variable! Please Refer to Our Discord for Help!")
+
+        return
+    end
+
+    return self:getChar():getMoney()
+end
+
+function vehicleMeta:keysLock()
+    self:Fire("lock")
+end
+
+function vehicleMeta:keysUnLock()
+    self:Fire("unlock")
+end
+
+function vehicleMeta:keysOwn(_p)
+    self.liaAccess = self.liaAccess or {}
+    self.liaAccess[_p] = 3
+end
+
+DarkRP.formatMoney = function(x) return lia.currency.get .. string.Comma(x) end
+
+function DarkRP.notify(ply, msgtype, len, msg)
+    if type(ply) ~= "table" and not IsValid(ply) then return end
+    umsg.Start("_Notify", ply)
+    umsg.String(msg)
+    umsg.Short(msgtype)
+    umsg.Long(len)
+    umsg.End()
+end
+-----------------Helix ----------------
