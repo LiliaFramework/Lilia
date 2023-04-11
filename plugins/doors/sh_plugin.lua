5a4717ac3322a581dd6ac595ae71f77ccd79c34e
Leonheart <33399712+bleonheart@users.noreply.github.com>
2023-04-11 14:42:37 +0100

    Added DarkRP & Helix Compatibility to the Doors > sh_plugin

--- plugins/doors/sh_plugin.lua
+++ plugins/doors/sh_plugin.lua
@@ -1,70 +1,178 @@
+local PLUGIN = PLUGIN
 PLUGIN.name = "Doors"
 PLUGIN.author = "Cheesenot & Leonheart#7476"
 PLUGIN.desc = "A simple door system // Now has multifaction support!"
-
 DOOR_OWNER = 3
 DOOR_TENANT = 2
 DOOR_GUEST = 1
 DOOR_NONE = 0
-
 lia.util.include("sv_plugin.lua")
 lia.util.include("cl_plugin.lua")
 lia.util.include("sh_commands.lua")
 
 do
-	local entityMeta = FindMetaTable("Entity")
+    local entityMeta = FindMetaTable("Entity")
 
-	function entityMeta:checkDoorAccess(client, access)
-		if (!self:isDoor()) then
-			return false
-		end
+    function entityMeta:getDoorOwner()
+        if not self:isDoor() then return nil end
+        local parent = self.nutParent
+        if IsValid(parent) then return parent:getDoorOwner() end
+        if not self.nutAccess then return nil end
 
-		access = access or DOOR_GUEST
+        for client, access in pairs(self.nutAccess) do
+            if access == DOOR_OWNER then return client end
+        end
 
-		local parent = self.liaParent
+        return nil
+    end
 
-		if (IsValid(parent)) then
-			return parent:checkDoorAccess(client, access)
-		end
+    function entityMeta:checkDoorAccess(client, access)
+        if not self:isDoor() then return false end
+        access = access or DOOR_GUEST
+        local parent = self.liaParent
+        if IsValid(parent) then return parent:checkDoorAccess(client, access) end
+        if hook.Run("CanPlayerAccessDoor", client, self, access) then return true end
+        if self.liaAccess and (self.liaAccess[client] or 0) >= access then return true end
 
-		if (hook.Run("CanPlayerAccessDoor", client, self, access)) then
-			return true
-		end
+        return false
+    end
 
-		if (self.liaAccess and (self.liaAccess[client] or 0) >= access) then
-			return true
-		end
+    function entityMeta:CheckDoorAccess(client, access)
+        if not self:IsDoor() then return false end
+        access = access or DOOR_GUEST
+        local parent = self.ixParent
+        if IsValid(parent) then return parent:CheckDoorAccess(client, access) end
+        if hook.Run("CanPlayerAccessDoor", client, self, access) then return true end
+        if self.ixAccess and (self.ixAccess[client] or 0) >= access then return true end
 
-		return false
-	end
+        return false
+    end
 
-	if (SERVER) then
-		function entityMeta:removeDoorAccessData()
-			-- Don't ask why. This happened with 60 player servers.
-			if (IsValid(self)) then
-				for k, v in pairs(self.liaAccess or {}) do
-					netstream.Start(k, "doorMenu")
-				end
-				
-				self.liaAccess = {}
-				self:SetDTEntity(0, nil)
-			end
-		end
-	end
+    if SERVER then
+        function entityMeta:keysOwn(client)
+            if not self:isDoor() then return end
+
+            if self.nutParent then
+                self.nutParent:keysOwn(client)
+
+                return
+            end
+
+            self:SetDTEntity(0, client)
+            self.nutAccess = self.nutAccess or {}
+            self.nutAccess[client] = DOOR_OWNER
+
+            PLUGIN:callOnDoorChildren(self, function(child)
+                child:SetDTEntity(0, client)
+            end)
+
+            PLUGIN:UpdateClientDoorAccess(self, client, DOOR_OWNER)
+        end
+
+        function entityMeta:keysUnOwn(client)
+            if not self:isDoor() then return end
+
+            if self.nutParent then
+                self.nutParent:keysUnOwn(client)
+
+                return
+            end
+
+            self:SetDTEntity(0, nil)
+            self.nutAccess = self.nutAccess or {}
+            self.nutAccess[client] = DOOR_NONE
+
+            PLUGIN:callOnDoorChildren(self, function(child)
+                child:SetDTEntity(0, nil)
+            end)
+
+            PLUGIN:UpdateClientDoorAccess(self, client, DOOR_NONE)
+        end
+
+        function entityMeta:addKeysDoorOwner(client)
+            if not self:isDoor() then return end
+
+            if self.nutParent then
+                self.nutParent:addKeysDoorOwner(client)
+
+                return
+            end
+
+            self.nutAccess = self.nutAccess or {}
+            self.nutAccess[client] = DOOR_TENANT
+            PLUGIN:UpdateClientDoorAccess(self, client, DOOR_TENANT)
+        end
+
+        function entityMeta:removeKeysDoorOwner(client)
+            if not self:isDoor() then return end
+
+            if self.nutParent then
+                self.nutParent:removeKeysDoorOwner(client)
+
+                return
+            end
+
+            self.nutAccess = self.nutAccess or {}
+            self.nutAccess[client] = DOOR_NONE
+            PLUGIN:UpdateClientDoorAccess(self, client, DOOR_NONE)
+        end
+
+        function entityMeta:removeDoorAccessData()
+            if IsValid(self) then
+                for k, v in pairs(self.liaAccess or {}) do
+                    netstream.Start(k, "doorMenu")
+                end
+
+                self.liaAccess = {}
+                self:SetDTEntity(0, nil)
+            end
+        end
+
+        function entityMeta:RemoveDoorAccessData()
+            local receivers = {}
+
+            for k, _ in pairs(self.ixAccess or {}) do
+                receivers[#receivers + 1] = k
+            end
+
+            if #receivers > 0 then
+                net.Start("ixDoorMenu")
+                net.Send(receivers)
+            end
+
+            self.ixAccess = {}
+            self:SetDTEntity(0, nil)
+
+            -- Remove door information on child doors
+            PLUGIN:CallOnDoorChildren(self, function(child)
+                child:SetDTEntity(0, nil)
+            end)
+        end
+    end
 end
-
 -- Configurations for door prices.
 lia.config.add("doorCost", 10, "The price to purchase a door.", nil, {
-	data = {min = 0, max = 500},
-	category = "dConfigName"
+    data = {
+        min = 0,
+        max = 500
+    },
+    category = "dConfigName"
 })
+
 lia.config.add("doorSellRatio", 0.5, "How much of the door price is returned when selling a door.", nil, {
-	form = "Float",
-	data = {min = 0, max = 1.0},
-	category = "dConfigName"
+    form = "Float",
+    data = {
+        min = 0,
+        max = 1.0
+    },
+    category = "dConfigName"
 })
+
 lia.config.add("doorLockTime", 1, "How long it takes to (un)lock a door.", nil, {
-	form = "Float",
-	data = {min = 0, max = 10.0},
-	category = "dConfigName"
+    form = "Float",
+    data = {
+        min = 0,
+        max = 10.0
+    },
+    category = "dConfigName"
 })
