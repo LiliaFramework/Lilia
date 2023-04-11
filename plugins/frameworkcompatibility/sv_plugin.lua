a745f1eaf2120d684ffec64bdb2f48235e50a45d
Leonheart <33399712+bleonheart@users.noreply.github.com>
2023-04-11 14:49:38 +0100

    Added IX default function

--- plugins/frameworkcompatibility/sv_plugin.lua
+++ plugins/frameworkcompatibility/sv_plugin.lua
@@ -1,7 +1,7 @@
 DarkRP = {}
 nut = lia or {}
-ix.config.Add = lia.config.add or {} 
-
+ix = lia or {}
+ix.config.Add = lia.config.add or {}
 local playerMeta = FindMetaTable("Player")
 local entityMeta = FindMetaTable("Entity")
 local vehicleMeta = FindMetaTable("Vehicle")
