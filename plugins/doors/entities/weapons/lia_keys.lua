8d69b9c4b35676b80c3412a18cb4fa6f5ec1302c
Leonheart <33399712+bleonheart@users.noreply.github.com>
2023-04-28 00:52:40 +0100

    Optimized Keys

--- plugins/doors/entities/weapons/lia_keys.lua
+++ plugins/doors/entities/weapons/lia_keys.lua
@@ -44,16 +44,16 @@ ACT_VM_FISTS_DRAW = 3
 ACT_VM_FISTS_HOLSTER = 2
 
 function SWEP:Deploy()
-    if not IsValid(self.Owner) then return end
-    local viewModel = self.Owner:GetViewModel()
+    if not IsValid(self:GetOwner()) then return end
+    local viewModel = self:GetOwner():GetViewModel()
     if IsValid(viewModel) then end --viewModel:SetPlaybackRate(1) --viewModel:ResetSequence(ACT_VM_FISTS_DRAW)
 
     return true
 end
 
 function SWEP:Holster()
-    if not IsValid(self.Owner) then return end
-    local viewModel = self.Owner:GetViewModel()
+    if not IsValid(self:GetOwner()) then return end
+    local viewModel = self:GetOwner():GetViewModel()
 
     if IsValid(viewModel) then
         viewModel:SetPlaybackRate(1)
@@ -78,9 +78,9 @@ function SWEP:PrimaryAttack()
     if not IsFirstTimePredicted() then return end
     if CLIENT then return end
     local data = {}
-    data.start = self.Owner:GetShootPos()
-    data.endpos = data.start + self.Owner:GetAimVector() * 96
-    data.filter = self.Owner
+    data.start = self:GetOwner():GetShootPos()
+    data.endpos = data.start + self:GetOwner():GetAimVector() * 96
+    data.filter = self:GetOwner()
     local entity = util.TraceLine(data).Entity
 
     --[[
@@ -88,9 +88,11 @@ function SWEP:PrimaryAttack()
 			1. The entity is door and client has access to the door.
 			2. The entity is vehicle and the "owner" variable is same as client's character ID.
 	--]]
-    if IsValid(entity) and ((entity:isDoor() and (entity:checkDoorAccess(self.Owner) or (self.Owner:IsAdmin() ))) or (entity:IsVehicle() and ((entity.CPPIGetOwner and entity:CPPIGetOwner() == self.Owner) or (entity.GetCreator and entity:GetCreator() == self.Owner)))) then
-        self.Owner:setAction("@locking", time, function()
-            self:toggleLock(entity, true)
+    if IsValid(entity) and ((entity:isDoor() and (entity:checkDoorAccess(self:GetOwner()) or self:GetOwner():IsAdmin())) or (entity:IsVehicle() and ((entity.CPPIGetOwner and entity:CPPIGetOwner() == self:GetOwner()) or (entity.GetCreator and entity:GetCreator() == self:GetOwner())))) then
+        self:GetOwner():setAction("@locking", time, function()
+            if SERVER then
+                self:toggleLock(entity, true)
+            end
         end)
 
         return
@@ -98,9 +100,7 @@ function SWEP:PrimaryAttack()
 end
 
 function SWEP:toggleLock(door, state)
-	if (IsValid(self.Owner) and self.Owner:GetPos():Distance(door:GetPos()) > 96) then
-		return
-	end
+    if IsValid(self:GetOwner()) and self:GetOwner():GetPos():Distance(door:GetPos()) > 96 then return end
 
     if door:isDoor() then
         local partner = door:getDoorPartner()
@@ -111,30 +111,34 @@ function SWEP:toggleLock(door, state)
             end
 
             door:Fire("lock")
-            self.Owner:EmitSound("doors/door_latch3.wav")
+            self:GetOwner():EmitSound("doors/door_latch3.wav")
         else
             if IsValid(partner) then
                 partner:Fire("unlock")
             end
 
             door:Fire("unlock")
-            self.Owner:EmitSound("doors/door_latch1.wav")
+            self:GetOwner():EmitSound("doors/door_latch1.wav")
         end
-    elseif (door:IsVehicle()) then
-		if (state) then
-			door:Fire("lock")
-			if (door.IsSimfphyscar) then
-				door:Lock()
-			end
-			self.Owner:EmitSound("doors/door_latch3.wav")
-		else
-			door:Fire("unlock")
-			if (door.IsSimfphyscar) then
-				door:UnLock()
-			end
-			self.Owner:EmitSound("doors/door_latch1.wav")
-		end
-	end
+    elseif door:IsVehicle() then
+        if state then
+            door:Fire("lock")
+
+            if door.IsSimfphyscar then
+                door.IsLocked = true
+            end
+
+            self:GetOwner():EmitSound("doors/door_latch3.wav")
+        else
+            door:Fire("unlock")
+
+            if door.IsSimfphyscar then
+                door.IsLocked = nil
+            end
+
+            self:GetOwner():EmitSound("doors/door_latch1.wav")
+        end
+    end
 end
 
 function SWEP:SecondaryAttack()
@@ -145,9 +149,9 @@ function SWEP:SecondaryAttack()
     if not IsFirstTimePredicted() then return end
     if CLIENT then return end
     local data = {}
-    data.start = self.Owner:GetShootPos()
-    data.endpos = data.start + self.Owner:GetAimVector() * 96
-    data.filter = self.Owner
+    data.start = self:GetOwner():GetShootPos()
+    data.endpos = data.start + self:GetOwner():GetAimVector() * 96
+    data.filter = self:GetOwner()
     local entity = util.TraceLine(data).Entity
 
     --[[
@@ -155,8 +159,8 @@ function SWEP:SecondaryAttack()
 			1. The entity is door and client has access to the door.
 			2. The entity is vehicle and the "owner" variable is same as client's character ID.
 	]]
-    if IsValid(entity) and ((entity:isDoor() and (entity:checkDoorAccess(self.Owner) or (self.Owner:IsAdmin() ))) or (entity:IsVehicle() and ((entity.CPPIGetOwner and entity:CPPIGetOwner() == self.Owner) or (entity.GetCreator and entity:GetCreator() == self.Owner)))) then
-        self.Owner:setAction("@unlocking", time, function()
+    if IsValid(entity) and ((entity:isDoor() and (entity:checkDoorAccess(self:GetOwner()) or self:GetOwner():IsAdmin())) or (entity:IsVehicle() and ((entity.CPPIGetOwner and entity:CPPIGetOwner() == self:GetOwner()) or (entity.GetCreator and entity:GetCreator() == self:GetOwner())))) then
+        self:GetOwner():setAction("@unlocking", time, function()
             self:toggleLock(entity, false)
         end)
 
