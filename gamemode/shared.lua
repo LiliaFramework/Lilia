ed3dd16c2607c8463a42b2baae332cf40c610053
Leonheart <33399712+bleonheart@users.noreply.github.com>
2023-05-02 18:52:01 +0100

    Update shared.lua

--- gamemode/shared.lua
+++ gamemode/shared.lua
@@ -59,6 +59,7 @@ lia.util.includeDir("core/hooks")
 -- Include language and default base items.
 lia.lang.loadFromDir("lilia/gamemode/languages")
 lia.item.loadFromDir("lilia/gamemode/items")
+lia.item.loadFromDir("lilia/gamemode/commands")
 
 -- Called after the gamemode has loaded.
 function GM:Initialize()
