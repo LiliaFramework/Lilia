059c621496418dba4ca10bc4a16be02ddeb54bf6
Leonheart <33399712+bleonheart@users.noreply.github.com>
2023-05-07 22:51:13 +0100

    Update sh_config.lua

--- plugins/time/sh_config.lua
+++ plugins/time/sh_config.lua
@@ -1,11 +1,11 @@
 lia.config.add("TimeOnScreenEnabled", false, "Enable Time On Screen?", nil, {
-    category = "Time"
+    category = "HUD"
 })
 
-lia.config.add("SchemaYear", 2023, "Year that should appear on the HUD.", nil, {
+lia.config.add("SchemaYear", 2023, "Year That The Gamemode Happens On.", nil, {
     data = {
         min = 1,
         max = 5000
     },
-    category = "Time"
+    category = "Server"
 })
