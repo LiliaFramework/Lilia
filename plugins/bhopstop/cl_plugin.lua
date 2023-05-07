6bcaf0570f842bfdd72a728a27aded540e3b232a
Leonheart <33399712+bleonheart@users.noreply.github.com>
2023-05-07 15:55:20 +0100

    Fixed Typo

--- plugins/bhopstop/cl_plugin.lua
+++ plugins/bhopstop/cl_plugin.lua
@@ -2,6 +2,6 @@ function PLUGIN:PlayerBindPress(client, bind, pressed)
     if lia.config.get("AntiBunnyHopEnabled", true) then
         if client:GetMoveType() == MOVETYPE_NOCLIP or not client:getChar() then return end
         bind = bind:lower()
-        if bind:find("jump") and (client:getLocalVar("stm", 0) < nut.config.get("bunnyHop")) then return true end
+        if bind:find("jump") and (client:getLocalVar("stm", 0) < lia.config.get("bunnyHop")) then return true end
     end
 end
