cff0f631cd79a06e6ac1f599f8da467ed8e8d4d6
Leonheart <33399712+bleonheart@users.noreply.github.com>
2023-05-08 17:56:46 +0100

    Update sv_hooks.lua

--- gamemode/core/hooks/sv_hooks.lua
+++ gamemode/core/hooks/sv_hooks.lua
@@ -488,12 +488,12 @@ function GM:InitializedSchema()
         game.ConsoleCommand("sbox_persist " .. newValue .. "\n")
     end
 end
-
 function GM:PlayerCanHearPlayersVoice(listener, speaker)
     local ImprovedRadios = lia.plugin.list.improvedradio -- Leonheart's Radios
     local ImprovedTying = lia.plugin.list.improvedtying -- Leonheart's Tying
     local ImprovedVoice = lia.plugin.list.improvedvoice -- Framework 3D Voice
     local BroadcastingRadioVoice = lia.plugin.list.broadcastradio -- Leonheart's Broadcast Radio
+    local StandingTelephoneVoice = lia.plugin.list.standingtelephone -- Leonheart's Standing Telephone
     local distance = 600 * 600
     local allowVoice = lia.config.get("allowVoice")
 
@@ -501,6 +501,10 @@ function GM:PlayerCanHearPlayersVoice(listener, speaker)
         if listener:GetPos():DistToSqr(speaker:GetPos()) < distance then
             return true, true
         else
+            if StandingTelephoneVoice then
+                if hook.Run("PlayerCanHearPlayersVoiceStandingTelephone", listener, speaker) then return true end
+            end
+
             if BroadcastingRadioVoice then
                 if hook.Run("PlayerCanHearPlayersVoicePlacedRadios", listener, speaker) then return true end
             end
@@ -521,6 +525,7 @@ function GM:PlayerCanHearPlayersVoice(listener, speaker)
 
     return false, false
 end
+
 
 function GM:OnPhysgunFreeze(weapon, physObj, entity, client)
     -- Object is already frozen (!?)
