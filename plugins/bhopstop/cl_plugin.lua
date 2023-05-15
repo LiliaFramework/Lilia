c7c7fe040475864f19bcc50afbd4aa65ea8ee597
Leonheart <33399712+bleonheart@users.noreply.github.com>
2023-05-15 17:36:11 +0100

    Update cl_plugin.lua

--- plugins/bhopstop/cl_plugin.lua
+++ plugins/bhopstop/cl_plugin.lua
@@ -1,6 +1,6 @@
 function PLUGIN:PlayerBindPress(client, bind, pressed)
     if lia.config.get("AntiBunnyHopEnabled", true) then
-        if client:GetMoveType() == MOVETYPE_NOCLIP or not client:getChar() then return end
+        if client:GetMoveType() == MOVETYPE_NOCLIP or not client:getChar() or client:InVehicle() then return end
         bind = bind:lower()
         if bind:find("jump") and (client:getLocalVar("stm", 0) < lia.config.get("bunnyHop")) then return true end
     end
