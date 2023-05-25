297fa8bf60f011382afba0f508ace88b31f3134b
Leonheart <33399712+bleonheart@users.noreply.github.com>
2023-05-25 01:21:55 +0100

    Update sv_plugin.lua

--- plugins/bhopstop/sv_plugin.lua
+++ plugins/bhopstop/sv_plugin.lua
@@ -1,10 +1,8 @@
 function PLUGIN:KeyPress(client, key)
     if lia.config.get("AntiBunnyHopEnabled", true) then
-        local jump_power = client:GetJumpPower()
-        local current = client:getLocalVar("stm", 0)
-        local value = math.Clamp(current - lia.config.get("bunnyHop"), 0, 100)
-
-        if (key == IN_JUMP) and client:IsOnGround() then
+        if key == IN_JUMP and client:IsOnGround() and not client:InVehicle() then
+            local current = client:getLocalVar("stm", 0)
+            local value = math.Clamp(current - lia.config.get("bunnyHop"), 0, 100)
             client:setLocalVar("stm", value)
         end
     end
