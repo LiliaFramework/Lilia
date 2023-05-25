56c345eba05f1d1a1c7d83de32e4395c014131e6
Leonheart <33399712+bleonheart@users.noreply.github.com>
2023-05-25 01:17:04 +0100

    Included File - Keegan

--- plugins/playerinjuries/sh_plugin.lua
+++ plugins/playerinjuries/sh_plugin.lua
@@ -1,5 +1,6 @@
 PLUGIN.name = "Player Injuries"
 PLUGIN.author = "Leonheart#7476/Cheesenot"
 PLUGIN.desc = "Adds more realistic damage and injury sounds."
+lia.util.include("sh_config.lua")
 lia.util.include("sv_hooks.lua")
 lia.util.include("sv_drowning.lua")
