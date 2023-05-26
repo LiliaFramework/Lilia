00baf7b42d81d213641e276dcf647210f606a433
Leonheart <33399712+bleonheart@users.noreply.github.com>
2023-05-26 06:41:59 +0100

    Added Admin System Loader

---
+++ plugins/adminsystemloader/sh_plugin.lua
@@ -0,0 +1,4 @@
+PLUGIN.name = "Admin Systems Support"
+PLUGIN.author = "Leonheart#7476"
+PLUGIN.desc = "Proper Admin Systems Support"
+lia.util.include("sv_plugin.lua")
--- plugins/serverguardsupport/sh_plugin.lua
+++
@@ -1,9 +0,0 @@
-PLUGIN.name = "ServerGuard Support"
-PLUGIN.author = "Leonheart#7476/Sample Name"
-PLUGIN.desc = "Proper ServerGuard support"
--- We don't want the code below to run when ServerGuard isn't installed
-if not serverguard then return end
--- ServerGuard's restrictions plugin was created to limit toolgun possibilies, but using it with Lilia
--- prevents any admins from using any of the tools, which is obviousbly a bug. Lilia utilizes flag system to limit toolgun usage,
--- so simply disabling this ServerGuard plugin will solve the problem.
-serverguard.plugin:Toggle("restrictions", false)
