fa6392bcd5ec4c9121785797befd9d50591d0e74
Leonheart <33399712+bleonheart@users.noreply.github.com>
2023-05-28 03:46:28 +0100

    Update sh_plugin.lua

--- plugins/coreplugins/plugins/recognition/sh_plugin.lua
+++ plugins/coreplugins/plugins/recognition/sh_plugin.lua
@@ -3,7 +3,7 @@ PLUGIN.author = "Leonheart#7476/Cheesenot"
 PLUGIN.desc = "Adds the ability to recognize people / You can also allow auto faction recognition."
 lia.util.include("sv_plugin.lua")
 lia.util.include("cl_plugin.lua")
-
+--[[ 
 PLUGIN.noRecognise = {
     [FACTION_CITIZEN] = false,
-}
+}]]
