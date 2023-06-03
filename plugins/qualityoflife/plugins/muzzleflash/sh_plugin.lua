4c62f9f285ffa1b0c8b37138c30125e23a10ffeb
Leonheart <33399712+bleonheart@users.noreply.github.com>
2023-06-03 20:38:27 +0100

    Realistic Muzzle Flash

---
+++ plugins/qualityoflife/plugins/muzzleflash/sh_plugin.lua
@@ -0,0 +1,4 @@
+PLUGIN.name = "Realistic Muzzle Flash"
+PLUGIN.author = "Leonheart#7476"
+PLUGIN.description = "A simple plugin that provides clientside muzzleflash effects when firing firearms."
+lia.config.add("sv_plugin.lua")
