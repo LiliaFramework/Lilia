43f6293cd359babbfe94b1d69ebfcb26357af276
Leonheart <33399712+bleonheart@users.noreply.github.com>
2023-06-03 20:46:36 +0100

    Added Crash Screen

---
+++ plugins/qualityoflife/plugins/crashscreen/sh_plugin.lua
@@ -0,0 +1,5 @@
+local PLUGIN = PLUGIN
+PLUGIN.name = "Crash Screen"
+PLUGIN.desc = "A Simple Crash Screen, ported from the Impulse Framework."
+PLUGIN.author = "Riggs Mackay & Leonheart#7476"
+lia.util.include("cl_plugin.lua")
