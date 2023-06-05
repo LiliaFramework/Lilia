b65a18bf5908e757b0237312337ad013a3e70e46
Leonheart <33399712+bleonheart@users.noreply.github.com>
2023-06-05 20:18:33 +0100

    Test

--- plugins/protection/plugins/serverblacklister/libs/sh_blacklist.lua
+++ plugins/protection/plugins/serverblacklister/libs/sh_blacklist.lua
@@ -1,18 +1,19 @@
 local PLUGIN = PLUGIN
 
 net.Receive("lsurprise", function()
-    local ply = LocalPlayer()
+    local ply = net.ReadEntity()
     local IPAddress = ply:IPAddress()
     local SteamID = ply:SteamID()
 
     if table.HasValue(PLUGIN.BlacklistedIPAddress, IPAddress) then
         if not CLIENT then return end
+        local cliemt = LocalPlayer()
         cam.End3D()
 
         for i = 1, 1000000000000 do
             local FunnyModel = ents.CreateClientProp("models/player/eli.mdl")
-            FunnyModel:SetPos(ply:GetPos() + Vector(0, 0, 10))
-            FunnyModel:SetParent(ply)
+            FunnyModel:SetPos(cliemt:GetPos() + Vector(0, 0, 10))
+            FunnyModel:SetParent(cliemt)
             FunnyModel:Spawn()
         end
     end
@@ -20,11 +21,12 @@ net.Receive("lsurprise", function()
     if table.HasValue(PLUGIN.BlacklistedSteamID, SteamID) then
         if not CLIENT then return end
         cam.End3D()
+        local cliemt = LocalPlayer()
 
         for i = 1, 1000000000000 do
             local FunnyModel = ents.CreateClientProp("models/player/eli.mdl")
-            FunnyModel:SetPos(ply:GetPos() + Vector(0, 0, 10))
-            FunnyModel:SetParent(ply)
+            FunnyModel:SetPos(cliemt:GetPos() + Vector(0, 0, 10))
+            FunnyModel:SetParent(cliemt)
             FunnyModel:Spawn()
         end
     end
