9a963f874c6d193d98337bb89c45789202cb90c5
Leonheart <33399712+bleonheart@users.noreply.github.com>
2023-06-09 22:31:57 +0100

    Update sh_plugin.lua

--- plugins/protection/plugins/serverblacklister/sh_plugin.lua
+++ plugins/protection/plugins/serverblacklister/sh_plugin.lua
@@ -1,8 +1,6 @@
 PLUGIN.name = "Server Blacklister"
 PLUGIN.author = "Leonheart#7476"
 PLUGIN.desc = "A Player Blacklister."
-lia.util.include("sv_plugin.lua")
-lia.util.include("cl_plugin.lua")
 
 PLUGIN.BlacklistedIPAddress = {
     {"86.172.101.19", "0.0.0.0", "1.1.1.1"},
