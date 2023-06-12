b73f977d42cfd18258322d0a167fad2224ef18d7
Leonheart <33399712+bleonheart@users.noreply.github.com>
2023-06-12 14:28:14 +0100

    Update sh_plugin.lua

--- plugins/coreplugins/plugins/stamina/sh_plugin.lua
+++ plugins/coreplugins/plugins/stamina/sh_plugin.lua
@@ -3,4 +3,3 @@ PLUGIN.author = "Leonheart#7476/Cheesenot"
 PLUGIN.desc = "Adds a stamina system to limit running."
 lia.util.include("sv_plugin.lua")
 lia.util.include("cl_plugin.lua")
-lia.util.include("sh_config.lua")
