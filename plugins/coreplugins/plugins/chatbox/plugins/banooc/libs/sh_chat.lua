56c8a254a68780cd64de26199b6f7e2e97298cf4
Leonheart <33399712+bleonheart@users.noreply.github.com>
2023-07-26 10:58:13 +0100

    Update sh_chat.lua

--- plugins/coreplugins/plugins/chatbox/plugins/banooc/libs/sh_chat.lua
+++ plugins/coreplugins/plugins/chatbox/plugins/banooc/libs/sh_chat.lua
@@ -1,51 +1,59 @@
 local PLUGIN = PLUGIN
-lia.chat.register(
-    "ooc",
-    {
-        onCanSay = function(speaker, text)
-            local delay = lia.config.get("oocDelay", 10)
-            local oocmaxsize = lia.config.get("oocLimit", 10)
-            if GetGlobalBool("oocblocked", false) then
-                speaker:notify("The OOC is Globally Blocked!")
+do
+    hook.Add(
+        "InitializedConfig",
+        "liaChatOOC",
+        function()
+            lia.chat.register(
+                "ooc",
+                {
+                    onCanSay = function(speaker, text)
+                        local delay = lia.config.get("oocDelay", 10)
+                        local oocmaxsize = lia.config.get("oocLimit", 10)
+                        if GetGlobalBool("oocblocked", false) then
+                            speaker:notify("The OOC is Globally Blocked!")
 
-                return false
-            end
+                            return false
+                        end
 
-            if PLUGIN.oocBans[speaker:SteamID()] then
-                speaker:notify("You have been banned from using OOC!")
+                        if PLUGIN.oocBans[speaker:SteamID()] then
+                            speaker:notify("You have been banned from using OOC!")
 
-                return false
-            end
+                            return false
+                        end
 
-            if string.len(text) > oocmaxsize then
-                speaker:notify("Text too big!")
+                        if string.len(text) > oocmaxsize then
+                            speaker:notify("Text too big!")
 
-                return false
-            end
+                            return false
+                        end
 
-            if not speaker:IsAdmin() then
-                -- Only need to check the time if they have spoken in OOC chat before.
-                if delay > 0 and speaker.liaLastOOC then
-                    local lastOOC = CurTime() - speaker.liaLastOOC
-                    -- Use this method of checking time in case the oocDelay config changes.
-                    if lastOOC <= delay then
-                        speaker:notifyLocalized("oocDelay", delay - math.ceil(lastOOC))
+                        if not speaker:IsAdmin() then
+                            -- Only need to check the time if they have spoken in OOC chat before.
+                            if delay > 0 and speaker.liaLastOOC then
+                                local lastOOC = CurTime() - speaker.liaLastOOC
+                                -- Use this method of checking time in case the oocDelay config changes.
+                                if lastOOC <= delay then
+                                    speaker:notifyLocalized("oocDelay", delay - math.ceil(lastOOC))
 
-                        return false
-                    end
-                end
-            end
+                                    return false
+                                end
+                            end
+                        end
 
-            -- Save the last time they spoke in OOC.
-            speaker.liaLastOOC = CurTime()
-        end,
-        onChatAdd = function(speaker, text)
-            local icon = "icon16/user.png"
-            icon = Material(hook.Run("GetPlayerIcon", speaker) or icon)
-            chat.AddText(icon, Color(255, 50, 50), " [OOC] ", speaker, color_white, ": " .. text)
-        end,
-        prefix = {"//", "/ooc"},
-        noSpaceAfter = true,
-        filter = "ooc"
-    }
-)
+                        -- Save the last time they spoke in OOC.
+                        speaker.liaLastOOC = CurTime()
+                    end,
+                    onChatAdd = function(speaker, text)
+                        local icon = "icon16/user.png"
+                        icon = Material(hook.Run("GetPlayerIcon", speaker) or icon)
+                        chat.AddText(icon, Color(255, 50, 50), " [OOC] ", speaker, color_white, ": " .. text)
+                    end,
+                    prefix = {"//", "/ooc"},
+                    noSpaceAfter = true,
+                    filter = "ooc"
+                }
+            )
+        end
+    )
+end
