74ab8cb385844bb7beacc9c62180eb28799876ba
Leonheart <33399712+bleonheart@users.noreply.github.com>
2023-07-30 14:57:43 +0100

    Removed Other Languages

--- gamemode/config/sh_config.lua
+++ gamemode/config/sh_config.lua
@@ -228,3 +228,189 @@ end, {
 })
 
 lia.config.squaredVoiceDistance = lia.config.get("voiceDistance", 600) * lia.config.get("voiceDistance", 600)
+
+lia.config.add("StaminaBlur", false, "Whether or not the Stamina Blur shown.", nil, {
+    category = "Client Settings"
+})
+
+lia.config.add("CrosshairEnabled", false, "Enables Crosshair?", nil, {
+    category = "Client Settings"
+})
+
+lia.config.add("DrawEntityShadows", true, "Should Entity Shadows Be Drawn?", nil, {
+    category = "Client Settings"
+})
+
+lia.config.add("vignette", true, "Whether or not the vignette is shown.", nil, {
+    category = "Client Settings"
+})
+
+lia.config.add("BarsDisabled", false, "Whether or not Bars is Disabled.", nil, {
+    category = "Client Settings"
+})
+
+lia.config.add("AmmoDrawEnabled", true, "Whether or not Ammo Draw is enabled.", nil, {
+    category = "Client Settings"
+})
+
+lia.config.add("DarkTheme", false, "Whether or not Dark Theme is enabled.", nil, {
+    category = "Client Settings"
+})
+
+lia.config.add("chatSizeDiff", false, "Whether or not to use different chat sizes.", nil, {
+    category = "Client Settings"
+})
+
+lia.config.add("color", Color(75, 119, 190), "The main color theme for the framework.", nil, {
+    category = "Client Settings"
+})
+
+lia.config.add("font", "Arial", "The font used to display titles.", function(oldValue, newValue)
+    if CLIENT then
+        hook.Run("LoadLiliaFonts", newValue, lia.config.get("genericFont"))
+    end
+end, {
+    category = "Client Settings"
+})
+
+lia.config.add("genericFont", "Segoe UI", "The font used to display generic texts.", function(oldValue, newValue)
+    if CLIENT then
+        hook.Run("LoadLiliaFonts", lia.config.get("font"), newValue)
+    end
+end, {
+    category = "Client Settings"
+})
+
+lia.config.add("fontScale", 1.0, "The scale for the font.", function(oldValue, newValue)
+    if CLIENT then
+        hook.Run("LoadLiliaFonts", lia.config.get("font"), lia.config.get("genericFont"))
+    end
+end, {
+    form = "Float",
+    data = {
+        min = 0.1,
+        max = 2.0
+    },
+    category = "Client Settings"
+})
+
+lia.config.add("maxChars", 5, "The maximum number of characters a player can have.", nil, {
+    data = {
+        min = 1,
+        max = 50
+    },
+    category = "Player Settings"
+})
+
+lia.config.add("spawnTime", 5, "The time it takes to respawn.", nil, {
+    data = {
+        min = 0,
+        max = 10000
+    },
+    category = "Player Settings"
+})
+
+lia.config.add("invW", 6, "How many slots in a row there is in a default inventory.", nil, {
+    data = {
+        min = 0,
+        max = 20
+    },
+    category = "Player Settings"
+})
+
+lia.config.add("invH", 4, "How many slots in a column there is in a default inventory.", nil, {
+    data = {
+        min = 0,
+        max = 20
+    },
+    category = "Player Settings"
+})
+
+lia.config.add("minDescLen", 16, "The minimum number of characters in a description.", nil, {
+    data = {
+        min = 0,
+        max = 300
+    },
+    category = "Player Settings"
+})
+
+lia.config.add("saveInterval", 300, "How often characters save in seconds.", nil, {
+    data = {
+        min = 60,
+        max = 3600
+    },
+    category = "Player Settings"
+})
+
+lia.config.add("walkSpeed", 130, "How fast a player normally walks.", function(oldValue, newValue)
+    for k, v in ipairs(player.GetAll()) do
+        v:SetWalkSpeed(newValue)
+    end
+end, {
+    data = {
+        min = 75,
+        max = 500
+    },
+    category = "Player Settings"
+})
+
+lia.config.add("runSpeed", 235, "How fast a player normally runs.", function(oldValue, newValue)
+    for k, v in ipairs(player.GetAll()) do
+        v:SetRunSpeed(newValue)
+    end
+end, {
+    data = {
+        min = 75,
+        max = 500
+    },
+    category = "Player Settings"
+})
+
+lia.config.add("walkRatio", 0.5, "How fast one goes when holding ALT.", nil, {
+    form = "Float",
+    data = {
+        min = 0,
+        max = 1
+    },
+    category = "Player Settings"
+})
+
+lia.config.add("punchStamina", 10, "How much stamina punches use up.", nil, {
+    data = {
+        min = 0,
+        max = 100
+    },
+    category = "Player Settings"
+})
+
+lia.config.add("defMoney", 0, "The amount of money that players start with.", nil, {
+    category = "Player Settings",
+    data = {
+        min = 0,
+        max = 10000
+    }
+})
+
+lia.config.add("StaffAutoRecognize", true, "Whether or not FACTION_STAFF is globally recognized.", nil, {
+    category = "Player Settings"
+})
+
+lia.config.add("FactionAutoRecognize", false, "Whether or not players from the same faction recognize themselves (if true, please configure the faction list in the module).", nil, {
+    category = "Player Settings"
+})
+
+lia.config.add("allowExistNames", true, "Whether or not players can use an already existing name upon character creation.", nil, {
+    category = "Player Settings"
+})
+
+lia.config.add("strMultiplier", 0.3, "The strength multiplier scale", nil, {
+    form = "Float",
+    data = {
+        min = 0,
+        max = 1.0
+    },
+    category = "Player Settings"
+})
+
+lia.config.language = "english"
+lia.config.itemFormat = "<font=liaGenericFont>%s</font>\n<font=liaSmallFont>%s</font>"
