0ef8f17dae8e4bf47500ad919311662082e03b42
Leonheart <33399712+bleonheart@users.noreply.github.com>
2023-08-11 15:50:53 +0100

    Added Commands

---
+++ gamemode/commands/cl_misc.lua
@@ -0,0 +1,17 @@
+-------------------------------------------------------------------------------------------------------------------------
+lia.command.add("advertisement", {
+    syntax = "<string factions> <string text>",
+    onRun = function(client, arguments) end
+})
+
+-------------------------------------------------------------------------------------------------------------------------
+lia.command.add("announce", {
+    syntax = "<string factions> <string text>",
+    onRun = function(client, arguments) end
+})
+
+-------------------------------------------------------------------------------------------------------------------------
+lia.command.add("checkinventory", {
+    syntax = "<string target>",
+    onRun = function(client, arguments) end
+})
