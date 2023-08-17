04691b711442d20f08012f827cde40543f932e8a
Leonheart <33399712+bleonheart@users.noreply.github.com>
2023-08-18 00:13:01 +0100

    Update sh_permissions.lua

--- modules/bodygrouper/libs/sh_permissions.lua
+++ modules/bodygrouper/libs/sh_permissions.lua
@@ -1,13 +1,13 @@
 --------------------------------------------------------------------------------------------------------
 function MODULE:CanChangeBodygroup(client)
     if CAMI then
-        return CAMI.PlayerHasAccess(client, "Bodygrouper - View Menu", nil)
+        return CAMI.PlayerHasAccess(client, "Characters - Change Bodygroups", nil)
     else
         if lia.config.BodygrouperAdminOnly then return client:IsAdmin() end
     end
     return true
 end
---------------------------------------------------------------------------------------------------------
+--------------------------a------------------------------------------------------------------------------
 function MODULE:CanAccessMenu(client)
     for k, v in pairs(ents.FindByClass("bodygrouper_closet")) do
         if v:GetPos():Distance(client:GetPos()) <= 128 then return true end
