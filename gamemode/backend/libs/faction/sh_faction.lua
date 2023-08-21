98d62da4613f80516de6d777c58bce93e401b139
Leonheart <33399712+bleonheart@users.noreply.github.com>
2023-08-21 09:27:05 +0100

    Added Faction Models

--- gamemode/backend/libs/faction/sh_faction.lua
+++ gamemode/backend/libs/faction/sh_faction.lua
@@ -3,6 +3,31 @@ lia.faction = lia.faction or {}
 lia.faction.indices = lia.faction.indices or {}
 lia.faction.teams = lia.faction.teams or {}
 --------------------------------------------------------------------------------------------------------
+lia.faction.DefaultModels = {
+	"models/humans/group01/male_01.mdl",
+	"models/humans/group01/male_02.mdl",
+	"models/humans/group01/male_04.mdl",
+	"models/humans/group01/male_05.mdl",
+	"models/humans/group01/male_06.mdl",
+	"models/humans/group01/male_07.mdl",
+	"models/humans/group01/male_08.mdl",
+	"models/humans/group01/male_09.mdl",
+	"models/humans/group02/male_01.mdl",
+	"models/humans/group02/male_03.mdl",
+	"models/humans/group02/male_05.mdl",
+	"models/humans/group02/male_07.mdl",
+	"models/humans/group02/male_09.mdl",
+	"models/humans/group01/female_01.mdl",
+	"models/humans/group01/female_02.mdl",
+	"models/humans/group01/female_03.mdl",
+	"models/humans/group01/female_06.mdl",
+	"models/humans/group01/female_07.mdl",
+	"models/humans/group02/female_01.mdl",
+	"models/humans/group02/female_03.mdl",
+	"models/humans/group02/female_06.mdl",
+	"models/humans/group01/female_04.mdl"
+}
+--------------------------------------------------------------------------------------------------------
 function lia.faction.loadFromDir(directory)
     for _, v in ipairs(file.Find(directory .. "/*.lua", "LUA")) do
         local niceName = v:sub(4, -5)
