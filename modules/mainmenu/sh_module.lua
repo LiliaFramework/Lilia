9a048ebbb997f893834d1833629e1ae630676723
Leonheart <33399712+bleonheart@users.noreply.github.com>
2023-08-24 00:56:46 +0100

    Changed File Location

--- modules/mainmenu/sh_module.lua
+++ modules/mainmenu/sh_module.lua
@@ -4,3 +4,4 @@ MODULE.desc = "Allows players to have multiple characters."
 MainMenu = MODULE
 lia.util.include("cl_module.lua")
 lia.util.include("sv_module.lua")
+lia.util.include("sh_config.lua")
