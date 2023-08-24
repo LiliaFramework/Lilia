8b82a02a1bcdb1ec4c9e36d9aa0ac74251b12c4c
Leonheart <33399712+bleonheart@users.noreply.github.com>
2023-08-24 01:41:08 +0100

    Update sh_config.lua

--- modules/mainmenu/sh_config.lua
+++ modules/mainmenu/sh_config.lua
@@ -11,8 +11,8 @@ lia.config.CharWarning = {"friends/friend_join.wav", 40, 255}
 --------------------------------------------------------------------------------------------------------
 --[[ CUSTOM UI STUFF]]
 lia.config.CustomUIBackgrounds = {lia.util.getMaterial('ui/bg1.png'), lia.util.getMaterial('ui/bg2.png'), lia.util.getMaterial('ui/bg3.png'), lia.util.getMaterial('ui/bg4.png'), lia.util.getMaterial('ui/bg5.png')}
-lia.config.WebsiteURL = 'https://colonial.noclip.me/'
-lia.config.DiscordURL = 'https://discord.gg/yBZ9NT66ch'
-lia.config.ContentURL = 'https://steamcommunity.com/sharedfiles/filedetails/?id=2786863009'
+lia.config.WebsiteURL = 'https://discord.gg/77JtZx2FCq'
+lia.config.DiscordURL = 'https://discord.gg/77JtZx2FCq'
+lia.config.ContentURL = 'https://steamcommunity.com/sharedfiles/filedetails/?id=2959728255'
 lia.config.CustomUIEnabled = false
 --------------------------------------------------------------------------------------------------------
