a0724af6e27f9d02c64f7551dee35ef4077ccfca
Leonheart <33399712+bleonheart@users.noreply.github.com>
2023-08-31 08:56:56 +0100

    Added PKToggle as per request

--- plugins/misc/plugins/permakill/sh_config.lua
+++
@@ -1,7 +0,0 @@
-lia.config.add("pkActive", false, "Whether or not permakill is activated on the server.", nil, {
-    category = "Server Settings"
-})
-
-lia.config.add("pkWorld", false, "Whether or not world and self damage produce permanent death.", nil, {
-    category = "Server Settings"
-})
