bd897b6f1dea7ba8ce269a02f17b5acace9124f2
Leonheart <33399712+bleonheart@users.noreply.github.com>
2023-09-08 17:09:19 +0100

    Fixed Bugs

--- gamemode/config.lua
+++ gamemode/config.lua
@@ -2,17 +2,6 @@
 lia.config = lia.config or {}
 --------------------------------------------------------------------------------------------------------
 function GM:LoadConfigValues()
-    if not lia.config.LoadedSchemaCoreConfig then
-        print("CONFIG: Loaded Default Core Config")
-    else
-        print("CONFIG: Loaded Schema Core Config")
-    end
-
-    if not lia.config.LoadedSchemaTposeModelConfig then
-        print("CONFIG: Loaded Default TPose Fixer!")
-    else
-        print("CONFIG: Loaded Schema Model Fixer Config")
-    end
     self:LoadCoreConfig()
     self:LoadModelsConfig()
     hook.Run("InitializedConfig")
