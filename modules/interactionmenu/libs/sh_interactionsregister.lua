4685e9f49ca56ff7ae1c89a98ab737d5df4bae8b
Leonheart <33399712+bleonheart@users.noreply.github.com>
2023-09-13 00:47:33 +0100

    Update sh_interactionsregister.lua

--- modules/interactionmenu/libs/sh_interactionsregister.lua
+++ modules/interactionmenu/libs/sh_interactionsregister.lua
@@ -1,10 +1,12 @@
 ----------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+lia.config.PlayerInteractionOptions = lia.config.PlayerInteractionOptions or {}
+----------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
 for name, vars in pairs(lia.config.PlayerInteractionOptions) do
     lia.playerInteract.addFunc(
         vars.name,
         {
             name = name,
-            Callback = function(client, target) netstream.Start(vars.Identifier, client, target) end,
+            Callback = vars.Callback,
             CanSee = vars.CanSee,
         }
     )
