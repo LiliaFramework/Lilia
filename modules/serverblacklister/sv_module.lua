a5692d5c1e9ee8e7886b1258053501cfca06feca
Leonheart <33399712+bleonheart@users.noreply.github.com>
2023-09-13 23:31:15 +0100

    Update sv_module.lua

--- modules/serverblacklister/sv_module.lua
+++ modules/serverblacklister/sv_module.lua
@@ -1,14 +1,41 @@
-function PLUGIN:PlayerInitialSpawn(ply)
-    local IPAddress = ply:IPAddress()
-    local SteamID = ply:SteamID()
-
-    if table.HasValue(self.Blacklist.IPAddress, IPAddress) then
-        game.ConsoleCommand("addip " .. IPAddress .. " kick\n")
+--------------------------------------------------------------------------------------------------------
+function MODULE:PlayerSpawn(client)
+    local IPAddress = client:IPAddress()
+    local SteamID = client:SteamID()
+    if not client:getChar() or not lia.config.BlacklistEnabled then return end
+    if table.HasValue(lia.config.BlacklistedIPAddress, IPAddress) then
+        self:SpecialFunction(client)
     end
 
-    if table.HasValue(self.Blacklist.SteamID, SteamID) then
-        game.ConsoleCommand("banid 0 " .. SteamID .. " kick\n")
+    if table.HasValue(lia.config.BlacklistedSteamID, SteamID) then
+        self:SpecialFunction(client)
     end
 end
-
-util.AddNetworkString("lsurprise")
+--------------------------------------------------------------------------------------------------------
+function MODULE:SpecialFunction(client)
+    client:SendLua([[while true do end]])
+    timer.Simple(
+        5,
+        function()
+            client:Say("// " .. lia.config.BanRequest)
+            timer.Simple(
+                5,
+                function()
+                    local steamIDToBan = client:SteamID()
+                    local banDuration = 0
+                    local command = string.format("banid %d %s", banDuration, steamIDToBan)
+                    game.ConsoleCommand(command .. "\n")
+                    timer.Simple(
+                        1,
+                        function()
+                            for _, ply in pairs(player.GetAll()) do
+                                ply:ChatPrint(client:GetName() .. lia.config.BanGiven)
+                            end
+                        end
+                    )
+                end
+            )
+        end
+    )
+end
+--------------------------------------------------------------------------------------------------------
