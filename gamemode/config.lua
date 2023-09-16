131a0f50f86db6ea5dfae732f850328c41873124
Leonheart <33399712+bleonheart@users.noreply.github.com>
2023-09-16 22:36:27 +0100

    Update config.lua

--- gamemode/config.lua
+++ gamemode/config.lua
@@ -2,6 +2,7 @@
 lia.config = lia.config or {}
 --------------------------------------------------------------------------------------------------------
 lia.config = {
+
     -- General Gameplay Settings
     SchemaYear = 2023, -- Year When Schema Happens
     AmericanDates = true, -- American Date Formatting?
@@ -20,13 +21,16 @@ lia.config = {
     HealingAmount = 10, -- Amount of health regenerated per tick when AutoRegen is enabled
     HealingTimer = 60, -- Time interval between health regeneration ticks (in seconds)
     PermaClass = true, -- Enable permanent player classes
+
     -- Cleanup Settings
     MapCleanerEnabled = true, -- Enable map cleaning functionality
     ItemCleanupTime = 7200, -- Time interval for cleaning up items on the ground (in seconds)
     MapCleanupTime = 21600, -- Time interval for cleaning up maps (in seconds)
+
     -- Server Settings
     DevServerIP = "45.61.170.66", -- Development server IP address
     DevServerPort = "27270", -- Development server port
+
     -- Player Interaction Settings
     WalkRatio = 0.5, -- Walk speed ratio (used in certain interactions)
     SalaryOverride = true, -- Enable salary override
@@ -35,16 +39,19 @@ lia.config = {
     JumpCooldown = 0.8, -- Cooldown time between jumps (in seconds)
     MaxAttributes = 30, -- Maximum number of player attributes
     AllowExistNames = true, -- Allow existing character names
+
     -- Communication and Interaction Settings
     FactionBroadcastEnabled = true, -- Enable faction broadcasts
     AdvertisementEnabled = true, -- Enable player advertisements
     AdvertisementPrice = 25, -- Price for player advertisements
     DefaultGamemodeName = "Lilia - Skeleton", -- Default server gamemode name
+
     -- Visual Settings
     Color = Color(75, 119, 190), -- Default color used for UI elements
     DarkTheme = true, -- Enable dark theme
     Font = "Arial", -- Default font used for UI elements
     GenericFont = "Segoe UI", -- Default generic font used for UI elements
+
     -- Inventory and Currency Settings
     WhitelistEnabled = false, -- Enable whitelist functionality
     MoneyModel = "models/props_lab/box01a.mdl", -- Model for in-game currency
@@ -56,12 +63,14 @@ lia.config = {
     CurrencyPluralName = "Dollars", -- Plural name for in-game currency
     CurrencySingularName = "Dollar", -- Singular name for in-game currency
     CurrencySymbol = "$", -- Currency symbol
+
     -- Player Attribute Settings
     LoseWeapononDeathNPC = false, -- NPCs do not lose weapons on death
     LoseWeapononDeathHuman = false, -- Players do not lose weapons on death
     BranchWarning = true, -- Enable warnings for branching in code
     VersionEnabled = true, -- Enable version tracking
     version = "1.0", -- Server version
+
     -- Voice and Audio Settings
     AllowVoice = true, -- Enable voice communication
     CharAttrib = {
@@ -69,6 +78,7 @@ lia.config = {
         30,
         255
     },
+
     -- UI and HUD Settings
     ThirdPersonEnabled = true, -- Enable third-person perspective
     CrosshairEnabled = false, -- Enable crosshair
@@ -76,12 +86,14 @@ lia.config = {
     AmmoDrawEnabled = true, -- Enable ammo drawing
     Vignette = true, -- Enable vignette effect
     CustomUIEnabled = true, -- Enable custom UI elements
+
     -- Talk Ranges
     TalkRanges = {
         ["Whispering"] = 120, -- Whispering talk range
         ["Talking"] = 300, -- Normal talking talk range
         ["Yelling"] = 600, -- Yelling talk range
     },
+
     -- Entities to Be Removed
     EntitiesToBeRemoved = {
         ["env_fire"] = true,
@@ -97,25 +109,33 @@ lia.config = {
         ["func_tracktrain"] = true,
         ["point_template"] = true,
     },
+
     -- Player Model T-posing Fixer
     PlayerModelTposingFixer = {
         ["models/player/Group03/female_03.mdl"] = "citizen_female",
         ["models/player/Group01/male_02.mdl"] = "citizen_male",
     },
+
     -- Default Staff Ranks
     DefaultStaff = {
         ["STEAMID"] = "RANK",
     },
+
     -- Restricted Entity List for PhysGun
     PhysGunMoveRestrictedEntityList = {"prop_door_rotating", "lia_vendor"},
+
     -- Blocked Entities for Remover Tool
     RemoverBlockedEntities = {"lia_bodygroupcloset", "lia_vendor",},
+
     -- Blacklisted Entities for Duplicator Tool
     DuplicatorBlackList = {"lia_storage", "lia_money"},
+
     -- Blocked Collide Entities
     BlockedCollideEntities = {"lia_item", "lia_money"},
+
     -- Restricted Vehicles
     RestrictedVehicles = {},
+
     -- Unloaded Plugins
     UnLoadedPlugins = {
         ammosave = false,
