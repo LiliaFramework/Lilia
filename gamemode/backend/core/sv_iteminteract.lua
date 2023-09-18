587a5784b1c56aa2bc57bbc35993371540c21b7b
Leonheart <33399712+bleonheart@users.noreply.github.com>
2023-09-18 18:35:42 +0100

    Added CanPlayerEquipItem

--- gamemode/backend/core/sv_iteminteract.lua
+++ gamemode/backend/core/sv_iteminteract.lua
@@ -10,24 +10,29 @@ function GM:CanPlayerInteractItem(client, action, item)
     if client:getNetVar("restricted") then return false end
     if action == "drop" and hook.Run("CanPlayerDropItem", client, item) == false then return false end
     if action == "take" and hook.Run("CanPlayerTakeItem", client, item) == false then return false end
+    if action == "equip" and hook.Run("CanPlayerEquipItem", client, item) == false then return false end
     if not client:Alive() or client:getLocalVar("ragdoll") then return false end
     if inventory and (inventory.isBag == true or inventory.isBank == true) and self.BagForbiddenActions[action] then return false, "forbiddenActionStorage" end
 end
-
+--------------------------------------------------------------------------------------------------------
+function GM:CanPlayerEquipItem(client, item)
+    if IsValid(item.entity) then
+        client:notify("You can't equip items that aren't on your inventory!")
+        return false
+    end
+    return true
+end
 --------------------------------------------------------------------------------------------------------
 function GM:CanPlayerTakeItem(client, item)
     if IsValid(item.entity) then
         local char = client:getChar()
         if item.entity.SteamID64 == client:SteamID() and item.entity.liaCharID ~= char:getID() then
             client:notifyLocalized("playerCharBelonging")
-
             return false
         end
     end
-
     return true
 end
-
 --------------------------------------------------------------------------------------------------------
 function GM:CanPlayerDropItem(client, item)
     if item.isBag then
@@ -37,13 +42,11 @@ function GM:CanPlayerDropItem(client, item)
             for _, otheritem in pairs(items) do
                 if not otheritem.ignoreEquipCheck and otheritem:getData("equip") == true then
                     client:notifyLocalized("cantDropBagHasEquipped")
-
                     return false
                 end
             end
         end
     end
-
     return true
 end
 --------------------------------------------------------------------------------------------------------
