c3d5e7cb28ab5cd5f2d3b03d1e0ac56aed9c53d6
Leonheart <33399712+bleonheart@users.noreply.github.com>
2023-09-19 12:05:37 +0100

    Added Dev HUD

---
+++ modules/devhud/sh_module.lua
@@ -0,0 +1,8 @@
+--------------------------------------------------------------------------------------------------------
+MODULE.name = "In-Development Hud"
+MODULE.author = "STEAM_0:1:176123778"
+MODULE.desc = "Implements a small hud for players to see at all times, admins will have access to a dev varient as well."
+--------------------------------------------------------------------------------------------------------
+lia.util.include("sv_module.lua")
+lia.util.include("cl_module.lua")
+--------------------------------------------------------------------------------------------------------
---
+++ modules/devhud/sv_module.lua
@@ -0,0 +1,15 @@
+CAMI.RegisterPrivilege(
+    {
+        Name = "Lilia - Management - Development HUD",
+        MinAccess = "superadmin",
+        Description = "Allows access to Development HUD.",
+    }
+)
+
+CAMI.RegisterPrivilege(
+    {
+        Name = "Lilia - Management - Staff HUD",
+        MinAccess = "superadmin",
+        Description = "Allows access to Staff HUD.",
+    }
+)
