fa017eeb75676213a246c46a2536ff27c6800484
Leonheart <33399712+bleonheart@users.noreply.github.com>
2023-09-23 21:47:08 +0100

    Updated a few things

--- modules/cmenu/libs/sh_callback.lua
+++
@@ -1,7 +0,0 @@
-----------------------------------------------------------------------------------------------
-function ExecuteCallback(callback, client, target)
-    if type(callback) == "function" then
-        callback(client, target)
-    end
-end
-----------------------------------------------------------------------------------------------
--- modules/cmenu/sv_module.lua
+++
@@ -1,7 +0,0 @@
-----------------------------------------------------------------------------------------------
-local netcalls = {"cmenu", "liaApproveSearch", "liaRequestSearch", "blindfold", "gag_player", "vehicle_usage", "cmenu_tying", "FF", "liaRequestID", "liaApproveID"}
-----------------------------------------------------------------------------------------------
-for k, v in pairs(netcalls) do
-    util.AddNetworkString(v)
-end
-----------------------------------------------------------------------------------------------
