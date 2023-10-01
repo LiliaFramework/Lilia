63038da2747cba99ac34adb4b94c9139d21b2db9
Leonheart <33399712+bleonheart@users.noreply.github.com>
2023-10-01 11:56:49 +0100

    Implemented Multi Language Support

--- gamemode/backend/libs/language/sv_language.lua
+++ gamemode/backend/libs/language/sv_language.lua
@@ -5,7 +5,6 @@ function L(key, client, ...)
     local languages = lia.lang.stored
     local langKey = ClientGetInfo(client, "lia_language")
     local info = languages[langKey] or languages.english
-
     return string.format(info and info[key] or key, ...)
 end
 
@@ -22,7 +21,6 @@ function L3(key, langKey, ...)
     local languages = lia.lang.stored
     if langKey then
         local info = languages[langKey] or languages.english
-
         return string.format(info and info[key] or key, ...)
     else
         return key
