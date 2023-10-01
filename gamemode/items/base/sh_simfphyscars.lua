0932fe8245916798b4c5d89936e1e7edd3d09f3f
Leonheart <33399712+bleonheart@users.noreply.github.com>
2023-10-01 11:55:29 +0100

    Updated a ton of items

---
+++ gamemode/items/base/sh_simfphyscars.lua
@@ -0,0 +1,18 @@
+--------------------------------------------------------------------------------------------------------
+ITEM.name = "Vehicles Simphys Base"
+ITEM.model = ""
+ITEM.description = ""
+ITEM.category = "Vehicles"
+ITEM.vehicleid = ""
+--------------------------------------------------------------------------------------------------------
+ITEM.functions.Place = {
+    onRun = function(itemTable)
+        local client = itemTable.player
+        local data = {}
+        data.start = client:GetShootPos()
+        data.endpos = data.start + client:GetAimVector() * 96
+        data.filter = client
+        local V = simfphys.SpawnVehicleSimple(itemTable.vehicleid, Vector(data.endpos), Angle(1, 1, 1))
+    end
+}
+--------------------------------------------------------------------------------------------------------
