a7e3acf6971e8a5d9c0a70520cd3755b7577e4d1
Leonheart <33399712+bleonheart@users.noreply.github.com>
2023-10-16 23:28:15 +0100

    Delayed Base Loading && Added a simfphys check

--- lilia/gamemode/backend/core/hooks/sh_hooks.lua
+++ lilia/gamemode/backend/core/hooks/sh_hooks.lua
@@ -458,11 +458,13 @@ end
 
 --------------------------------------------------------------------------------------------------------------------------
 function GM:InitializedExtrasShared()
-    RunConsoleCommand("sv_simfphys_gib_lifetime", "0")
-    RunConsoleCommand("sv_simfphys_fuel", "0")
-    RunConsoleCommand("sv_simfphys_teampassenger", "0")
-    RunConsoleCommand("sv_simfphys_traction_snow", "1")
-    RunConsoleCommand("sv_simfphys_damagemultiplicator", "100")
+    if simfphys then
+        RunConsoleCommand("sv_simfphys_gib_lifetime", "0")
+        RunConsoleCommand("sv_simfphys_fuel", "0")
+        RunConsoleCommand("sv_simfphys_teampassenger", "0")
+        RunConsoleCommand("sv_simfphys_traction_snow", "1")
+        RunConsoleCommand("sv_simfphys_damagemultiplicator", "100")
+    end
 end
 
 --------------------------------------------------------------------------------------------------------------------------
