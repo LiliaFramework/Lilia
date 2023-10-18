7cb3a2463d6a418788aed79303916650e0ff67ae
Leonheart <33399712+bleonheart@users.noreply.github.com>
2023-10-19 00:55:31 +0100

    Update sv_bots.lua

--- lilia/gamemode/backend/core/sv_bots.lua
+++ lilia/gamemode/backend/core/sv_bots.lua
@@ -1,14 +1,12 @@
 --------------------------------------------------------------------------------------------------------------------------
 function GM:SetupBotCharacter(client)
     local botID = os.time()
-    local index = math.random(1, table.Count(lia.faction.indices))
-    local faction = lia.faction.indices[index]
     local character = lia.char.new(
         {
             name = client:Name(),
             desc = "This is a bot. BotID is " .. botID .. ".",
-            faction = faction and faction.uniqueID or "unknown",
-            model = faction and table.Random(faction.models) or "models/gman.mdl"
+            faction = "unknown",
+            model = "models/gman.mdl"
         }, botID, client, client:SteamID64()
     )
 
