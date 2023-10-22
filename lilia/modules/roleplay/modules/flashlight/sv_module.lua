e3247e3cd409e276283e563289a02aaffbd92dff
Leonheart <33399712+bleonheart@users.noreply.github.com>
2023-10-23 00:41:57 +0100

    Tweaked Flashlight

--- lilia/modules/roleplay/modules/flashlight/sv_module.lua
+++ lilia/modules/roleplay/modules/flashlight/sv_module.lua
@@ -1,26 +1,8 @@
 --------------------------------------------------------------------------------------------------------------------------
 function MODULE:PlayerSwitchFlashlight(client, enabled)
-    if not (lia.config.flashlightenabled and client:getChar())  then return false end
-    if lia.config.FlashlightItemRequired ~= nil then
-        if not client:getChar():getInv():hasItem(lia.config.FlashlightItemRequired) then
-            return false
-        else
-            if (client.FlashlightEnable or 0) > CurTime() and enabled then
-                return false
-            else
-                client.FlashlightEnable = CurTime() + lia.config.FlashlightToggleCooldown
+    if not (lia.config.flashlightenabled or client:getChar()) then return false end
+    if lia.config.FlashlightItemRequired ~= nil and not client:getChar():getInv():hasItem(lia.config.FlashlightItemRequired) then return false end
 
-                return true
-            end
-        end
-    else
-        if (client.FlashlightEnable or 0) > CurTime() and enabled then
-            return false
-        else
-            client.FlashlightEnable = CurTime() + lia.config.FlashlightToggleCooldown
-
-            return true
-        end
-    end
+    return true
 end
 --------------------------------------------------------------------------------------------------------------------------
