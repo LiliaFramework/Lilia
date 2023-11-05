31e285825397c23c746ab4ebb3f8d3d0c9eb61e0
Leonheart <33399712+bleonheart@users.noreply.github.com>
2023-11-05 03:11:12 -0800

    Updated Some extra Permissions & Disabled Default SAM Restrictions

--- lilia/gamemode/backend/core/sv_permissions.lua
+++ lilia/gamemode/backend/core/sv_permissions.lua
@@ -1,49 +1,62 @@
 --------------------------------------------------------------------------------------------------------------------------
 function GM:PlayerSpawnNPC(client)
-    if CAMI.PlayerHasAccess(client, "Lilia - Spawn Permissions - Can Spawn NPCs", nil) or client:getChar():hasFlags("n") then return true end
+    if client:getChar() and AMI.PlayerHasAccess(client, "Lilia - Spawn Permissions - Can Spawn NPCs", nil) or client:getChar():hasFlags("n") then return true end
+    return false
 end
 
 --------------------------------------------------------------------------------------------------------------------------
-function GM:PlayerSpawnProp(client)
-    print(client:Name() .. " tried to spawn a prop!")
-    if CAMI.PlayerHasAccess(client, "Lilia - Spawn Permissions - Can Spawn Props", nil) then
-        print("He has CAMI permission for that!")
-    else
-        print("He doesnt have CAMI permission for that!")
+function GM:PlayerSpawnProp(client, model)
+    local nextSpawnTime = client.NextSpawn or 0
+    if (self:CheckSpawnPropBlackList(client, model) and client:getChar()) and CAMI.PlayerHasAccess(client, "Lilia - Spawn Permissions - Can Spawn Props", nil) or client:getChar():hasFlags("e") then
+        if CAMI.PlayerHasAccess(client, "Lilia - Spawn Permissions - No Spawn Delay") and (client.AdvDupe2 and client.AdvDupe2.Pasting) then return true end
+        if nextSpawnTime < CurTime() then
+            client.NextSpawn = CurTime() + 0.75
+            return true
+        else
+            client:notify("You can't spawn props that fast!")
+            return false
+        end
     end
-
-    if client:getChar():hasFlags("e") then
-        print("He has flag permission for that!")
-    else
-        print("He doesnt have flag permission for that!")
-    end
-
-    if CAMI.PlayerHasAccess(client, "Lilia - Spawn Permissions - Can Spawn Props", nil) or client:getChar():hasFlags("e") then return true end
+    return false
 end
 
 --------------------------------------------------------------------------------------------------------------------------
 function GM:PlayerSpawnRagdoll(client)
-    if CAMI.PlayerHasAccess(client, "Lilia - Spawn Permissions - Can Spawn Ragdolls", nil) or client:getChar():hasFlags("r") then return true end
+    if client:getChar() and CAMI.PlayerHasAccess(client, "Lilia - Spawn Permissions - Can Spawn Ragdolls", nil) or client:getChar():hasFlags("r") then
+        if CAMI.PlayerHasAccess(client, "Lilia - Spawn Permissions - No Spawn Delay") and (client.AdvDupe2 and client.AdvDupe2.Pasting) then return true end
+        if nextSpawnTime < CurTime() then
+            client.NextSpawn = CurTime() + 0.75
+            return true
+        else
+            client:notify("You can't spawn ragdolls that fast!")
+            return false
+        end
+        return true
+    end
+    return false
 end
 
 --------------------------------------------------------------------------------------------------------------------------
 function GM:PlayerSpawnSWEP(client)
-    if CAMI.PlayerHasAccess(client, "Lilia - Spawn Permissions - Can Spawn SWEPs", nil) or client:getChar():hasFlags("W") then return true end
+    if client:getChar() and CAMI.PlayerHasAccess(client, "Lilia - Spawn Permissions - Can Spawn SWEPs", nil) or client:getChar():hasFlags("W") then return true end
+    return false
 end
 
 --------------------------------------------------------------------------------------------------------------------------
 function GM:PlayerSpawnEffect(client)
-    if CAMI.PlayerHasAccess(client, "Lilia - Spawn Permissions - Can Spawn Effects", nil) or client:getChar():hasFlags("L") then return true end
+    if client:getChar() and CAMI.PlayerHasAccess(client, "Lilia - Spawn Permissions - Can Spawn Effects", nil) or client:getChar():hasFlags("L") then return true end
+    return false
 end
 
 --------------------------------------------------------------------------------------------------------------------------
 function GM:PlayerSpawnSENT(client)
-    if CAMI.PlayerHasAccess(client, "Lilia - Spawn Permissions - Can Spawn SENTs", nil) or client:getChar():hasFlags("E") then return true end
+    if client:getChar() and CAMI.PlayerHasAccess(client, "Lilia - Spawn Permissions - Can Spawn SENTs", nil) or client:getChar():hasFlags("E") then return true end
+    return false
 end
 
 --------------------------------------------------------------------------------------------------------------------------
 function GM:PlayerSpawnVehicle(client, model, name, data)
-    if client:getChar():hasFlags("C") or CAMI.PlayerHasAccess(client, "Lilia - Spawn Permissions - Can Spawn Cars", nil) then
+    if client:getChar() and client:getChar():hasFlags("C") or CAMI.PlayerHasAccess(client, "Lilia - Spawn Permissions - Can Spawn Cars", nil) then
         if table.HasValue(lia.config.RestrictedVehicles, name) then
             if CAMI.PlayerHasAccess(client, "Lilia - Spawn Permissions - Can Spawn Restricted Cars", nil) then
                 return true
@@ -54,40 +67,66 @@ function GM:PlayerSpawnVehicle(client, model, name, data)
             return true
         end
     end
+    return false
 end
 
 --------------------------------------------------------------------------------------------------------------------------
 function GM:CanTool(client, trace, tool)
     local privilege = "Lilia - Staff Permissions - Access Tool " .. tool:gsub("^%l", string.upper)
     local entity = client:GetTracedEntity()
-    if IsValid(entity) and (client:getChar():hasFlags("t") or CAMI.PlayerHasAccess(client, privilege, nil)) then
+    if client:getChar() and IsValid(entity) and (client:getChar():hasFlags("t") or CAMI.PlayerHasAccess(client, privilege, nil)) then
         if tool == "permaprops" and not string.StartWith(entity:GetClass(), "lia_") or (tool == "advdupe2" and not table.HasValue(lia.config.DuplicatorBlackList, entity:GetClass())) then return true end
         if tool == "remover" and table.HasValue(lia.config.RemoverBlockedEntities, entity:GetClass()) then return CAMI.PlayerHasAccess(client, "Lilia - Staff Permissions - Can Remove Blocked Entities", nil) end
     end
+    return false
 end
 
 --------------------------------------------------------------------------------------------------------------------------
 function GM:PhysgunPickup(client, entity)
-    if entity:GetCreator() == client and entity:GetClass() == "prop_physics" then
+    if client:getChar() and entity:GetCreator() == client and entity:GetClass() == "prop_physics" then
         return true
-    elseif CAMI.PlayerHasAccess(client, "Lilia - Staff Permissions - Physgun Pickup", nil) then
+    elseif client:getChar() and CAMI.PlayerHasAccess(client, "Lilia - Staff Permissions - Physgun Pickup", nil) then
         if table.HasValue(lia.config.PhysGunMoveRestrictedEntityList, entity:GetClass()) then
             return CAMI.PlayerHasAccess(client, "Lilia - Staff Permissions - Physgun Pickup on Restricted Entities", nil)
         elseif entity:IsVehicle() then
             return CAMI.PlayerHasAccess(client, "Lilia - Staff Permissions - Physgun Pickup on Vehicles", nil)
         end
     end
+    return false
 end
 
 --------------------------------------------------------------------------------------------------------------------------
 function GM:CanProperty(client, property, entity)
-    if CAMI.PlayerHasAccess(client, "Lilia - Staff Permissions - Access Tool " .. property:gsub("^%l", string.upper), nil) then
+    if client:getChar() and CAMI.PlayerHasAccess(client, "Lilia - Staff Permissions - Access Tool " .. property:gsub("^%l", string.upper), nil) then
         if table.HasValue(lia.config.RemoverBlockedEntities, entity:GetClass()) or table.HasValue(lia.config.PhysGunMoveRestrictedEntityList, entity:GetClass()) then
             return CAMI.PlayerHasAccess(client, "Lilia - Staff Permissions - Use Entity Properties on Blocked Entities", nil)
         else
             return true
         end
     end
+    return false
+end
+
+--------------------------------------------------------------------------------------------------------------------------
+function GM:CheckSpawnPropBlackList(client, model)
+    for _, gredwitch in pairs(file.Find("models/gredwitch/bombs/*.mdl", "GAME")) do
+        if model == "models/gredwitch/bombs/" .. gredwitch then return false end
+    end
+
+    for _, gbombs in pairs(file.Find("models/gbombs/*.mdl", "GAME")) do
+        if model == "models/gbombs/" .. gbombs then return false end
+    end
+
+    for _, phx in pairs(file.Find("models/props_phx/*.mdl", "GAME")) do
+        if model == "models/props_phx/" .. phx then return false end
+    end
+
+    for _, mikeprops in pairs(file.Find("models/mikeprops/*.mdl", "GAME")) do
+        if model == "models/mikeprops/" .. mikeprops then return false end
+    end
+
+    if table.HasValue(lia.config.BlackListedProps, model:lower()) then return false end
+    return true
 end
 
 --------------------------------------------------------------------------------------------------------------------------
--- lilia/gamemode/backend/core/sv_protection.lua
+++ lilia/gamemode/backend/core/sv_protection.lua
@@ -1,18 +1,8 @@
 --------------------------------------------------------------------------------------------------------------------------
 function GM:OnPlayerDropWeapon(client, item, entity)
     local physObject = entity:GetPhysicsObject()
-    if physObject then
-        physObject:EnableMotion()
-    end
-
-    timer.Simple(
-        lia.config.TimeUntilDroppedSWEPRemoved,
-        function()
-            if entity and entity:IsValid() then
-                entity:Remove()
-            end
-        end
-    )
+    if physObject then physObject:EnableMotion() end
+    timer.Simple(lia.config.TimeUntilDroppedSWEPRemoved, function() if entity and entity:IsValid() then entity:Remove() end end)
 end
 
 --------------------------------------------------------------------------------------------------------------------------
@@ -22,25 +12,13 @@ end
 
 --------------------------------------------------------------------------------------------------------------------------
 function GM:OnEntityCreated(entity)
-    if lia.config.DrawEntityShadows then
-        entity:DrawShadow(false)
-    end
-
+    if lia.config.DrawEntityShadows then entity:DrawShadow(false) end
     if entity:GetClass() == "prop_vehicle_prisoner_pod" then
         entity:AddEFlags(EFL_NO_THINK_FUNCTION)
         entity.nicoSeat = true
     end
 
-    if entity:IsWidget() then
-        hook.Add(
-            "PlayerTick",
-            "GODisableEntWidgets2",
-            function(entity, n)
-                widgets.PlayerTick(entity, n)
-            end
-        )
-    end
-
+    if entity:IsWidget() then hook.Add("PlayerTick", "GODisableEntWidgets2", function(entity, n) widgets.PlayerTick(entity, n) end) end
     if not entity:IsRagdoll() then return end
     if entity:getNetVar("player", nil) then return end
     timer.Simple(
@@ -68,10 +46,7 @@ end
 --------------------------------------------------------------------------------------------------------------------------
 function GM:PlayerSpawnedVehicle(client, entity)
     local delay = lia.config.PlayerSpawnVehicleDelay
-    if not client:IsSuperAdmin() then
-        client.NextVehicleSpawn = SysTime() + delay
-    end
-
+    if not client:IsSuperAdmin() then client.NextVehicleSpawn = SysTime() + delay end
     self:PlayerSpawnedEntity(client, entity)
 end
 
@@ -90,16 +65,12 @@ function GM:OnPhysgunFreeze(weapon, physObj, entity, client)
     client:AddFrozenPhysicsObject(entity, physObj)
     client:SendHint("PhysgunUnfreeze", 0.3)
     client:SuppressHint("PhysgunFreeze")
-
     return true
 end
 
 --------------------------------------------------------------------------------------------------------------------------
 function GM:PlayerSpawnedNPC(client, entity)
-    if lia.config.NPCsDropWeapons then
-        entity:SetKeyValue("spawnflags", "8192")
-    end
-
+    if lia.config.NPCsDropWeapons then entity:SetKeyValue("spawnflags", "8192") end
     self:PlayerSpawnedEntity(client, entity)
 end
 
@@ -110,9 +81,7 @@ function GM:PlayerDisconnected(client)
     if character then
         local charEnts = character:getVar("charEnts") or {}
         for _, v in ipairs(charEnts) do
-            if v and IsValid(v) then
-                v:Remove()
-            end
+            if v and IsValid(v) then v:Remove() end
         end
 
         hook.Run("OnCharDisconnect", client, character)
@@ -127,54 +96,19 @@ function GM:PlayerDisconnected(client)
 
     lia.char.cleanUpForPlayer(client)
     for _, entity in pairs(ents.GetAll()) do
-        if entity:GetCreator() == client then
-            entity:Remove()
-        end
+        if entity:GetCreator() == client then entity:Remove() end
     end
 end
 
 --------------------------------------------------------------------------------------------------------------------------
 function GM:OnPhysgunPickup(client, entity)
-    if entity:GetClass() == "prop_physics" and entity:GetCollisionGroup() == COLLISION_GROUP_NONE then
-        entity:SetCollisionGroup(COLLISION_GROUP_PASSABLE_DOOR)
-    end
-end
-
---------------------------------------------------------------------------------------------------------------------------
-function GM:PlayerSpawnObject(client, model, skin)
-    local bEditLimit = ents.GetEdictCount() >= 7900
-    if bEditLimit then
-        ErrorNoHalt(string.format("[Lilia] %s attempted to spawn an entity but edict limit is too high!\n", client:Name()))
-        client:notify("The server is too close to the edict limit to spawn this!")
-
-        return false
-    end
-
-    if client:IsSuperAdmin() then return true end
-    if (client.liaNextSpawn or 0) < CurTime() then
-        if client.AdvDupe2 and client.AdvDupe2.Pasting then
-            client.liaNextSpawn = CurTime() + 5
-        else
-            client.liaNextSpawn = CurTime() + 0.75
-        end
-    else
-        if client.AdvDupe2 and client.AdvDupe2.Pasting then return true end
-
-        return false
-    end
+    if entity:GetClass() == "prop_physics" and entity:GetCollisionGroup() == COLLISION_GROUP_NONE then entity:SetCollisionGroup(COLLISION_GROUP_PASSABLE_DOOR) end
 end
 
 --------------------------------------------------------------------------------------------------------------------------
 function GM:PhysgunDrop(client, entity)
     if entity:GetClass() ~= "prop_physics" then return end
-    timer.Simple(
-        5,
-        function()
-            if IsValid(entity) and entity:GetCollisionGroup() == COLLISION_GROUP_PASSABLE_DOOR then
-                entity:SetCollisionGroup(COLLISION_GROUP_NONE)
-            end
-        end
-    )
+    timer.Simple(5, function() if IsValid(entity) and entity:GetCollisionGroup() == COLLISION_GROUP_PASSABLE_DOOR then entity:SetCollisionGroup(COLLISION_GROUP_NONE) end end)
 end
 
 --------------------------------------------------------------------------------------------------------------------------
@@ -194,38 +128,6 @@ end
 
 --------------------------------------------------------------------------------------------------------------------------
 function GM:PlayerSpawnedProp(client, model, entity)
-    for _, gredwitch in pairs(file.Find("models/gredwitch/bombs/*.mdl", "GAME")) do
-        if model == "models/gredwitch/bombs/" .. gredwitch then
-            entity:Remove()
-
-            return
-        end
-    end
-
-    for _, gbombs in pairs(file.Find("models/gbombs/*.mdl", "GAME")) do
-        if model == "models/gbombs/" .. gbombs then
-            entity:Remove()
-
-            return
-        end
-    end
-
-    for _, phx in pairs(file.Find("models/props_phx/*.mdl", "GAME")) do
-        if model == "models/props_phx/" .. phx then
-            entity:Remove()
-
-            return
-        end
-    end
-
-    for _, mikeprops in pairs(file.Find("models/mikeprops/*.mdl", "GAME")) do
-        if model == "models/mikeprops/" .. mikeprops then
-            entity:Remove()
-
-            return
-        end
-    end
-
     self:PlayerSpawnedEntity(client, entity)
 end
 
