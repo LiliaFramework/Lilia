451886e4061969f829602d9ea69d39b992c74164
Leonheart <33399712+bleonheart@users.noreply.github.com>
2023-11-08 04:53:48 -0800

    Updated Identation

--- lilia/gamemode/backend/core/hooks/sh_hooks.lua
+++ lilia/gamemode/backend/core/hooks/sh_hooks.lua
@@ -416,32 +416,8 @@ end
 
 --------------------------------------------------------------------------------------------------------------------------
 function GM:PSALoader()
-    local TalkModesPSAString = "Please Remove Talk Modes. Our framework has such built in by default."
-    local ULXPSAString = [[
-            /*------------------------------------------------------------
-            
-            PUBLIC SERVICE ANNOUNCEMENT FOR LILIA SERVER OWNERS
-            
-            There is a ENOURMOUS performance issue with ULX Admin mod.
-            Lilia Development Team found ULX is the main issue
-            that make the server freeze when player count is higher
-            than 20-30. The duration of freeze will be increased as you get
-            more players on your server.
-            
-            If you're planning to open big server with ULX/ULib, Lilia
-            Development Team does not recommend your plan. Server Performance
-            Issues with ULX/Ulib on your server will be ignored and we're
-            going to consider that you're taking the risk of ULX/Ulib's
-            critical performance issue.
-            
-            Lilia 1.2 only displays this message when you have ULX or
-            ULib on your server.
-            
-                                           -Lilia Development Team
-            
-            */------------------------------------------------------------]]
-    if ulx or ULib then MsgC(Color(255, 0, 0), ULXPSAString .. "\n") end
-    if TalkModes then timer.Simple(2, function() MsgC(Color(255, 0, 0), TalkModesPSAString) end) end
+    if ulx or ULib then MsgC(Color(255, 0, 0), "WE DO NOT RECOMMEND THE USE OF ULX AS IT MAY CREATE PERFOMANCE ISSUES!" .. "\n") end
+    if TalkModes then timer.Simple(2, function() MsgC(Color(255, 0, 0), "Please Remove Talk Modes. Our framework has such built in by default.") end) end
 end
 
 --------------------------------------------------------------------------------------------------------------------------
