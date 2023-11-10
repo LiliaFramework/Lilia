c95628a8a036d2bfce12db00241b9ddcdfe0f6af
GitHub Action <action@github.com>
2023-11-10 14:42:02 +0000

    Auto-commit after running GLuaFixer

--- lilia/modules/roleplay/modules/recognition/cl_module.lua
+++ lilia/modules/roleplay/modules/recognition/cl_module.lua
@@ -9,7 +9,6 @@ end
 function MODULE:GetDisplayedDescription(client, isHUD)
     if client:getChar() and client ~= LocalPlayer() and LocalPlayer():getChar() and not LocalPlayer():getChar():doesRecognize(client:getChar():getID()) then
         if isHUD then return client:getChar():getDesc() end
-
         return "You do not recognize this person."
     end
 end
