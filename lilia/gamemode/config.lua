ad5755d554bc81462905edda570dd9124fe112ea
Leonheart <33399712+bleonheart@users.noreply.github.com>
2023-11-13 11:13:53 +0000

    Update config.lua

--- lilia/gamemode/config.lua
+++ lilia/gamemode/config.lua
@@ -1,4 +1,4 @@
-﻿--------------------------------------------------------------------------------------------------------------------------
+--------------------------------------------------------------------------------------------------------------------------
 lia.config = lia.config or {}
 --------------------------------------------------------------------------------------------------------------------------
 if not lia.config.WasInitialized then
@@ -103,7 +103,7 @@ if not lia.config.WasInitialized then
         -- Restricted Prop List
         BlackListedProps = {"models/props_combine/combinetrain02b.mdl", "models/props_combine/combinetrain02a.mdl", "models/props_combine/combinetrain01.mdl", "models/cranes/crane_frame.mdl", "models/props_wasteland/cargo_container01.mdl", "models/props_junk/trashdumpster02.mdl", "models/props_c17/oildrum001_explosive.mdl", "models/props_canal/canal_bridge02.mdl", "models/props_canal/canal_bridge01.mdl", "models/props_canal/canal_bridge03a.mdl", "models/props_canal/canal_bridge03b.mdl", "models/props_wasteland/cargo_container01.mdl", "models/props_wasteland/cargo_container01c.mdl", "models/props_wasteland/cargo_container01b.mdl", "models/props_combine/combine_mine01.mdl", "models/props_junk/glassjug01.mdl", "models/props_c17/paper01.mdl", "models/props_junk/garbage_takeoutcarton001a.mdl", "models/props_c17/trappropeller_engine.mdl", "models/props/cs_office/microwave.mdl", "models/items/item_item_crate.mdl", "models/props_junk/gascan001a.mdl", "models/props_c17/consolebox01a.mdl", "models/props_buildings/building_002a.mdl", "models/props_phx/mk-82.mdl", "models/props_phx/cannonball.mdl", "models/props_phx/ball.mdl", "models/props_phx/amraam.mdl", "models/props_phx/misc/flakshell_big.mdl", "models/props_phx/ww2bomb.mdl", "models/props_phx/torpedo.mdl", "models/props/de_train/biohazardtank.mdl", "models/props_buildings/project_building01.mdl", "models/props_combine/prison01c.mdl", "models/props/cs_militia/silo_01.mdl", "models/props_phx/huge/evildisc_corp.mdl", "models/props_phx/misc/potato_launcher_explosive.mdl", "models/props_combine/combine_citadel001.mdl", "models/props_phx/oildrum001_explosive.mdl"},
         -- Restricted Entity List for PhysGun
-        PhysGunMoveRestrictedEntityList = {"func_movelinear", "prop_door_rotating", "lia_vendor"},
+        PhysGunMoveRestrictedEntityList = {"prop_dynamic", "func_movelinear", "prop_door_rotating", "lia_vendor"},
         -- Blocked Entities for Remover Tool
         RemoverBlockedEntities = {"lia_bodygroupcloset", "lia_vendor",},
         -- Blacklisted Entities for Duplicator Tool
@@ -389,7 +389,6 @@ if not lia.config.WasInitialized then
             ["r_lod"] = "-1",
             ["lia_cheapblur"] = "1",
         },
-        ToolRequiresEntity = {"rb655_easy_bodygroup", "eyeposer", "remover", "thruster", "weld", "nocollide", "simfphyseditor", "simfphysduplicator", "colour", "precision", "stacker_improved", "advmat", "permaprops", "inflator", "physprop", "simfphyswheeleditor", "simfphysfueleditor", "paint", "simfphysgeareditor", "simfphyssuspensioneditor", "finger", "editentity", "material", "duplicator", "simfphyssoundeditor", "faceposer", "simfphysmiscsoundeditor",},
         RemovableHooks = {
             ["StartChat"] = {"StartChatIndicator",},
             ["FinishChat"] = {"EndChatIndicator",},
