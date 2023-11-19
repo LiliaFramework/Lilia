66ffa4eb5331d2f0e0fceb431421f8e0e905bc14
GitHub Action <action@github.com>
2023-11-19 21:41:03 +0000

    Auto-commit after running GLuaFixer

--- lilia/modules/roleplay/modules/recognition/meta/sh_meta.lua
+++ lilia/modules/roleplay/modules/recognition/meta/sh_meta.lua
@@ -5,6 +5,7 @@ function charMeta:doesRecognize(id)
     if not isnumber(id) and id.getID then id = id:getID() end
     return hook.Run("IsCharRecognized", self, id) ~= false
 end
+
 --------------------------------------------------------------------------------------------------------------------------
 function charMeta:doesFakeRecognize(id)
     if not isnumber(id) and id.getID then id = id:getID() end
