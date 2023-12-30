3aa47afe2aa296064cac0741accaebd3486bc1a8
Leonheart <33399712+bleonheart@users.noreply.github.com>
2023-12-30 01:38:40 +0000

    Moved a methd

--- lilia/modules/utilities/thirdperson/derma/client.lua
+++ lilia/modules/utilities/thirdperson/derma/client.lua
@@ -9,30 +9,30 @@ function PANEL:Init()
     self.list = self:Add("DPanel")
     self.list:Dock(FILL)
     self.list:DockMargin(0, 0, 0, 0)
-    local cfgheight = self.list:Add("DNumSlider")
-    cfgheight:Dock(TOP)
-    cfgheight:SetText("Height") -- Set the text above the slider
-    cfgheight:SetMin(0) -- Set the minimum number you can slide to
-    cfgheight:SetMax(30) -- Set the maximum number you can slide to
-    cfgheight:SetDecimals(0) -- Decimal places - zero for whole number
-    cfgheight:SetConVar("lia_tp_vertical") -- Changes the ConVar when you slide
-    cfgheight:DockMargin(10, 0, 0, 5)
-    local cfghorizontal = self.list:Add("DNumSlider")
-    cfghorizontal:Dock(TOP)
-    cfghorizontal:SetText("Horizontal") -- Set the text above the slider
-    cfghorizontal:SetMin(-30) -- Set the minimum number you can slide to
-    cfghorizontal:SetMax(30) -- Set the maximum number you can slide to
-    cfghorizontal:SetDecimals(0) -- Decimal places - zero for whole number
-    cfghorizontal:SetConVar("lia_tp_horizontal") -- Changes the ConVar when you slide
-    cfghorizontal:DockMargin(10, 0, 0, 5)
-    local cfgdist = self.list:Add("DNumSlider")
-    cfgdist:Dock(TOP)
-    cfgdist:SetText("Distance") -- Set the text above the slider
-    cfgdist:SetMin(0) -- Set the minimum number you can slide to
-    cfgdist:SetMax(100) -- Set the maximum number you can slide to
-    cfgdist:SetDecimals(0) -- Decimal places - zero for whole number
-    cfgdist:SetConVar("lia_tp_distance") -- Changes the ConVar when you slide
-    cfgdist:DockMargin(10, 0, 0, 5)
+    local cfg = self.list:Add("DNumSlider")
+    cfg:Dock(TOP)
+    cfg:SetText("") -- Set the text above the slider
+    cfg:SetMin(0) -- Set the minimum number you can slide to
+    cfg:SetMax(30) -- Set the maximum number you can slide to
+    cfg:SetDecimals(0) -- Decimal places - zero for whole number
+    cfg:SetConVar("lia_tp_vertical") -- Changes the ConVar when you slide
+    cfg:DockMargin(10, 0, 0, 5)
+    local cfg = self.list:Add("DNumSlider")
+    cfg:Dock(TOP)
+    cfg:SetText("") -- Set the text above the slider
+    cfg:SetMin(-30) -- Set the minimum number you can slide to
+    cfg:SetMax(30) -- Set the maximum number you can slide to
+    cfg:SetDecimals(0) -- Decimal places - zero for whole number
+    cfg:SetConVar("lia_tp_") -- Changes the ConVar when you slide
+    cfg:DockMargin(10, 0, 0, 5)
+    local cfg = self.list:Add("DNumSlider")
+    cfg:Dock(TOP)
+    cfg:SetText("ance") -- Set the text above the slider
+    cfg:SetMin(0) -- Set the minimum number you can slide to
+    cfg:SetMax(100) -- Set the maximum number you can slide to
+    cfg:SetDecimals(0) -- Decimal places - zero for whole number
+    cfg:SetConVar("lia_tp_ance") -- Changes the ConVar when you slide
+    cfg:DockMargin(10, 0, 0, 5)
 end
 
 ------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
