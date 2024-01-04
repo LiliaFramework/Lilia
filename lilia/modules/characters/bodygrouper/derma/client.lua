993df893aeefbc718423d4bfb6e554dab58de96e
GitHub Action <action@github.com>
2024-01-04 23:19:24 +0000

    Linted Code

--- lilia/modules/characters/bodygrouper/derma/client.lua
+++ lilia/modules/characters/bodygrouper/derma/client.lua
@@ -36,9 +36,7 @@ function PANEL:Init()
     self.skinSelector:SetVisible(false)
     self.skinSelector.OnValueChanged = function(_, value)
         local model = self.model.Entity
-        if IsValid(model) then
-            model:SetSkin(math.Round(value))
-        end
+        if IsValid(model) then model:SetSkin(math.Round(value)) end
     end
 
     local color = lia.config.Color
@@ -68,10 +66,7 @@ function PANEL:Init()
             end
 
             local makeChange = true
-            if self.originalSkin == skn then
-                makeChange = false
-            end
-
+            if self.originalSkin == skn then makeChange = false end
             if not makeChange then
                 for i = 0, model:GetNumBodyGroups() - 1 do
                     if self.originalBodygroups[i] ~= groups[i] then
@@ -128,9 +123,7 @@ function PANEL:PopulateOptions()
             panel:SetMax(target:GetBodygroupCount(i) - 1)
             panel:SetDecimals(0)
             panel:SetValue(group)
-            panel.OnValueChanged = function(_, value)
-                model:SetBodygroup(i, math.Round(value))
-            end
+            panel.OnValueChanged = function(_, value) model:SetBodygroup(i, math.Round(value)) end
         end
     else
         if not self.skinSelector:IsVisible() then
@@ -170,7 +163,6 @@ local function RotatePointAroundPivot(point, pivot, angles)
     local newpoint = point - pivot
     newpoint:Rotate(angles)
     newpoint = newpoint + pivot
-
     return newpoint
 end
 
