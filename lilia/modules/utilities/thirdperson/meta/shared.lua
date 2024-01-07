82ca8dc518bca4273064f25ff2c27ccd04148a29
Leonheart <33399712+bleonheart@users.noreply.github.com>
2024-01-07 10:38:41 +0000

    Bug Fixing

--- lilia/modules/utilities/thirdperson/meta/shared.lua
+++
@@ -1,9 +0,0 @@
-﻿--------------------------------------------------------------------------------------------------------------------------
-local playerMeta = FindMetaTable("Player")
---------------------------------------------------------------------------------------------------------------------------
-function playerMeta:CanOverrideView()
-    local ragdoll = Entity(self:getLocalVar("ragdoll", 0))
-    if IsValid(lia.gui.char) and lia.gui.char:IsVisible() then return false end
-    return ThirdPerson:GetBool() and not IsValid(self:GetVehicle()) and self.ThirdPersonEnabled and IsValid(self) and self:getChar() and not IsValid(ragdoll) and LocalPlayer():Alive()
-end
---------------------------------------------------------------------------------------------------------------------------
