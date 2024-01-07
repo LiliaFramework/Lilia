b0977079c6f81edf6955afda5f218e591469d59c
Leonheart <33399712+bleonheart@users.noreply.github.com>
2024-01-07 12:43:36 +0000

    Updated

--- lilia/modules/core/permissions/submodules/toolgun/libraries/server.lua
+++ lilia/modules/core/permissions/submodules/toolgun/libraries/server.lua
@@ -2,10 +2,11 @@
 local GM = GM or GAMEMODE
 ------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
 function GM:CanTool(client, _, tool)
+    if not IsValid(client) then return false end
     local privilege = "Staff Permissions - Access Tool " .. tool:gsub("^%l", string.upper)
     local entity = client:GetTracedEntity()
     local validEntity = IsValid(entity)
-    if IsValid(client) and (client:getChar():hasFlags("t") or client:isStaffOnDuty()) and CAMI.PlayerHasAccess(client, privilege, nil) then
+    if client:getChar():hasFlags("t") or client:isStaffOnDuty() and CAMI.PlayerHasAccess(client, privilege, nil) then
         if tool == "material" and validEntity and (entity:GetClass() == "prop_vehicle_jeep" or string.lower(toolobj:GetClientInfo("override")) == "pp/copy") then return false end
         if tool == "weld" and validEntity and entity:GetClass() == "sent_ball" then return false end
         if tool == "duplicator" then
@@ -16,6 +17,7 @@ function GM:CanTool(client, _, tool)
                     if v.ModelScale > 10 then
                         client:notify("A model within this duplication exceeds the size limit!")
                         print("[Server Warning] Potential server crash using dupes attempt by player: " .. client:Nick() .. " (" .. client:SteamID() .. ")")
+
                         return false
                     end
 
@@ -24,18 +26,24 @@ function GM:CanTool(client, _, tool)
             end
         end
 
-        if tool == "remover" then
-            if table.HasValue(RestrictionCore.RemoverBlockedEntities, entity:GetClass()) and validEntity then return CAMI.PlayerHasAccess(client, "Staff Permissions - Can Remove Blocked Entities", nil) end
-            if entity:IsWorld() and validEntity then return CAMI.PlayerHasAccess(client, "Staff Permissions - Can Remove World Entities", nil) end
+        if tool == "remover" and validEntity then
+            if table.HasValue(RestrictionCore.RemoverBlockedEntities, entity:GetClass()) then
+                return CAMI.PlayerHasAccess(client, "Staff Permissions - Can Remove Blocked Entities", nil)
+            elseif entity:IsWorld() then
+                return CAMI.PlayerHasAccess(client, "Staff Permissions - Can Remove World Entities", nil)
+            end
         end
 
         if tool == "button" and not table.HasValue(RestrictionCore.ButtonList, client:GetInfo("button_model")) then
             client:ConCommand("button_model models/maxofs2d/button_05.mdl")
             client:ConCommand("button_model")
+
             return false
         end
+
         return true
     end
+
     return false
 end
 ------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
