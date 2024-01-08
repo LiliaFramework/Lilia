1e8a39423d4182e59b4ae3240161ace12b87393c
Leonheart <33399712+bleonheart@users.noreply.github.com>
2024-01-08 00:59:32 +0000

    Added More Inclusion to Toolgun

--- lilia/modules/core/permissions/submodules/toolgun/libraries/server.lua
+++ lilia/modules/core/permissions/submodules/toolgun/libraries/server.lua
@@ -6,8 +6,8 @@ function GM:CanTool(client, _, tool)
     local privilege = "Staff Permissions - Access Tool " .. tool:gsub("^%l", string.upper)
     local entity = client:GetTracedEntity()
     local validEntity = IsValid(entity)
-    if client:getChar():hasFlags("t") or client:isStaffOnDuty() and CAMI.PlayerHasAccess(client, privilege, nil) then
-        if tool == "material" and validEntity and (entity:GetClass() == "prop_vehicle_jeep" or string.lower(toolobj:GetClientInfo("override")) == "pp/copy") then return false end
+    if client:getChar():hasFlags("t") or client:isStaffOnDuty() or CAMI.PlayerHasAccess(client, privilege, nil) then
+        if tool == "material" and validEntity and (entity:GetClass() == "prop_vehicle_jeep" or string.lower(tool:GetClientInfo("override")) == "pp/copy") then return false end
         if tool == "weld" and validEntity and entity:GetClass() == "sent_ball" then return false end
         if tool == "duplicator" then
             if table.HasValue(RestrictionCore.DuplicatorBlackList, entity) and validEntity then return false end
@@ -17,6 +17,7 @@ function GM:CanTool(client, _, tool)
                     if v.ModelScale > 10 then
                         client:notify("A model within this duplication exceeds the size limit!")
                         print("[Server Warning] Potential server crash using dupes attempt by player: " .. client:Nick() .. " (" .. client:SteamID() .. ")")
+
                         return false
                     end
 
@@ -31,15 +32,20 @@ function GM:CanTool(client, _, tool)
             elseif entity:IsWorld() then
                 return CAMI.PlayerHasAccess(client, "Staff Permissions - Can Remove World Entities", nil)
             end
+
+            return true
         end
 
         if tool == "button" and not table.HasValue(RestrictionCore.ButtonList, client:GetInfo("button_model")) then
             client:ConCommand("button_model models/maxofs2d/button_05.mdl")
             client:ConCommand("button_model")
+
             return false
         end
+
         return true
     end
+
     return false
 end
 ------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
