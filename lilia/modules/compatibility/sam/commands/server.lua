fecc76126572d50390518743d5b787aa3a8c0265
Leonheart <33399712+bleonheart@users.noreply.github.com>
2024-01-10 09:30:15 +0000

    Fixed WhiteSpaces

--- lilia/modules/compatibility/sam/commands/server.lua
+++ lilia/modules/compatibility/sam/commands/server.lua
@@ -3,7 +3,7 @@ lia.command.add("cleardecals", {
     adminOnly = true,
     privilege = "Clear Decals",
     onRun = function()
-        for k, v in pairs(player.GetAll()) do
+        for _, v in pairs(player.GetAll()) do
             v:ConCommand("r_cleardecals")
         end
     end
