ea8ea255ea42b92599fdc0074ee912392387de34
Leonheart <33399712+bleonheart@users.noreply.github.com>
2024-01-10 18:11:50 +0000

    Added Cooldown to Toolgun

--- lilia/modules/core/permissions/submodules/toolgun/libraries/server.lua
+++ lilia/modules/core/permissions/submodules/toolgun/libraries/server.lua
@@ -1,9 +1,14 @@
 ﻿local GM = GM or GAMEMODE
 function GM:CanTool(client, _, tool)
-    if not IsValid(client) then return false end
     local privilege = "Staff Permissions - Access Tool " .. tool:gsub("^%l", string.upper)
     local entity = client:GetTracedEntity()
     local validEntity = IsValid(entity)
+    if CurTime() < (client.ToolInterval or 0) then
+        client:notify("Tool on Cooldown!")
+
+        return false
+    end
+
     if client:getChar():hasFlags("t") or client:isStaffOnDuty() or CAMI.PlayerHasAccess(client, privilege, nil) then
         if tool == "material" and validEntity and (entity:GetClass() == "prop_vehicle_jeep" or string.lower(tool:GetClientInfo("override")) == "pp/copy") then return false end
         if tool == "weld" and validEntity and entity:GetClass() == "sent_ball" then return false end
@@ -15,6 +20,7 @@ function GM:CanTool(client, _, tool)
                     if v.ModelScale > 10 then
                         client:notify("A model within this duplication exceeds the size limit!")
                         print("[Server Warning] Potential server crash using dupes attempt by player: " .. client:Nick() .. " (" .. client:SteamID() .. ")")
+
                         return false
                     end
 
@@ -29,15 +35,21 @@ function GM:CanTool(client, _, tool)
             elseif entity:IsWorld() then
                 return CAMI.PlayerHasAccess(client, "Staff Permissions - Can Remove World Entities", nil)
             end
+
             return true
         end
 
         if tool == "button" and not table.HasValue(RestrictionCore.ButtonList, client:GetInfo("button_model")) then
             client:ConCommand("button_model models/maxofs2d/button_05.mdl")
             client:ConCommand("button_model")
+
             return false
         end
+
+        client.ToolInterval = CurTime() + RestrictionCore.ToolInterval
+
         return true
     end
+
     return false
 end
