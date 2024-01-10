c16691347e2e1f3419830c5cbcf8cf9b1b004952
Leonheart <33399712+bleonheart@users.noreply.github.com>
2024-01-10 09:30:38 +0000

    Tweaked Arguments

--- lilia/modules/core/protection/submodules/antifamilysharing/libraries/server.lua
+++ lilia/modules/core/protection/submodules/antifamilysharing/libraries/server.lua
@@ -1,13 +1,13 @@
 ------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
-function AntiFamilySharing:PlayerAuthed(ply, steamid)
+function AntiFamilySharing:PlayerAuthed(client, steamid)
     if not self.FamilySharingEnabled then return end
     local steamID64 = util.SteamIDTo64(steamid)
-    local OwnerSteamID64 = ply:OwnerSteamID64()
-    local JoiningPlayerName = ply:Name()
+    local OwnerSteamID64 = client:OwnerSteamID64()
+    local JoiningPlayerName = client:Name()
     local OwnerAccount = "Unknown"
 
     if OwnerSteamID64 ~= steamID64 then
-        ply:Kick("Family share accounts are not allowed on this server.")
+        client:Kick("Family share accounts are not allowed on this server.")
         for _, admin in ipairs(player.GetAll()) do
             if IsValid(admin) and CAMI.PlayerHasAccess(admin, "Staff Permissions - Can See Family Sharing Notifications", nil) then
                 steamworks.RequestPlayerInfo(OwnerSteamID64, function(name)
