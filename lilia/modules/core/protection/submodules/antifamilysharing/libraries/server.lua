9988342b14af43cc6b48400838392df8551727a4
GitHub Action <action@github.com>
2024-01-10 09:31:24 +0000

    Linted Code

--- lilia/modules/core/protection/submodules/antifamilysharing/libraries/server.lua
+++ lilia/modules/core/protection/submodules/antifamilysharing/libraries/server.lua
@@ -1,19 +1,21 @@
-------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+﻿------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
 function AntiFamilySharing:PlayerAuthed(client, steamid)
     if not self.FamilySharingEnabled then return end
     local steamID64 = util.SteamIDTo64(steamid)
     local OwnerSteamID64 = client:OwnerSteamID64()
     local JoiningPlayerName = client:Name()
     local OwnerAccount = "Unknown"
-
     if OwnerSteamID64 ~= steamID64 then
         client:Kick("Family share accounts are not allowed on this server.")
         for _, admin in ipairs(player.GetAll()) do
             if IsValid(admin) and CAMI.PlayerHasAccess(admin, "Staff Permissions - Can See Family Sharing Notifications", nil) then
-                steamworks.RequestPlayerInfo(OwnerSteamID64, function(name)
-                    OwnerAccount = name or OwnerAccount
-                    admin:ChatPrint("Family share account " .. JoiningPlayerName .. " [" .. steamID64 .. "] attempted to join the server. Original owner: " .. OwnerAccount)
-                end)
+                steamworks.RequestPlayerInfo(
+                    OwnerSteamID64,
+                    function(name)
+                        OwnerAccount = name or OwnerAccount
+                        admin:ChatPrint("Family share account " .. JoiningPlayerName .. " [" .. steamID64 .. "] attempted to join the server. Original owner: " .. OwnerAccount)
+                    end
+                )
             end
         end
     end
