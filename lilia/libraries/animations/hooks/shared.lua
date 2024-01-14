8ddf2c4eb9befe17b81a0d8a6728b82110ac444f
Leonheart <33399712+bleonheart@users.noreply.github.com>
2024-01-14 16:05:13 +0000

    Animation testing

--- lilia/libraries/animations/hooks/shared.lua
+++ lilia/libraries/animations/hooks/shared.lua
@@ -13,7 +13,7 @@ function GM:TranslateActivity(client, act)
     local class = getModelClass(model) or "player"
     local weapon = client.GetActiveWeapon(client)
     if class == "player" then
-        if not (RaisedWeaponCore and RaisedWeaponCore.WepAlwaysRaised) and IsValid(weapon) and (client.isWepRaised and not client.isWepRaised(client)) and client:OnGround() then
+        if (RaisedWeaponCore and not RaisedWeaponCore.WepAlwaysRaised) and IsValid(weapon) and (client.isWepRaised and not client.isWepRaised(client)) and client:OnGround() then
             if string.find(model, "zombie") then
                 local tree = lia.anim.zombie
                 if string.find(model, "fast") then tree = lia.anim.fastZombie end
