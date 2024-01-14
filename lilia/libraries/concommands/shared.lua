a434778a7772c695b45b0a806a20744929f6df87
Leonheart <33399712+bleonheart@users.noreply.github.com>
2024-01-14 12:25:34 +0000

    Added entity count to list_entities

--- lilia/libraries/concommands/shared.lua
+++ lilia/libraries/concommands/shared.lua
@@ -5,11 +5,15 @@
 end)
 
 concommand.Add("list_entities", function(client)
+    local count = 0
     if client:IsSuperAdmin() then
         print("Entities on the server:")
         for _, ent in pairs(ents.GetAll()) do
+            count = count + 1
             print(string.format("Entity #%d - Class: %s", ent:EntIndex(), ent:GetClass()))
         end
+
+        print("There is currently " .. count .. " entities on the server.")
     else
         print("Nuh-uh!")
     end
