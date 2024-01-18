105da45a26fe99f6cd80e1bb61a218b88f8ae4b4
Leonheart <33399712+bleonheart@users.noreply.github.com>
2024-01-18 05:09:52 +0000

    Update server.lua

--- lilia/modules/frameworkui/weaponselector/concommands/server.lua
+++ lilia/modules/frameworkui/weaponselector/concommands/server.lua
@@ -1,3 +1,1 @@
-concommand.Add("lia_selectwep", function(client, command, arguments)
-    client:SelectWeapon(arguments[1] or ("lia_fists" or "lia_keys"))
-end)
+concommand.Add("lia_selectwep", function(client, _, arguments) client:SelectWeapon(arguments[1] or ("lia_fists" or "lia_keys")) end)
