a31aafbe4431440efb4a55c00e0ed4ac0b8d3605
Leonheart <33399712+bleonheart@users.noreply.github.com>
2024-01-24 15:42:38 +0000

    Moved Hooks

--- lilia/gamemode/shared.lua
+++ lilia/gamemode/shared.lua
@@ -4,6 +4,24 @@ GM.Name = "Lilia"
 GM.Author = "Leonheart"
 GM.Website = "https://discord.gg/jjrhyeuzYV"
 function GM:Initialize()
+    for _, timerName in ipairs(lia.config.TimersToRemove) do
+        if timer.Exists(timerName) then
+            timer.Remove(timerName)
+            print(string.format("Removed timer: %s", timerName))
+        end
+    end
+
+    for hookType, identifiers in pairs(lia.config.RemovableHooks) do
+        for _, identifier in ipairs(identifiers) do
+            local hookTable = hook.GetTable()[hookType]
+            if hookTable and isfunction(hookTable[identifier]) then
+                hook.Remove(hookType, identifier)
+                print(string.format("Removed hook: %s - %s", hookType, identifier))
+            end
+        end
+    end
+
+    self:ServerInitialize()
     hook.Run("LoadLiliaFonts", "Arial", "Segoe UI")
     lia.module.initialize()
 end
@@ -18,7 +36,13 @@ function GM:OnReloaded()
     hook.Run("LoadLiliaFonts", lia.config.Font, lia.config.GenericFont)
 end
 
-if game.IsDedicated() then
-    concommand.Remove("gm_save")
-    concommand.Add("gm_save", function() end)
+function GM:ServerInitialize()
+    for command, value in pairs(lia.config.StartupConsoleCommands) do
+        if concommand.GetTable()[command] ~= nil then
+            RunConsoleCommand(command, value)
+            print(string.format("Executed console command on server: %s %s", command, value))
+        end
+    end
 end
+
+if game.IsDedicated() then concommand.Remove("gm_save") end
--- lilia/libraries/hooks/shared.lua
+++ lilia/libraries/hooks/shared.lua
@@ -16,36 +16,6 @@ function GM:InitializedModules()
         lia.anim.setModelClass(tpose, animtype)
     end
 
-    for hookType, identifiers in pairs(lia.config.RemovableHooks) do
-        for _, identifier in ipairs(identifiers) do
-            local hookTable = hook.GetTable()[hookType]
-            if hookTable and hookTable[identifier] then
-                hook.Remove(hookType, identifier)
-                print(string.format("Removed hook: %s - %s", hookType, identifier))
-            end
-        end
-    end
-
-    for _, timerName in ipairs(lia.config.TimersToRemove) do
-        if timer.Exists(timerName) then
-            timer.Remove(timerName)
-            print(string.format("Removed timer: %s", timerName))
-        end
-    end
-
-    for command, value in pairs(lia.config.StartupConsoleCommands) do
-        local client_command = command .. " " .. value
-        if concommand.GetTable()[command] then
-            if SERVER then
-                RunConsoleCommand(command, value)
-                print(string.format("Executed console command on server: %s %s", command, value))
-            else
-                LocalPlayer():ConCommand(client_command)
-                print(string.format("Executed console command on client: %s", client_command))
-            end
-        end
-    end
-
     if CLIENT then
         hook.Run("LoadLiliaFonts", lia.config.Font, lia.config.GenericFont)
         RunConsoleCommand("spawnmenu_reload")
