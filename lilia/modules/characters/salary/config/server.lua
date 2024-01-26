c30aab107b705d7639d56a470bc2b577b409a03a
Leonheart <33399712+bleonheart@users.noreply.github.com>
2024-01-26 22:20:15 +0000

    Improved Salary

--- lilia/modules/characters/salary/config/server.lua
+++ lilia/modules/characters/salary/config/server.lua
@@ -1,4 +1,4 @@
 ﻿--[[ Time interval between salary payments in seconds ]]
 SalaryCore.SalaryInterval = 300
---[[ Money threshold below which salaries will not be given ]]
+--[[ Money threshold above which salaries will not be given ]]
 SalaryCore.SalaryThreshold = 0
--- lilia/modules/characters/salary/libraries/server.lua
+++ lilia/modules/characters/salary/libraries/server.lua
@@ -5,7 +5,7 @@
     local faction = lia.faction.indices[character:getFaction()]
     local class = lia.class.list[character:getClass()]
     local PayAmount = hook.Run("GetSalaryAmount", client, faction, class) or (class and class.pay) or (faction and faction.pay) or 0
-    local SalaryLimit = hook.Run("VerifySalaryLimit", client, faction, class) or (class and class.payLimit) or (faction and faction.playLimit)
+    local SalaryLimit = hook.Run("GetSalaryLimit", client, faction, class) or (class and class.payLimit) or (faction and faction.payLimit) or SalaryCore.SalaryThreshold
     local timerFunc = timer.Exists(timerID) and timer.Adjust or timer.Create
     local delay = (class and class.payTimer) or (faction and faction.payTimer) or self.SalaryInterval
     if PayAmount > 0 then
@@ -15,17 +15,20 @@
                 return
             end
 
-            if SalaryLimit then return end
+            if SalaryLimit > 0 and SalaryLimit > (character:getMoney() + PayAmount) then
+                client:notify("You reached the limit of your salary! You can't get more money!")
+                character:setMoney(SalaryLimit)
+                return
+            end
+
             character:giveMoney(PayAmount)
             client:notifyLocalized("salary", lia.currency.get(PayAmount))
         end)
     end
 end
 
-function SalaryCore:PlayerLoadedChar(_, _, _)
-    for _, client in ipairs(player.GetAll()) do
-        hook.Run("CreateSalaryTimer", client)
-    end
+function SalaryCore:PlayerLoadedChar(client, _, _)
+    hook.Run("CreateSalaryTimer", client)
 end
 
 function SalaryCore:OnReloaded()
@@ -36,9 +39,4 @@ end
 
 function SalaryCore:GetSalaryAmount(client, faction, _)
     if faction.index == FACTION_STAFF or client.HasWarning then return 0 end
-end
-
-function SalaryCore:VerifySalaryLimit(client, _, _)
-    if self.SalaryThreshold > 0 and client:getChar():getMoney() >= self.SalaryThreshold then return true end
-    return false
 end
