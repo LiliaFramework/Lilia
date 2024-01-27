d52575d74e0c3ea68b63118dd632213d5c9537de
Leonheart <33399712+bleonheart@users.noreply.github.com>
2024-01-27 05:57:39 +0000

    Fixed Rare Death Bug

--- lilia/libraries/hooks/client.lua
+++ lilia/libraries/hooks/client.lua
@@ -31,8 +31,7 @@ function GM:CalcView(client, origin, angles, fov)
     local view = self.BaseClass:CalcView(client, origin, angles, fov)
     local entity = Entity(client:getLocalVar("ragdoll", 0))
     local ragdoll = client:GetRagdollEntity()
-    if client:GetViewEntity() ~= client then return view end
-    if (not client:ShouldDrawLocalPlayer() and IsValid(entity) and entity:IsRagdoll()) or (not LocalPlayer():Alive() and IsValid(ragdoll)) then
+    if client:GetViewEntity() == client and (not client:ShouldDrawLocalPlayer() and IsValid(entity) and entity:IsRagdoll()) or (not LocalPlayer():Alive() and IsValid(ragdoll)) then
         local ent = LocalPlayer():Alive() and entity or ragdoll
         local index = ent:LookupAttachment("eyes")
         if index then
@@ -41,11 +40,11 @@ function GM:CalcView(client, origin, angles, fov)
                 view = view or {}
                 view.origin = data.Pos
                 view.angles = data.Ang
+                view.znear = 1
             end
             return view
         end
     end
-    return view
 end
 
 function GM:HUDPaintBackground()
