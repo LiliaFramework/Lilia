eedd35b354f09e2383dea8af1083d503e4b611db
Leonheart <33399712+bleonheart@users.noreply.github.com>
2024-01-30 11:51:09 +0000

    Spacing

--- lilia/libraries/hooks/server.lua
+++ lilia/libraries/hooks/server.lua
@@ -291,7 +291,7 @@ end
 ---------------------------------------------------------------------------[[//////////////////]]---------------------------------------------------------------------------
 function GM:OnServerLog(client, logType, logString)
     for _, v in pairs(lia.util.getAdmins()) do
-        if hook.Run("CanPlayerSeeLog", v, logType) ~= false then lia.log.send(v, lia.log.getString(client, logType, logString) end
+        if hook.Run("CanPlayerSeeLog", v, logType) ~= false then lia.log.send(v, lia.log.getString(client, logType, logString)) end
     end
 end
 
