f510712b412c539a3cd3905468c314a2c129ceb7
Leonheart <33399712+bleonheart@users.noreply.github.com>
2024-01-30 11:36:49 +0000

    Added Temporary Module Text

--- lilia/modules/frameworkui/mainmenu/submodules/charselect/libraries/client.lua
+++ lilia/modules/frameworkui/mainmenu/submodules/charselect/libraries/client.lua
@@ -1,15 +1,15 @@
 ﻿---------------------------------------------------------------------------[[//////////////////]]---------------------------------------------------------------------------
-function CharacterSelection:LiliaLoaded()
+function CharacterSelectionCore:LiliaLoaded()
     vgui.Create("liaCharacter")
 end
 
 ---------------------------------------------------------------------------[[//////////////////]]---------------------------------------------------------------------------
-function CharacterSelection:KickedFromCharacter(_, isCurrentChar)
+function CharacterSelectionCore:KickedFromCharacter(_, isCurrentChar)
     if isCurrentChar then vgui.Create("liaCharacter") end
 end
 
 ---------------------------------------------------------------------------[[//////////////////]]---------------------------------------------------------------------------
-function CharacterSelection:CreateMenuButtons(tabs)
+function CharacterSelectionCore:CreateMenuButtons(tabs)
     tabs["characters"] = function(_)
         if IsValid(lia.gui.menu) then lia.gui.menu:Remove() end
         if self.KickOnEnteringMainMenu then netstream.Start("liaCharKickSelf") end
