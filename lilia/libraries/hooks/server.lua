48ec50b595953886957e22e15da29201315b8934
Leonheart <33399712+bleonheart@users.noreply.github.com>
2024-01-31 20:59:45 +0000

    Testing

--- lilia/libraries/hooks/server.lua
+++ lilia/libraries/hooks/server.lua
@@ -8,6 +8,88 @@ local defaultAngleData = {
 }
 
 ---------------------------------------------------------------------------[[//////////////////]]---------------------------------------------------------------------------
+function GM:PrePlayerLoadedChar(client, _, _)
+    client:SetBodyGroups("000000000")
+    client:SetSkin(0)
+    client:ExitVehicle()
+    client:Freeze(false)
+end
+
+---------------------------------------------------------------------------[[//////////////////]]---------------------------------------------------------------------------
+function GM:CreateDefaultInventory(character)
+    local charID = character:getID()
+    if lia.inventory.types["grid"] then
+        return lia.inventory.instance("grid", {
+            char = charID
+        })
+    end
+end
+
+---------------------------------------------------------------------------[[//////////////////]]---------------------------------------------------------------------------
+function GM:CharacterPreSave(character)
+    local client = character:getPlayer()
+    if not character:getInv() then return end
+    for _, v in pairs(character:getInv():getItems()) do
+        if v.onSave then v:call("onSave", client) end
+    end
+end
+
+---------------------------------------------------------------------------[[//////////////////]]---------------------------------------------------------------------------
+function GM:PlayerLoadedChar(client, character, lastChar)
+    local timeStamp = os.date("%Y-%m-%d %H:%M:%S", os.time())
+    lia.db.updateTable({
+        _lastJoinTime = timeStamp
+    }, nil, "characters", "_id = " .. character:getID())
+
+    if lastChar then
+        local charEnts = lastChar:getVar("charEnts") or {}
+        for _, v in ipairs(charEnts) do
+            if v and IsValid(v) then v:Remove() end
+        end
+
+        lastChar:setVar("charEnts", nil)
+    end
+
+    if IsValid(client.liaRagdoll) then
+        client.liaRagdoll.liaNoReset = true
+        client.liaRagdoll.liaIgnoreDelete = true
+        client.liaRagdoll:Remove()
+    end
+
+    character:setData("loginTime", os.time())
+    hook.Run("PlayerLoadout", client)
+end
+
+---------------------------------------------------------------------------[[//////////////////]]---------------------------------------------------------------------------
+function GM:CharacterLoaded(id)
+    local character = lia.char.loaded[id]
+    if character then
+        local client = character:getPlayer()
+        if IsValid(client) then
+            local uniqueID = "liaSaveChar" .. client:SteamID()
+            timer.Create(uniqueID, lia.config.CharacterDataSaveInterval, 0, function()
+                if IsValid(client) and client:getChar() then
+                    client:getChar():save()
+                else
+                    timer.Remove(uniqueID)
+                end
+            end)
+        end
+    end
+end
+
+---------------------------------------------------------------------------[[//////////////////]]---------------------------------------------------------------------------
+function GM:OnCharFallover(client, entity, bFallenOver)
+    bFallenOver = bFallenOver or false
+    if IsValid(entity) then
+        entity:SetCollisionGroup(COLLISION_GROUP_NONE)
+        entity:SetCustomCollisionCheck(false)
+    end
+
+    client:setNetVar("fallingover", bFallenOver)
+end
+
+---------------------------------------------------------------------------[[//////////////////]]---------------------------------------------------------------------------
 function GM:OnPlayerInteractItem(client, action, item)
     if isentity(item) then
         if IsValid(item) then
--- lilia/libraries/hooks/shared.lua
+++ lilia/libraries/hooks/shared.lua
@@ -47,6 +47,82 @@ function GM:Move(client, moveData)
 end
 
 ---------------------------------------------------------------------------[[//////////////////]]---------------------------------------------------------------------------
+function GM:VerifyModuleValidity(uniqueID, MODULE)
+    local isEnabled = MODULE.enabled
+    if uniqueID == "schema" then return true end
+    for ModuleName, conditions in pairs(lia.module.ModuleConditions) do
+        if uniqueID ~= ModuleName then continue end
+        if _G[conditions.global] ~= nil then
+            print(conditions.name .. " found. Activating Compatibility!")
+            return true
+        else
+            return false
+        end
+    end
+    return isEnabled
+end
+
+---------------------------------------------------------------------------[[//////////////////]]---------------------------------------------------------------------------
+function GM:LiliaLoaded()
+    local namecache = {}
+    for _, MODULE in pairs(lia.module.list) do
+        local authorID = (tonumber(MODULE.author) and tostring(MODULE.author)) or (string.match(MODULE.author, "STEAM_") and util.SteamIDTo64(MODULE.author)) or "Unknown"
+        if authorID then
+            if namecache[authorID] ~= nil then
+                MODULE.author = namecache[authorID]
+            else
+                steamworks.RequestPlayerInfo(authorID, function(newName)
+                    namecache[authorID] = newName
+                    MODULE.author = newName or MODULE.author
+                end)
+            end
+        end
+    end
+
+    lia.module.namecache = namecache
+end
+
+---------------------------------------------------------------------------[[//////////////////]]---------------------------------------------------------------------------
+function GM:ModuleLoaded(uniqueID, ModuleGlobal, MODULE)
+    local ModuleWorkshopContent = MODULE.WorkshopContent
+    local ModuleCAMIPermissions = MODULE.CAMIPrivileges
+    local IsValidForGlobal = ModuleGlobal ~= "" and ModuleGlobal ~= nil
+    if ModuleCAMIPermissions and istable(ModuleCAMIPermissions) then
+        for _, privilegeData in ipairs(ModuleCAMIPermissions) do
+            local privilegeInfo = {
+                Name = privilegeData.Name,
+                MinAccess = privilegeData.MinAccess or "admin",
+                Description = privilegeData.Description or ("Allows access to " .. privilegeData.Name:gsub("^%l", string.upper))
+            }
+
+            if not CAMI.GetPrivilege(privilegeData.Name) then CAMI.RegisterPrivilege(privilegeInfo) end
+        end
+    end
+
+    if IsValidForGlobal and uniqueID ~= "schema" then _G[ModuleGlobal] = MODULE end
+    if ModuleWorkshopContent then
+        if istable(ModuleWorkshopContent) then
+            for i = 1, #ModuleWorkshopContent do
+                local workshopID = ModuleWorkshopContent[i]
+                if isstring(workshopID) and workshopID:match("^%d+$") then
+                    resource.AddWorkshop(workshopID)
+                else
+                    print("Invalid Workshop ID:", workshopID)
+                end
+            end
+        else
+            resource.AddWorkshop(ModuleWorkshopContent)
+        end
+    end
+end
+
+---------------------------------------------------------------------------[[//////////////////]]---------------------------------------------------------------------------
+function GM:ModuleDependenciesPreLoad(uniqueID, ModuleGlobal, MODULE)
+    local IsValidForGlobal = ModuleGlobal ~= "" and ModuleGlobal ~= nil
+    if IsValidForGlobal and uniqueID ~= "schema" then _G[ModuleGlobal] = MODULE end
+end
+
+---------------------------------------------------------------------------[[//////////////////]]---------------------------------------------------------------------------
 function GM:InitPostEntity()
     if SERVER then
         lia.faction.formatModelData()
