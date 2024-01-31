343f096bcb3e174ce68c11f079dcec6701282cd0
Leonheart <33399712+bleonheart@users.noreply.github.com>
2024-01-31 01:19:32 +0000

    Update __category.lua

--- lilia/modules/compatibility/mlogs/putinyourmlogsloggers/lilia/__category.lua
+++ lilia/modules/compatibility/mlogs/putinyourmlogsloggers/lilia/__category.lua
@@ -1,18 +1,6 @@
-mLogs.addCategory(
-	"Lilia",
-	"lia", 
-	Color( 121, 65, 203 ),
-	function()
-		return lia != nil
-	end,
-	nil,
-	105
-)
-
+mLogs.addCategory("Lilia", "lia", Color(121, 65, 203), function() return lia ~= nil end, nil, 105)
 mLogs.addCategoryDefinitions("lia", {
 	LiliaLog = function(data) return mLogs.doLogReplace({"[Lilia Logs]", "^log"}, data) end,
 })
 
-if (SERVER) then
-	mLogs.addLogger("Lilia Logs", "LiliaLog", "lia")
-end
+if SERVER then mLogs.addLogger("Lilia Logs", "LiliaLog", "lia") end
