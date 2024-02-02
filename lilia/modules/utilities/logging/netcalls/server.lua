ede38edf87c51cfc7d8c60156c26f2135c867ff3
Leonheart <33399712+bleonheart@users.noreply.github.com>
2024-02-02 03:51:27 +0000

    Update server.lua

--- lilia/modules/utilities/logging/netcalls/server.lua
+++ lilia/modules/utilities/logging/netcalls/server.lua
@@ -6,6 +6,11 @@ util.AddNetworkString("liaDrawLogs")
 util.AddNetworkString("liaRequestLogs")
 ---------------------------------------------------------------------------[[//////////////////]]---------------------------------------------------------------------------
 net.Receive("liaRequestLogs", function(_, client)
+    if not CAMI.PlayerHasAccess(client, "Commands - View Logs", nil) then
+        client:notify(":|")
+        return
+    end
+
     local selectedDate = net.ReadString()
     local logs = LoggerCore:ReadLogsFromFile(selectedDate)
     net.Start("liaDrawLogs")
