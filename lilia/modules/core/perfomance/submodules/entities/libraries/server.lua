9192e6967b20fbf61ce31c0a67b154555761ba4a
Leonheart <33399712+bleonheart@users.noreply.github.com>
2024-02-06 23:54:30 +0000

    Tests

---
+++ lilia/modules/core/perfomance/submodules/entities/libraries/server.lua
@@ -0,0 +1,100 @@
+﻿---------------------------------------------------------------------------[[//////////////////]]---------------------------------------------------------------------------
+function MODULE:PlayerEnteredVehicle(_, vehicle)
+    if vehicle:GetClass() == "prop_vehicle_prisoner_pod" then vehicle:RemoveEFlags(EFL_NO_THINK_FUNCTION) end
+end
+
+---------------------------------------------------------------------------[[//////////////////]]---------------------------------------------------------------------------
+function MODULE:PropBreak(_, entity)
+    if entity:IsValid() and entity:GetPhysicsObject():IsValid() then constraint.RemoveAll(entity) end
+end
+
+---------------------------------------------------------------------------[[//////////////////]]---------------------------------------------------------------------------
+function MODULE:PlayerInitialSpawn(_)
+    local annoying = ents.FindByName("music")
+    local val = ents.GetMapCreatedEntity(1733)
+    if #annoying > 0 then
+        annoying[1]:SetKeyValue("RefireTime", 99999999)
+        annoying[1]:Fire("Disable")
+        annoying[1]:Fire("Kill")
+        val:SetKeyValue("RefireTime", 99999999)
+        val:Fire("Disable")
+        val:Fire("Kill")
+    end
+end
+
+---------------------------------------------------------------------------[[//////////////////]]---------------------------------------------------------------------------
+function MODULE:EntityEmitSound(tab)
+    if self.SoundsToMute[tab.SoundName] then return false end
+end
+
+---------------------------------------------------------------------------[[//////////////////]]---------------------------------------------------------------------------
+function MODULE:PreGamemodeLoaded()
+    function widgets.PlayerTick()
+    end
+
+    hook.Remove("PlayerTick", "TickWidgets")
+end
+
+---------------------------------------------------------------------------[[//////////////////]]---------------------------------------------------------------------------
+function MODULE:EntityRemoved(entity)
+    if entity:IsRagdoll() and not entity:getNetVar("player", nil) and self.RagdollCleaningTimer > 0 then
+        timer.Simple(self.RagdollCleaningTimer, function()
+            if not IsValid(entity) then return end
+            entity:SetSaveValue("m_bFadingOut", true)
+            timer.Simple(3, function()
+                if not IsValid(entity) then return end
+                entity:Remove()
+            end)
+        end)
+    end
+end
+
+---------------------------------------------------------------------------[[//////////////////]]---------------------------------------------------------------------------
+function MODULE:PlayerLeaveVehicle(_, vehicle)
+    if vehicle:GetClass() == "prop_vehicle_prisoner_pod" then
+        local sName = "PodFix" .. vehicle:EntIndex()
+        hook.Add("Think", sName, function()
+            if vehicle:IsValid() then
+                local tSave = vehicle:GetSaveTable()
+                if tSave.m_bEnterAnimOn then
+                    hook.Remove("Think", sName)
+                elseif not tSave.m_bExitAnimOn then
+                    vehicle:AddEFlags(EFL_NO_THINK_FUNCTION)
+                    hook.Remove("Think", sName)
+                end
+            else
+                hook.Remove("Think", sName)
+            end
+        end)
+    end
+end
+
+---------------------------------------------------------------------------[[//////////////////]]---------------------------------------------------------------------------
+function MODULE:InitPostEntity()
+    for _, v in pairs(ents.FindByClass("prop_physics")) do
+        if table.HasValue(self.UnOptimizableModels, v:GetModel()) then continue end
+        local optimizedEntity = ents.Create("prop_physics_multiplayer")
+        optimizedEntity:SetModel(v:GetModel())
+        optimizedEntity:SetPos(v:GetPos())
+        optimizedEntity:SetAngles(v:GetAngles())
+        optimizedEntity:SetSkin(v:GetSkin())
+        optimizedEntity:SetColor(v:GetColor())
+        optimizedEntity:SetMaterial(v:GetMaterial())
+        optimizedEntity:SetCollisionGroup(v:GetCollisionGroup())
+        optimizedEntity:SetKeyValue("fademindist", "1000")
+        optimizedEntity:SetKeyValue("fademaxdist", "1250")
+        optimizedEntity:Spawn()
+        local bodyGroups = v:GetBodyGroups()
+        if istable(bodyGroups) then
+            for _, v2 in pairs(bodyGroups) do
+                if v:GetBodygroup(v2.id) > 0 then optimizedEntity:SetBodygroup(v2.id, v:GetBodygroup(v2.id)) end
+            end
+        end
+
+        local physicsObject = v:GetPhysicsObject()
+        local optimizedEntityPhysicsObject = optimizedEntity:GetPhysicsObject()
+        if IsValid(physicsObject) and IsValid(optimizedEntityPhysicsObject) then optimizedEntityPhysicsObject:EnableMotion(physicsObject:IsMoveable()) end
+        v:Remove()
+    end
+end
+---------------------------------------------------------------------------[[//////////////////]]---------------------------------------------------------------------------
---
+++ lilia/modules/core/perfomance/submodules/entities/libraries/shared.lua
@@ -0,0 +1,36 @@
+﻿---------------------------------------------------------------------------[[//////////////////]]---------------------------------------------------------------------------
+function MODULE:InitializedModules()
+    if CLIENT then
+        if self.DrawEntityShadows then
+            for _, v in ipairs(ents.FindByClass("prop_door_rotating")) do
+                if IsValid(v) and v:isDoor() then v:DrawShadow(false) end
+            end
+        end
+    else
+        if self.GarbageCleaningTimer > 0 then
+            timer.Create("CleanupGarbage", self.GarbageCleaningTimer, 0, function()
+                for _, v in ipairs(ents.GetAll()) do
+                    if table.HasValue(self.Perfomancekillers, v:GetClass()) then SafeRemoveEntity(v) end
+                end
+            end)
+        end
+    end
+end
+
+---------------------------------------------------------------------------[[//////////////////]]---------------------------------------------------------------------------
+function MODULE:InitPostEntity()
+    for _, v in next, list.Get("ThrusterSounds") do
+        self.SoundsToMute[v.thruster_soundname] = true
+    end
+end
+
+---------------------------------------------------------------------------[[//////////////////]]---------------------------------------------------------------------------
+function MODULE:OnEntityCreated(entity)
+    if CLIENT then
+        if self.DrawEntityShadows then entity:DrawShadow(false) end
+    else
+        if entity:GetClass() == "prop_vehicle_prisoner_pod" then entity:AddEFlags(EFL_NO_THINK_FUNCTION) end
+        if entity:IsWidget() then hook.Add("PlayerTick", "GODisableEntWidgets2", function(_, n) widgets.PlayerTick(entity, n) end) end
+    end
+end
+---------------------------------------------------------------------------[[//////////////////]]---------------------------------------------------------------------------
