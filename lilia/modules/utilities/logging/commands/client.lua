f988c03d8665fed5ecbda303aa11bd475db72aca
Leonheart <33399712+bleonheart@users.noreply.github.com>
2024-02-12 02:45:52 +0000

    Update client.lua

--- lilia/modules/utilities/logging/commands/client.lua
+++ lilia/modules/utilities/logging/commands/client.lua
@@ -2,18 +2,18 @@
 lia.command.add("logs", {
     superAdminOnly = true,
     privilege = "View Logs",
-    onRun = function(client) end
+    onRun = function() end
 })
 
 lia.command.add("netlogs", {
     superAdminOnly = true,
     privilege = "View Logs",
-    onRun = function(client) end
+    onRun = function() end
 })
 
 lia.command.add("concommandlogs", {
     superAdminOnly = true,
     privilege = "View Logs",
-    onRun = function(client) end
+    onRun = function() end
 })
 ---------------------------------------------------------------------------[[//////////////////]]---------------------------------------------------------------------------
