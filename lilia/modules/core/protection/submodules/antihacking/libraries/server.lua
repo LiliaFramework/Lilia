90c8d8a068a5dd0ee746558676aa50796d377690
Leonheart <33399712+bleonheart@users.noreply.github.com>
2024-02-12 18:44:57 +0000

    Minor Fixes

--- lilia/modules/core/protection/submodules/antihacking/libraries/server.lua
+++ lilia/modules/core/protection/submodules/antihacking/libraries/server.lua
@@ -38,39 +38,28 @@ function net.Incoming(len, client)
     local flaggedNetPlayers = AntiHacking.flaggedNetPlayers
     antiNetSpam[plySteamid] = antiNetSpam[plySteamid] or {}
     antiNetSpam[plySteamid][name] = (antiNetSpam[plySteamid][name] or 0) + 1
-    if table.HasValue(MODULE.NetMessageWhitelist, name) then return end
     if antiNetSpam[plySteamid][name] > AntiHacking.threshold then
-        if not flaggedNetPlayers[plySteamid] then
-            ServerLog(string.format("Net spam attempted on Net Message: %s Client: %s (STEAMID: %s) (IP: %s) \n", name, plyNick, plySteamid, plyIP))
-            file.Append("lilia/netlogs/" .. os.date("%x"):gsub("/", "-") .. ".txt", "[" .. os.date("%X") .. "]\t" .. string.format("Net spam attempted on Net Message: %s Client: %s (STEAMID: %s) (IP: %s) \n", name, plyNick, plySteamid, plyIP) .. "\r\n")
-        end
-
+        if not flaggedNetPlayers[plySteamid] then file.Append("lilia/netlogs/" .. os.date("%x"):gsub("/", "-") .. ".txt", "[" .. os.date("%X") .. "]\t" .. string.format("Net spam attempted on Net Message: %s Client: %s (STEAMID: %s) (IP: %s) \n", name, plyNick, plySteamid, plyIP) .. "\r\n") end
         flaggedNetPlayers[plySteamid] = true
     end
 
     local func = net.Receivers[name:lower()]
     if not func then
-        ServerLog(string.format("No receiving function for '%s' (net msg #%d) Client: %s (STEAMID: %s) (IP: %s) \n", name, i, plyNick, plySteamid, plyIP))
         file.Append("lilia/netlogs/" .. os.date("%x"):gsub("/", "-") .. ".txt", "[" .. os.date("%X") .. "]\t" .. string.format("No receiving function for '%s' (net msg #%d) Client: %s (STEAMID: %s) (IP: %s) \n", name, i, plyNick, plySteamid, plyIP) .. "\r\n")
         return
     end
 
     len = len - 16
     local curString = not table.HasValue(AntiHacking.ExploitableNetMessages, name) and "Net message '%s' (%d) received (%.2fkb (%db)) Client: %s (STEAMID: %s) (IP: %s) \n" or "Net message '%s' (%d) received (%.2fkb (%db)) Client: %s (STEAMID: %s) (IP: %s) [ Exploitable String ] \n"
-    ServerLog(string.format(curString, name, i, len / 8 / 1024, len / 8, plyNick, plySteamid, plyIP))
     file.Append("lilia/netlogs/" .. os.date("%x"):gsub("/", "-") .. ".txt", "[" .. os.date("%X") .. "]\t" .. string.format(curString, name, i, len / 8 / 1024, len / 8, plyNick, plySteamid, plyIP) .. "\r\n")
     local status, error = pcall(function() func(len, client) end)
-    if not status then
-        ServerLog(string.format("Error during net message (%s). Reasoning: %s \n", name, error))
-        file.Append("lilia/netlogs/" .. os.date("%x"):gsub("/", "-") .. ".txt", "[" .. os.date("%X") .. "]\t" .. string.format("Error during net message (%s). Reasoning: %s \n", name, error) .. "\r\n")
-    end
+    if not status then file.Append("lilia/netlogs/" .. os.date("%x"):gsub("/", "-") .. ".txt", "[" .. os.date("%X") .. "]\t" .. string.format("Error during net message (%s). Reasoning: %s \n", name, error) .. "\r\n") end
 end
 
 ---------------------------------------------------------------------------[[//////////////////]]---------------------------------------------------------------------------
 AntiHacking.crun = AntiHacking.crun or concommand.Run
 ---------------------------------------------------------------------------[[//////////////////]]---------------------------------------------------------------------------
 function concommand.Run(ply, cmd, args, argStr)
-    if MODULE.ConCommandWhitelist then return AntiHacking.crun(ply, cmd, args, argStr) end
     if not IsValid(ply) then return AntiHacking.crun(ply, cmd, args, argStr) end
     if not cmd then return AntiHacking.crun(ply, cmd, args, argStr) end
     local plySteamid = IsValid(ply) and ply:SteamID() or "UNKNOWN STEAMID"
@@ -82,15 +71,10 @@ function concommand.Run(ply, cmd, args, argStr)
     antiConSpam[plySteamid][cmd] = (antiConSpam[plySteamid][cmd] or 0) + 1
     local temp = (args and args ~= "" and #args ~= 0) and " " .. table.concat(args, " ") or "None"
     if antiConSpam[plySteamid][cmd] > 10 then
-        if not flaggedConPlayers[plySteamid] then
-            ServerLog("Player " .. "'" .. plyNick .. "'" .. " (" .. plySteamid .. ") " .. "(" .. plyIP .. ")" .. " has attempted to concommand spam with command: " .. cmd .. " args: " .. temp .. ". \n")
-            file.Append("lilia/concommandlogs/" .. os.date("%x"):gsub("/", "-") .. ".txt", "[" .. os.date("%X") .. "]\t" .. "Player " .. "'" .. plyNick .. "'" .. " (" .. plySteamid .. ") " .. "(" .. plyIP .. ")" .. " has attempted to concommand spam with command: " .. cmd .. " args: " .. temp .. "\r\n")
-        end
-
+        if not flaggedConPlayers[plySteamid] then file.Append("lilia/concommandlogs/" .. os.date("%x"):gsub("/", "-") .. ".txt", "[" .. os.date("%X") .. "]\t" .. "Player " .. "'" .. plyNick .. "'" .. " (" .. plySteamid .. ") " .. "(" .. plyIP .. ")" .. " has attempted to concommand spam with command: " .. cmd .. " args: " .. temp .. "\r\n") end
         flaggedConPlayers[plySteamid] = true
     end
 
-    ServerLog("Player " .. "'" .. plyNick .. "'" .. " (" .. plySteamid .. ") " .. "(" .. plyIP .. ")" .. " has executed this command: " .. cmd .. " args: " .. temp .. ". \n")
     file.Append("lilia/concommandlogs/" .. os.date("%x"):gsub("/", "-") .. ".txt", "[" .. os.date("%X") .. "]\t" .. "Player " .. "'" .. plyNick .. "'" .. " (" .. plySteamid .. ") " .. "(" .. plyIP .. ")" .. " has executed this command: " .. cmd .. " args: " .. temp .. "\r\n")
     return AntiHacking.crun(ply, cmd, args, argStr)
 end
