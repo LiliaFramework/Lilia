e496594eac4c04424a93fe5f476c1070014bc9c2
Leonheart <33399712+bleonheart@users.noreply.github.com>
2024-02-21 23:11:14 +0000

    Update server.lua

--- lilia/modules/compatibility/permaprops/libraries/server.lua
+++ lilia/modules/compatibility/permaprops/libraries/server.lua
@@ -3,6 +3,12 @@ function MODULE:CanTool(client, _, tool)
     local privilege = "Staff Permissions - Access Tool " .. tool:gsub("^%l", string.upper)
     local entity = client:GetTracedEntity()
     local validEntity = IsValid(entity)
-    if validEntity and tool == "permaprops" and IsValid(client) and (client:getChar():hasFlags("t") or client:isStaffOnDuty()) and CAMI.PlayerHasAccess(client, privilege, nil) and not string.StartsWith(entity:GetClass(), "lia_") and not table.HasValue(PermissionCore.CanNotPermaProp, entity:GetClass()) then return true end
+    if validEntity and tool == "permaprops" and ((client:getChar():hasFlags("t") or client:isStaffOnDuty()) or CAMI.PlayerHasAccess(client, privilege, nil)) then
+        if not (string.StartsWith(entity:GetClass(), "lia_") and table.HasValue(PermissionCore.CanNotPermaProp, entity:GetClass())) then
+            return true
+        else
+            return false
+        end
+    end
 end
 ---------------------------------------------------------------------------[[//////////////////]]---------------------------------------------------------------------------
