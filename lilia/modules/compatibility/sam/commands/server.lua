4f7ddddbfa12aba865dd9b567719081ad1d826cf
Leonheart <33399712+bleonheart@users.noreply.github.com>
2024-02-21 01:08:27 +0000

    Added /playtime (requires SAM)

--- lilia/modules/compatibility/sam/commands/server.lua
+++ lilia/modules/compatibility/sam/commands/server.lua
@@ -8,4 +8,24 @@ lia.command.add("cleardecals", {
         end
     end
 })
+
+---------------------------------------------------------------------------[[//////////////////]]---------------------------------------------------------------------------
+lia.command.add("playtime", {
+    adminOnly = true,
+    privilege = "Default User Commands",
+    onRun = function(client)
+        local steamID = client:SteamID()
+        local query = "SELECT play_time FROM sam_players WHERE steamid = " .. SQLStr(steamID) .. ";"
+        local result = sql.QueryRow(query)
+        if result then
+            local playTimeInSeconds = tonumber(result.play_time) or 0
+            local hours = math.floor(playTimeInSeconds / 3600)
+            local minutes = math.floor((playTimeInSeconds % 3600) / 60)
+            local seconds = playTimeInSeconds % 60
+            client:ChatPrint(string.format("Your playtime: %d hours, %d minutes, %d seconds", hours, minutes, seconds))
+        else
+            client:ChatPrint("Could not retrieve your playtime. Please try again or contact an admin if the issue persists.")
+        end
+    end
+})
 ---------------------------------------------------------------------------[[//////////////////]]---------------------------------------------------------------------------
