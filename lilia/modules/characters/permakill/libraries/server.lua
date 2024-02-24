d98756497c3be4765d91cf75d7a93ca632369887
Leonheart <33399712+bleonheart@users.noreply.github.com>
2024-02-24 19:42:49 +0000

    Update server.lua

--- lilia/modules/characters/permakill/libraries/server.lua
+++ lilia/modules/characters/permakill/libraries/server.lua
@@ -1,7 +1,7 @@
 ﻿---------------------------------------------------------------------------[[//////////////////]]---------------------------------------------------------------------------
 function MODULE:PlayerDeath(client, inflictor, attacker)
     local character = client:getChar()
-    if (attacker == client or inflictor:IsWorld() or attacker:GetClass() == "worldspawn") and not lia.config.PKWorld then return end
+    if (attacker == client or inflictor:IsWorld() or attacker:GetClass() == "worldspawn") and not self.PKWorld then return end
     if character:getData("canbepermakilled", false) then character:ban() end
 end
 ---------------------------------------------------------------------------[[//////////////////]]---------------------------------------------------------------------------
