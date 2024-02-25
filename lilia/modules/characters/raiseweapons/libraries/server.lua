a95c0acf316fd2af3f7709ec9d8031967b2015ff
Leonheart <33399712+bleonheart@users.noreply.github.com>
2024-02-25 11:08:01 +0000

    Raise Weapons QOL Changes

--- lilia/modules/characters/raiseweapons/libraries/server.lua
+++ lilia/modules/characters/raiseweapons/libraries/server.lua
@@ -1,15 +1,14 @@
 ﻿---------------------------------------------------------------------------[[//////////////////]]---------------------------------------------------------------------------
-function MODULE:PlayerSwitchWeapon(client, _, _)
-    client:setWepRaised(false)
+function MODULE:PlayerSwitchWeapon(ply, _, newWeapon)
+    if self.WepAlwaysRaised then return end
+    if IsValid(ply) and IsValid(newWeapon) then
+        local weaponClass = newWeapon:GetClass()
+        if weaponClass ~= "lia_hands" and weaponClass ~= "lia_keys" then timer.Simple(1, function() ply:setWepRaised(true) end) end
+    end
 end
 
 ---------------------------------------------------------------------------[[//////////////////]]---------------------------------------------------------------------------
 function MODULE:KeyPress(client, key)
-    if key == IN_RELOAD then timer.Create("liaToggleRaise" .. client:SteamID(), 1, 1, function() if IsValid(client) then client:toggleWepRaised() end end) end
-end
-
----------------------------------------------------------------------------[[//////////////////]]---------------------------------------------------------------------------
-function MODULE:KeyRelease(client, key)
-    if key == IN_RELOAD then timer.Remove("liaToggleRaise" .. client:SteamID()) end
+    if key == IN_RELOAD and IsValid(client) then client:toggleWepRaised() end
 end
 ---------------------------------------------------------------------------[[//////////////////]]---------------------------------------------------------------------------
