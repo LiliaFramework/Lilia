b8fafbcae5904ab4a9602c725a57fd6c319b0cd6
Leonheart <33399712+bleonheart@users.noreply.github.com>
2024-03-06 09:38:28 +0000

    .

--- documentation/hooks/class.lua
+++ documentation/hooks/class.lua
@@ -1,4 +1,6 @@
+
 -- luacheck: ignore 111
+
 --[[--
 Class setup hooks.
 
@@ -6,6 +8,7 @@ As with `Faction`s, `Class`es get their own hooks for when players leave/join a 
 valid in class tables that are created in `schema/classes/sh_classname.lua`, and cannot be used like regular gamemode hooks.
 ]]
 -- @hooks Class
+
 --- Whether or not a player can switch to this class.
 -- @realm shared
 -- @player client Client that wants to switch to this class
