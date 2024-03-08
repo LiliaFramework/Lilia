c7b5a5c2beeeee96fa34f3623cd4cc7a28972a59
Leonheart <33399712+bleonheart@users.noreply.github.com>
2024-03-08 21:17:22 +0000

    Documented getLocalVar & getNetVar

--- lilia/libraries/meta/entity/client.lua
+++ lilia/libraries/meta/entity/client.lua
@@ -1,4 +1,8 @@
-﻿local entityMeta = FindMetaTable("Entity")
+﻿-- @module Entity
+-- @moduleCommentStart
+-- Entity meta functions.
+-- @moduleCommentEnd
+local entityMeta = FindMetaTable("Entity")
 function entityMeta:isDoor()
     return self:GetClass():find("door")
 end
@@ -14,10 +18,28 @@ function entityMeta:getDoorPartner()
     end
 end
 
+-- @type method Entity:getNetVar(key, default)
+-- @typeCommentStart
+-- Returns the networked variable of the entity.
+-- @typeCommentEnd
+-- @realm shared
+-- @classmod Entity
+-- @string key The key of the networked variable.
+-- @string default The default value to return if the networked variable is not set.
+-- @treturn any The networked variable.
 function entityMeta:getNetVar(key, default)
     local index = self:EntIndex()
     if lia.net[index] and lia.net[index][key] ~= nil then return lia.net[index][key] end
     return default
 end
 
+-- @type method Entity:getLocalVar(key, value)
+-- @typeCommentStart
+-- Returns the networked variable of a player.
+-- @typeCommentEnd
+-- @realm shared
+-- @classmod Player
+-- @string key The key of the networked variable.
+-- @string default The default value to return if the networked variable is not set.
+-- @treturn any The networked variable.
 FindMetaTable("Player").getLocalVar = entityMeta.getNetVar
