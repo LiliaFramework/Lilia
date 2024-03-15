069b3d5083ad047e30c03bf3e2b8e1ef58f23e4f
Leonheart <33399712+bleonheart@users.noreply.github.com>
2024-03-15 19:01:26 +0000

    Update faction.lua

--- docs/hooks/faction.lua
+++ docs/hooks/faction.lua
@@ -12,10 +12,11 @@ regular gamemode hooks.
 -- @param client The client for whom the default name is being retrieved
 -- @treturn string The default name for the newly created character
 -- @usage function FACTION:getDefaultName(client)
---     return "CT-" .. math.random(111111, 999999) -- Sets the name as CT-XXXXXX, where the XXXXXX are randomly generated digits.
+-- 	return "CT-" .. math.random(111111, 999999)
 -- end
 function getDefaultName(client)
 end
+
 --- Called when the default description for a character needs to be retrieved.
 -- This function allows factions to define custom default descriptions for characters.
 -- @realm shared
@@ -23,34 +24,37 @@ end
 -- @param faction The faction ID for which the default description is being retrieved
 -- @treturn string The default description for the newly created character
 -- @usage function FACTION:getDefaultDesc(client, faction)
--- 	return "A police officer" -- Example default description.
+-- 	return "A police officer"
 -- end
 function getDefaultDesc(client, faction)
 end
+
 --- Called when a character has been initially created and assigned to this faction.
 -- @realm server
 -- @param client The client that owns the character
 -- @param character The character that has been created
 -- @usage function FACTION:onCharCreated(client, character)
 -- 	local inventory = character:getInv()
--- 	inventory:add("fancy_suit") -- Adds a Fancy Suit item to the character's inventory.
+-- 	inventory:add("fancy_suit")
 -- end
 function onCharCreated(client, character)
 end
+
 --- Called when a character in this faction has spawned in the world.
 -- @realm server
 -- @param client The player that has just spawned
 -- @usage function FACTION:onSpawn(client)
--- 	client:ChatPrint("You have spawned!") -- Notifies the client that they have spawned.
+-- 	client:ChatPrint("You have spawned!")
 -- end
 function onSpawn(client)
 end
+
 --- Called when a player's character has been transferred to this faction.
 -- @realm server
 -- @param character The character that was transferred
 -- @usage function FACTION:onTransferred(character)
 -- 	local randomModelIndex = math.random(1, #self.models)
--- 	character:setModel(self.models[randomModelIndex]) -- Sets a random model from the faction's model list as the character's model.
+-- 	character:setModel(self.models[randomModelIndex])
 -- end
 function onTransferred(character)
 end
