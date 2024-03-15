0238971c24882c9110e1ffb849010a09805eac36
Leonheart <33399712+bleonheart@users.noreply.github.com>
2024-03-15 19:08:26 +0000

    Update class.lua

--- docs/hooks/class.lua
+++ docs/hooks/class.lua
@@ -5,41 +5,45 @@ As with `Faction`s, `Class`es get their own hooks for when players leave/join a 
 valid in class tables that are created in `schema/classes/classname.lua`, and cannot be used like regular gamemode hooks.
 ]]
 -- @hooks Class
+
 --- Whether or not a player can switch to this class.
 -- @realm shared
--- @player client Client that wants to switch to this class
+-- @param client Player who wants to switch to this class
 -- @treturn bool True if the player is allowed to switch to this class
 -- @usage function CLASS:onCanBe(client)
--- 	return client:IsAdmin() or client:getChar():hasFlags("Z")
+-- 	return client:IsAdmin() or client:getChar():hasFlags("Z") -- Only admins or people with the Z flag are allowed in this class!
 -- end
-function onCanBe(client)
+function CLASS:onCanBe(client)
 end
 
 --- Called when a character has left this class and has joined a different one.
 -- @realm server
 -- @param client Player who left this class
--- @usage function CLASS:onLeave(client)
+-- @usage
+-- function CLASS:onLeave(client)
 -- 	local character = client:getChar()
 -- 	character:setModel("models/player/alyx.mdl")
 -- end
-function onLeave(client)
+function CLASS:onLeave(client)
 end
 
 --- Called when a character has joined this class.
 -- @realm server
 -- @param client Player who has joined this class
--- @usage function CLASS:onSet(client)
+-- @usage
+-- function CLASS:onSet(client)
 -- 	client:setModel("models/police.mdl")
 -- end
-function onSet(client)
+function CLASS:onSet(client)
 end
 
 --- Called when a character in this class has spawned in the world.
 -- @realm server
 -- @param client Player that has just spawned
--- @usage function CLASS:onSpawn(client)
--- 	client:SetMaxHealth(500)
--- 	client:SetHealth(500)
+-- @usage
+-- function CLASS:onSpawn(client)
+-- 	client:SetMaxHealth(500) -- Sets your Max Health to 500.
+-- 	client:SetHealth(500) -- Subsequently sets your Health to 500.
 -- end
-function onSpawn(client)
+function CLASS:onSpawn(client)
 end
