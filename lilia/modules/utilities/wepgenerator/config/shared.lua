8b6adaaab8fd77f671b85148d76ee0e82be30c24
Leonheart <33399712+bleonheart@users.noreply.github.com>
2024-03-16 21:09:22 +0000

    Update shared.lua

--- lilia/modules/utilities/wepgenerator/config/shared.lua
+++ lilia/modules/utilities/wepgenerator/config/shared.lua
@@ -1,2 +1,2 @@
-﻿MODULE.AutomaticWeaponRegister = false
+﻿MODULE.AutomaticWeaponRegister = true
 MODULE.RegisterWeaponsBlackList = {"sf2_tool", "weapon_fists", "weapon_medkit", "gmod_camera", "gmod_tool", "lightning_gun", "weapon_base", "lia_hands", "lia_keys",}
