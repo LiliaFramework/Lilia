b686a2ab76fd07998a3639c8788a6e90a8920268
Leonheart <33399712+bleonheart@users.noreply.github.com>
2024-03-26 11:16:09 +0000

    Updated some comments

--- lilia/libraries/config.lua
+++ lilia/libraries/config.lua
@@ -8,151 +8,151 @@ if not ConfigWasInitialized then
     lia.config = {
         --- How fast characters walk.
         -- @realm shared
-        -- @int Default Walk Speed
+        -- @int Default Walk Speed.
         WalkSpeed = 130,
 
         --- How fast characters run.
         -- @realm shared
-        -- @int Default Running Speed
+        -- @int Default Running Speed.
         RunSpeed = 235,
 
         --- Walk speed ratio when holding Alt.
         -- @realm shared
-        -- @float Default Walk Speed Ratio
+        -- @float Default Walk Speed Ratio.
         WalkRatio = 0.5,
 
         --- Maximum number of characters per player.
         -- @realm shared
-        -- @int Default Character Amount
+        -- @int Default Character Amount.
         MaxCharacters = 5,
 
         --- Period between data saves.
         -- @realm shared
-        -- @int Time in Seconds
+        -- @int Time in Seconds.
         DataSaveInterval = 600,
 
         --- Time between character data saves.
         -- @realm shared
-        -- @int Time in Seconds
+        -- @int Time in Seconds.
         CharacterDataSaveInterval = 300,
 
         --- The limit of money you can have on yourself at a given time.
         -- @realm shared
-        -- @int Limit Of Money (0 = infinite)
+        -- @int Limit Of Money (0 = infinite).
         MoneyLimit = 0,
 
         --- Default inventory width.
         -- @realm shared
-        -- @int Inventory Width
+        -- @int Inventory Width.
         invW = 6,
 
         --- Default inventory height.
         -- @realm shared
-        -- @int Inventory Height
+        -- @int Inventory Height.
         invH = 4,
 
         --- Default money amount.
         -- @realm shared
-        -- @int Default Money Amount
+        -- @int Default Money Amount.
         DefaultMoney = 0,
 
         --- Maximum chat message length.
         -- @realm shared
-        -- @int Max Chat Length
+        -- @int Max Chat Length.
         MaxChatLength = 256,
 
         --- Currency symbol.
         -- @realm shared
-        -- @string Currency Symbol
+        -- @string Currency Symbol.
         CurrencySymbol = "$",
 
         --- Time to respawn after death.
         -- @realm shared
-        -- @int Time to Respawn
+        -- @int Time to Respawn.
         SpawnTime = 5,
 
         --- Maximum attributes one can have.
         -- @realm shared
-        -- @int Maximum Attributes
+        -- @int Maximum Attributes.
         MaxAttributes = 30,
 
         --- Year in the gamemode's schema.
         -- @realm shared
-        -- @int Schema Year
+        -- @int Schema Year.
         SchemaYear = 2023,
 
         --- Minimum length of a character's description.
         -- @realm shared
-        -- @int Minimum Description Length
+        -- @int Minimum Description Length.
         MinDescLen = 16,
 
         --- Time required to enter a vehicle.
         -- @realm shared
-        -- @int Time to Enter Vehicle
+        -- @int Time to Enter Vehicle.
         TimeToEnterVehicle = 1,
 
         --- Allow duplicated character names.
         -- @realm shared
-        -- @bool AllowExistNames
+        -- @bool AllowExistNames.
         AllowExistNames = true,
 
         --- Name of the gamemode.
         -- @realm shared
-        -- @string Gamemode Name
+        -- @string Gamemode Name.
         GamemodeName = "A Lilia Gamemode",
 
         --- Theme color.
         -- @realm shared
-        -- @color Color
+        -- @color Color.
         Color = Color(34, 139, 34),
 
         --- Core font.
         -- @realm shared
-        -- @string Font
+        -- @string Font.
         Font = "Arial",
 
         --- Secondary font.
         -- @realm shared
-        -- @string Generic Font
+        -- @string Generic Font.
         GenericFont = "Segoe UI",
 
         --- Money model.
         -- @realm shared
-        -- @string Money Model
+        -- @string Money Model.
         MoneyModel = "models/props_lab/box01a.mdl",
 
         --- Singular currency name.
         -- @realm shared
-        -- @string Currency Singular Name
+        -- @string Currency Singular Name.
         CurrencySingularName = "Dollar",
 
         --- Plural currency name.
         -- @realm shared
-        -- @string Currency Plural Name
+        -- @string Currency Plural Name.
         CurrencyPluralName = "Dollars",
 
         --- Use American date format.
         -- @realm shared
-        -- @bool American Dates
+        -- @bool American Dates.
         AmericanDates = true,
 
         --- Use American timestamp format.
         -- @realm shared
-        -- @bool American TimeStamp
+        -- @bool American TimeStamp.
         AmericanTimeStamp = true,
 
         --- If Car Entry Delay is Applicable.
         -- @realm shared
-        -- @bool Car Entry Delay Enabled
+        -- @bool Car Entry Delay Enabled.
         CarEntryDelayEnabled = true,
 
         --- Notification sound and volume.
         -- @realm shared
-        -- @table Notify
+        -- @table Notify.
         Notify = {
-            "garrysmod/content_downloaded.wav", -- Notification Sound
-            50, -- Notification Volume
-            250 -- Notification Pitch
+            "garrysmod/content_downloaded.wav",
+            50,
+            250
         }
     }
     hook.Run("InitializedConfig")
