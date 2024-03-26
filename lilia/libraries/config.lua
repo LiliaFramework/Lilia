9380da45fd7b18c95b8c9adea8c6058ca7c01cbe
Leonheart <33399712+bleonheart@users.noreply.github.com>
2024-03-26 16:33:47 +0000

    Test Change

--- lilia/libraries/config.lua
+++ lilia/libraries/config.lua
@@ -1,40 +1,40 @@
 ﻿--[[--
 Configuration options for Lilia.
 
-This module contains various configuration options used in the Lilia gamemode. Each option in the `lia.config` table serves a specific purpose and defines various aspects of the gamemode's functionality.
+This module contains various configuration options used in the Lilia gamemode. Each option in the *lia.config* table serves a specific purpose and defines various aspects of the gamemode's functionality.
 
-- `WalkSpeed`: (integer) Controls how fast characters walk.
-- `RunSpeed`: (integer) Controls how fast characters run.
-- `WalkRatio`: (float) Defines the walk speed ratio when holding the Alt key.
-- `AllowExistNames`: (boolean) Determines whether duplicated character names are allowed.
-- `GamemodeName`: (string) Specifies the name of the gamemode.
-- `Color`: (color) Sets the theme color used throughout the gamemode.
-- `Font`: (string) Specifies the core font used for UI elements.
-- `GenericFont`: (string) Specifies the secondary font used for UI elements.
-- `MoneyModel`: (string) Defines the model used for representing money in the game.
-- `MaxCharacters`: (integer) Sets the maximum number of characters per player.
-- `DataSaveInterval`: (integer) Time interval between data saves.
-- `CharacterDataSaveInterval`: (integer) Time interval between character data saves.
-- `MoneyLimit`: (integer) Sets the limit of money a player can have (0 for infinite).
-- `invW`: (integer) Defines the width of the default inventory.
-- `invH`: (integer) Defines the height of the default inventory.
-- `DefaultMoney`: (integer) Specifies the default amount of money a player starts with.
-- `MaxChatLength`: (integer) Sets the maximum length of chat messages.
-- `CurrencySymbol`: (string) Specifies the currency symbol used in the game.
-- `SpawnTime`: (integer) Time to respawn after death.
-- `MaxAttributes`: (integer) Maximum attributes a character can have.
-- `CurrencySingularName`: (string) Singular name of the in-game currency.
-- `CurrencyPluralName`: (string) Plural name of the in-game currency.
-- `SchemaYear`: (integer) Year in the gamemode's schema.
-- `AmericanDates`: (boolean) Determines whether to use the American date format.
-- `AmericanTimeStamp`: (boolean) Determines whether to use the American timestamp format.
-- `MinDescLen`: (integer) Minimum length required for a character's description.
-- `TimeToEnterVehicle`: (integer) Time (in seconds) required to enter a vehicle.
-- `CarEntryDelayEnabled`: (boolean) Determines if the car entry delay is applicable.
-- `Notify`: (table) Contains notification sound and volume settings.
-  - `Notify[1]`: (string) Notification sound file path.
-  - `Notify[2]`: (integer) Notification volume.
-  - `Notify[3]`: (integer) Notification pitch.
+- *WalkSpeed*: (integer) Controls how fast characters walk.
+- *RunSpeed*: (integer) Controls how fast characters run.
+- *WalkRatio*: (float) Defines the walk speed ratio when holding the Alt key.
+- *AllowExistNames*: (boolean) Determines whether duplicated character names are allowed.
+- *GamemodeName*: (string) Specifies the name of the gamemode.
+- *Color*: (color) Sets the theme color used throughout the gamemode.
+- *Font*: (string) Specifies the core font used for UI elements.
+- *GenericFont*: (string) Specifies the secondary font used for UI elements.
+- *MoneyModel*: (string) Defines the model used for representing money in the game.
+- *MaxCharacters*: (integer) Sets the maximum number of characters per player.
+- *DataSaveInterval*: (integer) Time interval between data saves.
+- *CharacterDataSaveInterval*: (integer) Time interval between character data saves.
+- *MoneyLimit*: (integer) Sets the limit of money a player can have (0 for infinite).
+- *invW*: (integer) Defines the width of the default inventory.
+- *invH*: (integer) Defines the height of the default inventory.
+- *DefaultMoney*: (integer) Specifies the default amount of money a player starts with.
+- *MaxChatLength*: (integer) Sets the maximum length of chat messages.
+- *CurrencySymbol*: (string) Specifies the currency symbol used in the game.
+- *SpawnTime*: (integer) Time to respawn after death.
+- *MaxAttributes*: (integer) Maximum attributes a character can have.
+- *CurrencySingularName*: (string) Singular name of the in-game currency.
+- *CurrencyPluralName*: (string) Plural name of the in-game currency.
+- *SchemaYear*: (integer) Year in the gamemode's schema.
+- *AmericanDates*: (boolean) Determines whether to use the American date format.
+- *AmericanTimeStamp*: (boolean) Determines whether to use the American timestamp format.
+- *MinDescLen*: (integer) Minimum length required for a character's description.
+- *TimeToEnterVehicle*: (integer) Time (in seconds) required to enter a vehicle.
+- *CarEntryDelayEnabled*: (boolean) Determines if the car entry delay is applicable.
+- *Notify*: (table) Contains notification sound and volume settings.
+  - *Notify[1]*: (string) Notification sound file path.
+  - *Notify[2]*: (integer) Notification volume.
+  - *Notify[3]*: (integer) Notification pitch.
 
 These configuration options control various aspects of the gamemode's mechanics, user interface, and gameplay experience.
 ]]
