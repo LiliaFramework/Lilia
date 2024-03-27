d0f33920ad7c93ba0cb9a318d283ae9c396f47e2
Leonheart <33399712+bleonheart@users.noreply.github.com>
2024-03-27 16:33:32 +0000

    Prepared Attributes For Documentation

--- lilia/libraries/attributes.lua
+++ lilia/libraries/attributes.lua
@@ -1,4 +1,7 @@
-﻿lia.attribs = lia.attribs or {}
+﻿--- Helper library that manages roleplay improving attributes.
+-- @module lia.attribs
+
+lia.attribs = lia.attribs or {}
 lia.attribs.list = lia.attribs.list or {}
 function lia.attribs.loadFromDir(directory)
     for _, v in ipairs(file.Find(directory .. "/*.lua", "LUA")) do
@@ -43,3 +46,14 @@ lia.char.registerVar("attribs", {
     end,
     shouldDisplay = function(_) return table.Count(lia.attribs.list) > 0 end
 })
+
+if SERVER then
+    function lia.attribs.setup(client)
+        local character = client:getChar()
+        if character then
+            for k, v in pairs(lia.attribs.list) do
+                if v.onSetup then v:onSetup(client, character:getAttrib(k, 0)) end
+            end
+        end
+    end
+end
--- lilia/libraries/attributes/server.lua
+++
@@ -1,8 +0,0 @@
-﻿function lia.attribs.setup(client)
-    local character = client:getChar()
-    if character then
-        for k, v in pairs(lia.attribs.list) do
-            if v.onSetup then v:onSetup(client, character:getAttrib(k, 0)) end
-        end
-    end
-end
