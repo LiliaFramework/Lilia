b964a10bb6ef047d2d624f0d69f0d92f0dc6befa
Leonheart <33399712+bleonheart@users.noreply.github.com>
2024-03-28 23:07:27 +0000

    Raised Weapon Notification Changes

--- lilia/modules/characters/raiseweapons/meta/server.lua
+++ lilia/modules/characters/raiseweapons/meta/server.lua
@@ -1,5 +1,5 @@
 ﻿local playerMeta = FindMetaTable("Player")
-function playerMeta:setWepRaised(state)
+function playerMeta:setWepRaised(state, notification)
     self:setNetVar("raised", state)
     local weapon = self:GetActiveWeapon()
     if IsValid(weapon) then
@@ -7,11 +7,11 @@ function playerMeta:setWepRaised(state)
         weapon:SetNextSecondaryFire(CurTime() + 1)
     end
 
-    lia.chat.send(self, "iteminternal", state and "raises his weapon" or "lowers his weapon", false)
+    if notification then lia.chat.send(self, "iteminternal", state and "raises his weapon" or "lowers his weapon", false) end
 end
 
 function playerMeta:toggleWepRaised()
-    timer.Simple(1, function() self:setWepRaised(not self:isWepRaised()) end)
+    timer.Simple(1, function() self:setWepRaised(not self:isWepRaised(), true) end)
     local weapon = self:GetActiveWeapon()
     if IsValid(weapon) then
         if self:isWepRaised() and weapon.OnRaised then
