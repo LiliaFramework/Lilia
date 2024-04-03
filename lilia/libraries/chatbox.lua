cbe22b6db2fa741ccea8b77828811e4c883edaac
Leonheart <33399712+bleonheart@users.noreply.github.com>
2024-04-03 11:00:19 +0100

    Standalonized Chatbox

--- lilia/libraries/chatbox.lua
+++ lilia/libraries/chatbox.lua
@@ -94,3 +94,21 @@ function lia.chat.parse(client, message, noSend)
     if SERVER and not noSend then lia.chat.send(client, chatType, hook.Run("PlayerMessageSend", client, chatType, message, anonymous) or message, anonymous) end
     return chatType, message, anonymous
 end
+
+if SERVER then
+    function lia.chat.send(speaker, chatType, text, anonymous, receivers)
+        local class = lia.chat.classes[chatType]
+        if class and class.onCanSay(speaker, text) ~= false then
+            if class.onCanHear and not receivers then
+                receivers = {}
+                for _, v in ipairs(player.GetAll()) do
+                    if v:getChar() and class.onCanHear(speaker, v) ~= false then receivers[#receivers + 1] = v end
+                end
+    
+                if #receivers == 0 then return end
+            end
+    
+            netstream.Start(receivers, "cMsg", speaker, chatType, hook.Run("PlayerMessageSend", speaker, chatType, text, anonymous, receivers) or text, anonymous)
+        end
+    end
+end
