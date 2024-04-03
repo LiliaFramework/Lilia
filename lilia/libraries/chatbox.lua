5b21d806c30260f828e2c940c9eebfaac79e9ec1
Leonheart <33399712+bleonheart@users.noreply.github.com>
2024-04-03 11:45:14 +0100

    Linting

--- lilia/libraries/chatbox.lua
+++ lilia/libraries/chatbox.lua
@@ -104,10 +104,10 @@ if SERVER then
                 for _, v in ipairs(player.GetAll()) do
                     if v:getChar() and class.onCanHear(speaker, v) ~= false then receivers[#receivers + 1] = v end
                 end
-    
+
                 if #receivers == 0 then return end
             end
-    
+
             netstream.Start(receivers, "cMsg", speaker, chatType, hook.Run("PlayerMessageSend", speaker, chatType, text, anonymous, receivers) or text, anonymous)
         end
     end
