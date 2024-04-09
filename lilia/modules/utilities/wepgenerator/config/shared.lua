129b34c166416f16d9ca3545068a536bcbb82e24
Leonheart <33399712+bleonheart@users.noreply.github.com>
2024-04-09 02:04:10 +0100

    Removed Bases from auto weapon generator

--- lilia/modules/utilities/wepgenerator/config/shared.lua
+++ lilia/modules/utilities/wepgenerator/config/shared.lua
@@ -1,5 +1,5 @@
 ﻿MODULE.AutomaticWeaponRegister = true
-MODULE.RegisterWeaponsBlackList = {"sf2_tool", "weapon_fists", "weapon_medkit", "gmod_camera", "gmod_tool", "lightning_gun", "weapon_base", "lia_hands", "lia_keys",}
+MODULE.RegisterWeaponsBlackList = {"tfa_shotty_base", "tfa_melee_base", "tfa_3dbash_base", "tfa_bow_base", "tfa_gun_base", "cw_grenade_base", "tfa_melee_base", "cw_base", "weapon_swsft_awoi_base", "arccw_base_nade", "_base_launcher_", "tfa_knife_base", "tfa_bash_base", "tfa_scoped_base", "tfa_akimbo_base", "arccw_base_melee", "tfa_cssnade_base", "weapon_swsft_dod_base", "deika_base", "cw_melee_base", "tfa_nade_base", "tfa_3dscoped_base", "sf2_tool", "weapon_fists", "weapon_medkit", "gmod_camera", "gmod_tool", "lightning_gun", "weapon_base", "lia_hands", "lia_keys",}
 MODULE.WeaponNameOverrides = {
     [""] = "",
     [""] = "",
