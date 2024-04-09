465bbde58ece5878adbb1c4fb98ccaf534f0c2e5
Leonheart <33399712+bleonheart@users.noreply.github.com>
2024-04-09 01:17:24 +0100

    Auto Weapon Register Changes

--- lilia/modules/utilities/wepgenerator/config/shared.lua
+++ lilia/modules/utilities/wepgenerator/config/shared.lua
@@ -1,2 +1,12 @@
 ﻿MODULE.AutomaticWeaponRegister = true
 MODULE.RegisterWeaponsBlackList = {"sf2_tool", "weapon_fists", "weapon_medkit", "gmod_camera", "gmod_tool", "lightning_gun", "weapon_base", "lia_hands", "lia_keys",}
+MODULE.WeaponNameOverrides = {
+    [""] = "",
+    [""] = "",
+    [""] = "",
+    [""] = "",
+    [""] = "",
+    [""] = "",
+    [""] = "",
+    [""] = "",
+}
--- lilia/modules/utilities/wepgenerator/libraries/shared.lua
+++ lilia/modules/utilities/wepgenerator/libraries/shared.lua
@@ -6,7 +6,7 @@ function MODULE:RegisterWeapons()
     for _, wep in ipairs(weapons.GetList()) do
         if table.HasValue(self.RegisterWeaponsBlackList, wep.ClassName) or not wep.ClassName then continue end
         local ITEM = lia.item.register(wep.ClassName, "base_weapons", nil, nil, true)
-        ITEM.name = tostring(wep.PrintName) or "Undefined SWEP"
+        ITEM.name = (wep.PrintName or self.WeaponNameOverrides[wep.ClassName]) or wep.ClassName
         ITEM.desc = "A Weapon"
         ITEM.model = wep.WorldModel
         ITEM.class = wep.ClassName
@@ -14,5 +14,10 @@ function MODULE:RegisterWeapons()
         ITEM.height = 2
         ITEM.width = 2
         ITEM.category = "Weapons"
+        if ITEM.name == wep.ClassName then
+            print("Invalid Swep Item Name:", ITEM.name)
+        else
+            print("Generated weapon:", ITEM.name)
+        end
     end
 end
