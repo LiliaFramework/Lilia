e43c4a6b1fe0689428d073f949b68de56f44c439
Leonheart <33399712+bleonheart@users.noreply.github.com>
2024-04-14 00:53:06 +0100

    Added Recognition to PIM GiveMoney

--- lilia/modules/core/interactionmenu/netcalls/server.lua
+++ lilia/modules/core/interactionmenu/netcalls/server.lua
@@ -2,8 +2,8 @@
     if amount <= 0 and not client:getChar():hasMoney(amount) and not client or not target then return end
     target:getChar():giveMoney(amount)
     client:getChar():takeMoney(amount)
-    client:notify("You transfered " .. lia.currency.symbol .. amount .. " to " .. target:Nick(), NOT_CORRECT)
-    target:notify("You received " .. lia.currency.symbol .. amount .. " from " .. client:Nick(), NOT_CORRECT)
+    client:notify("You transfered " .. lia.currency.symbol .. amount .. " to " .. hook.Run("GetDisplayedName", target) or target:getChar():getName(), NOT_CORRECT)
+    target:notify("You received " .. lia.currency.symbol .. amount .. " from " .. hook.Run("GetDisplayedName", client) or client:getChar():getName(), NOT_CORRECT)
     lia.log.add("moneyGivenTAB", client, target:Name(), amount)
 end)
 
