a85f619c824c30d021bebba9a82478f4ecdb4067
Leonheart <33399712+bleonheart@users.noreply.github.com>
2024-04-16 21:32:53 +0000

    Documentation

--- lilia/gamemode/libraries/chatbox.lua
+++ lilia/gamemode/libraries/chatbox.lua
@@ -1,4 +1,24 @@
-﻿lia.chat = lia.chat or {}
+﻿--[[--
+Chat manipulation and helper functions.
+
+Chat messages are a core part of the framework - it's takes up a good chunk of the gameplay, and is also used to interact with
+the framework. Chat messages can have types or "classes" that describe how the message should be interpreted. All chat messages
+will have some type of class: `ic` for regular in-character speech, `me` for actions, `ooc` for out-of-character, etc. These
+chat classes can affect how the message is displayed in each player's chatbox. See `lia.chat.register`.
+to create your own chat classes.
+]]
+-- @module lia.chat
+
+lia.chat = lia.chat or {}
+
+
+--- List of all chat classes that have been registered by the framework, where each key is the name of the chat class, and value
+-- is the chat class data. Accessing a chat class's data is useful for when you want to copy some functionality or properties
+-- to use in your own. Note that if you're accessing this table, you should do so inside of the `InitializedChatClasses` hook.
+-- @realm shared
+-- @table lia.chat.classes
+-- @usage print(lia.chat.classes.ic.format)
+-- > "%s says \"%s\""
 lia.chat.classes = lia.char.classes or {}
 local DUMMY_COMMAND = {
     syntax = "<string text>",
@@ -9,7 +29,78 @@ local DUMMY_COMMAND = {
 function lia.chat.timestamp(ooc)
     return lia.config.ChatShowTime and (ooc and " " or "") .. "(" .. lia.date.GetFormattedDate(nil, false, false, false, false, true) .. ")" .. (ooc and "" or " ") or ""
 end
+-- note we can't use commas in the "color" field's default value since the metadata is separated by commas which will break the
+-- formatting for that field
 
+--- Chat messages can have different classes or "types" of messages that have different properties. This can include how the
+-- text is formatted, color, hearing distance, etc.
+-- @realm shared
+-- @table ChatClassStructure
+-- @see lia.chat.register
+-- @field[type=string] prefix What the player must type before their message in order to use this chat class. For example,
+-- having a prefix of `/y` will require to type `/y I am yelling` in order to send a message with this chat class. This can also
+-- be a table of strings if you want to allow multiple prefixes, such as `{"//", "/ooc"}`.
+--
+-- **NOTE:** the prefix should usually start with a `/` to be consistent with the rest of the framework. However, you are able
+-- to use something different like the `looc` chat class where the prefixes are `.//`, `[[`, and `/looc`.
+-- character in the prefix is non-alphanumeric (i.e `noSpaceAfter` with `/Y` will not work, but `/!` will).
+-- @field[type=string,opt="%s: \"%s\""] format How to format a message with this chat class. The first `%s` will be the speaking
+-- player's name, and the second one will be their message
+-- @field[type=color,opt=Color(242 230 160)] color Color to use when displaying a message with this chat class
+-- @field[type=bool,opt=false] bNoIndicator Whether or not to avoid showing the typing indicator above the speaking player's
+-- head
+-- @field[type=string,opt=liaChatFont] font Font to use for displaying a message with this chat class
+-- @field[type=bool,opt=false] deadCanChat Whether or not a dead player can send a message with this chat class
+-- @field[type=number] CanHear This can be either a `number` representing how far away another player can hear this message.
+-- IC messages will use the `chatRange` config, for example. This can also be a function, which returns `true` if the given
+-- listener can hear the message emitted from a speaker.
+-- 	-- message can be heard by any player 1000 units away from the speaking player
+-- 	CanHear = 1000
+-- OR
+-- 	CanHear = function(self, speaker, listener)
+-- 		-- the speaking player will be heard by everyone
+-- 		return true
+-- 	end
+-- @field[type=function,opt] onCanSay Function to run to check whether or not a player can send a message with this chat class.
+-- By default, it will return `false` if the player is dead and `deadCanChat` is `false`. Overriding this function will prevent
+-- `deadCanChat` from working, and you must implement this functionality manually.
+-- 	onCanSay = function(self, speaker, text)
+-- 		-- the speaker will never be able to send a message with this chat class
+-- 		return false
+-- 	end
+-- @field[type=function,opt] onGetColor Function to run to set the color of a message with this chat class. You should generally
+-- stick to using `color`, but this is useful for when you want the color of the message to change with some criteria.
+-- 	onGetColor = function(self, speaker, text)
+-- 		-- each message with this chat class will be colored a random shade of red
+-- 		return Color(math.random(120, 200), 0, 0)
+-- 	end
+-- @field[type=function,opt] onChatAdd Function to run when a message with this chat class should be added to the chatbox. If
+-- using this function, make sure you end the function by calling `chat.AddText` in order for the text to show up.
+--
+-- **NOTE:** using your own `onChatAdd` function will prevent `color`, `onGetColor`, or `format` from being used since you'll be
+-- overriding the base function that uses those properties. In such cases you'll need to add that functionality back in
+-- manually. In general, you should avoid overriding this function where possible. The `data` argument in the function is
+-- whatever is passed into the same `data` argument in `lia.chat.send`.
+--
+-- 	onChatAdd = function(self, speaker, text, bAnonymous, data)
+-- 		-- adds white text in the form of "Player Name: Message contents"
+-- 		chat.AddText(color_white, speaker:GetName(), ": ", text)
+-- 	end
+
+--- Registers a new chat type with the information provided. Chat classes should usually be created inside of the
+-- `InitializedChatClasses` hook.
+-- @realm shared
+-- @string chatType Name of the chat type
+-- @tparam ChatClassStructure data Properties and functions to assign to this chat class
+-- @usage -- this is the "me" chat class taken straight from the framework as an example
+-- lia.chat.register("me", {
+--     format = "**%s %s",
+--     onGetColor = lia.chat.classes.ic.onGetColor,
+--     onCanHear = ChatboxCore.ChatRange,
+--     prefix = {"/me", "/action"},
+--     deadCanChat = true
+-- })
+-- @see ChatClassStructure
 function lia.chat.register(chatType, data)
     if not data.onCanHear then
         if isfunction(data.radius) then
