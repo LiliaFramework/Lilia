8d5375bc0a5637228c5a637e821982ab6caebb63
Leonheart <33399712+bleonheart@users.noreply.github.com>
2024-04-16 03:42:36 +0100

    unholsters > takes out

--- lilia/gamemode/objects/items/base/weapons.lua
+++ lilia/gamemode/objects/items/base/weapons.lua
@@ -51,7 +51,7 @@ ITEM.functions.EquipUn = {
         client:EmitSound(item.unequipSound or "items/ammo_pickup.wav", 80)
         client.carryWeapons[item.weaponCategory] = nil
         item:setData("equip", nil)
-        lia.chat.send(client, "iteminternal", Format("holsters his %s.", item.name), false)
+        lia.chat.send(client, "iteminternal", Format("puts away his %s.", item.name), false)
         if item.onUnequipWeapon then item:onUnequipWeapon(client, weapon) end
         return false
     end,
@@ -83,7 +83,7 @@ ITEM.functions.Equip = {
             if ammoCount == weapon:Clip1() and item:getData("ammo", 0) == 0 then client:RemoveAmmo(weapon:Clip1(), weapon:GetPrimaryAmmoType()) end
             item:setData("equip", true)
             weapon:SetClip1(item:getData("ammo", 0))
-            lia.chat.send(client, "iteminternal", Format("unholsters his %s.", item.name), false)
+            lia.chat.send(client, "iteminternal", Format("takes out his %s.", item.name), false)
             if item.onEquipWeapon then item:onEquipWeapon(client, weapon) end
         else
             print(Format("[Lilia] Weapon %s does not exist!", item.class))
