c4bb0897ee3f65dc34b05e43cef7df75ecca7ae0
Leonheart <33399712+bleonheart@users.noreply.github.com>
2024-04-19 06:37:02 +0100

    Update faction.lua

--- docs/hooks/faction.lua
+++ docs/hooks/faction.lua
@@ -6,6 +6,7 @@ once it's created and assigned to a certain faction. For example, giving a polic
 These hooks are used in faction tables that are created in `schema/factions/sfactionname.lua` and cannot be used like
 regular gamemode hooks.
 ]]
+
 -- @hooks Faction
 --- Called when the default name for a character needs to be retrieved (i.e., upon initial creation).
 -- @realm shared
