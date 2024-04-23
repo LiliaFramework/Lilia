9b1593d849cfffee621f6506b4404a7c2a9da751
Leonheart <33399712+bleonheart@users.noreply.github.com>
2024-04-23 16:43:24 +0100

    Linting

--- lilia/modules/frameworkui/hud/libraries/cl_crosshair.lua
+++ lilia/modules/frameworkui/hud/libraries/cl_crosshair.lua
@@ -3,12 +3,10 @@
     local entity = Entity(client:getLocalVar("ragdoll", 0))
     local wep = client:GetActiveWeapon()
     if IsValid(wep) then
-    local className = wep:GetClass()
-    if className == "gmod_tool" or string.find(className, "lia_") or string.find(className, "detector_") then return true end
-    if not self.NoDrawCrosshairWeapon[wep:GetClass()] and self.CrosshairEnabled and IsValid(client) and client:Alive() and client:getChar()  and  npt IsValid(entity) wep and not (g_ContextMenu:IsVisible() or IsValid(lia.gui.character) and lia.gui.character:IsVisible()) then
-        return true
+        local className = wep:GetClass()
+        if className == "gmod_tool" or string.find(className, "lia_") or string.find(className, "detector_") then return true end
+        if not self.NoDrawCrosshairWeapon[wep:GetClass()] and self.CrosshairEnabled and IsValid(client) and client:Alive() and client:getChar() and not IsValid(entity) and wep and not (g_ContextMenu:IsVisible() or IsValid(lia.gui.character) and lia.gui.character:IsVisible()) then return true end
     end
-end
 end
 
 function MODULE:DrawCrosshair()
