b307fb6de078670fac8ab9f198a0930ad8aea5b8
Leonheart <33399712+bleonheart@users.noreply.github.com>
2024-04-28 08:26:14 +0100

    Update shared.lua

--- lilia/modules/characters/bodygrouper/config/shared.lua
+++ lilia/modules/characters/bodygrouper/config/shared.lua
@@ -1,3 +1,3 @@
 ﻿MODULE.BodygrouperOpenSound = "doors/door_metal_thin_open1.wav"
 MODULE.BodygrouperCloseSound = "doors/door_metal_thin_close2.wav"
-MODULE.BodygrouperModel = "models/props_wasteland/controlroom_storagecloset001a.mdl"
+MODULE.BodygrouperModel = "models/props_c17/FurnitureDresser001a.mdl"
