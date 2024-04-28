49ef23acbb277ae5209c20b8dd20b619045a173e
Leonheart <33399712+bleonheart@users.noreply.github.com>
2024-04-28 12:34:55 +0100

    Merged Code

--- lilia/modules/compatibility/advdupe/module.lua
+++
@@ -1,4 +0,0 @@
-﻿MODULE.name = "Compatibility - Advanced Dupe 1"
-MODULE.author = "76561198312513285"
-MODULE.discord = "@liliaplayer"
-MODULE.desc = "Adds Advanced Dupe 1 Compatibility to Lilia."
--- lilia/modules/compatibility/advdupe2/module.lua
+++
@@ -1,4 +0,0 @@
-﻿MODULE.name = "Compatibility - Advanced Dupe 2"
-MODULE.author = "76561198312513285"
-MODULE.discord = "@liliaplayer"
-MODULE.desc = "Adds Advanced Dupe 2 Compatibility to Lilia."
