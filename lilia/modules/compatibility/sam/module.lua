0bc7e499be47b6079a839852869790361edb1db5
Leonheart <33399712+bleonheart@users.noreply.github.com>
2024-05-08 10:55:10 +0000

    Test

--- lilia/modules/compatibility/sam/module.lua
+++ lilia/modules/compatibility/sam/module.lua
@@ -1,11 +1,15 @@
 ﻿--[[--
 
+Default database configuration for Lilia.
+This table defines the default settings for connecting to the database used in Lilia.
 
-- DefaultStaff: List of Default Staff and correspondent usergroups | **table**.
-
-- DisplayStaffCommands : Should staff commands pop in chat | **bool**.
+@table DatabaseConnection
+@realm server
+@field DefaultStaff List of Default Staff and corresponding usergroups | **table**
+@field DisplayStaffCommands Should staff commands appear in chat? | **bool**
 ]]
 -- @configurations SAM
+
 MODULE.name = "Compatibility - SAM"
 MODULE.author = "76561198312513285"
 MODULE.discord = "@liliaplayer"
