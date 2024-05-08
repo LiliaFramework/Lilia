178ef4e9be903164e3d9e34e29baa144f89f432d
Leonheart <33399712+bleonheart@users.noreply.github.com>
2024-05-08 14:53:18 +0000

    Linting

--- lilia/modules/compatibility/simfphys/module.lua
+++ lilia/modules/compatibility/simfphys/module.lua
@@ -3,9 +3,7 @@
 -- @table Configuration
 -- @field DamageInCars Either or not you take damage while in cars? | **bool**
 -- @field ValidCarDamages The valid types of damage on cars that should apply to the client | **table**
-
-
--- @configurations Simfphys 
+-- @configurations Simfphys
 MODULE.name = "Compatibility - Simfphys"
 MODULE.author = "76561198312513285"
 MODULE.discord = "@liliaplayer"
