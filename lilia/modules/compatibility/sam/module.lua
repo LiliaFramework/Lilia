1ad801e81fd1ba9cb5db8517eede6ee7a09aa566
Leonheart <33399712+bleonheart@users.noreply.github.com>
2024-05-08 12:37:31 +0000

    Testing

--- lilia/modules/compatibility/sam/module.lua
+++ lilia/modules/compatibility/sam/module.lua
@@ -1,4 +1,13 @@
-﻿MODULE.name = "Compatibility - SAM"
+﻿--- Configuration for SAM Compatibility Module.
+-- @configurations SAM
+
+
+-- Configuration for SAM Compatibility Module.
+-- @realm shared
+-- @table SAMConfiguration
+-- @field DefaultStaff List of Default Staff and corresponding usergroups | **table**
+-- @field DisplayStaffCommands Should staff commands appear in chat? | **bool**
+MODULE.name = "Compatibility - SAM"
 MODULE.author = "76561198312513285"
 MODULE.discord = "@liliaplayer"
 MODULE.desc = "Adds SAM Compatibility"
