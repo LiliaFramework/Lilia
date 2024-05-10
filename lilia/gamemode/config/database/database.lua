ecc1d5d3ba1168d5d4bdee14b140f9ed81e6c99e
Leonheart <33399712+bleonheart@users.noreply.github.com>
2024-05-10 15:36:46 +0100

    Update database.lua

--- lilia/gamemode/config/database/database.lua
+++ lilia/gamemode/config/database/database.lua
@@ -1,7 +1,8 @@
 ﻿--- Default database configuration for Lilia.
--- This table defines the default settings for connecting to the database used in Lilia.
 -- @configurations DatabaseConnection
 
+
+--- This table defines the default settings for connecting to the database used in Lilia.
 -- @table DefaultDatabaseDetails
 -- @realm server
 -- @field module Database module to be used (sqlite or mysqloo) | **string**
