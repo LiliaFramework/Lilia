e2134923a2b75113cee23db72c854b7d4ad9a1e8
Leonheart <33399712+bleonheart@users.noreply.github.com>
2024-05-16 21:19:26 +0100

    Formatting

--- lilia/modules/characters/raiseweapons/meta/server.lua
+++ lilia/modules/characters/raiseweapons/meta/server.lua
@@ -6,22 +6,21 @@ function playerMeta:setWepRaised(state, notification)
 		weapon:SetNextPrimaryFire(CurTime() + 1)
 		weapon:SetNextSecondaryFire(CurTime() + 1)
 	end
+
 	local weaponClass = weapon:GetClass()
 	local action = state and "raises" or "lowers"
 	local itemType = weaponClass == "lia_hands" and "hands" or "weapon"
-	if notification then
-		lia.chat.send(self, "iteminternal", action .. " his " .. itemType, false)
-	end
+	if notification then lia.chat.send(self, "iteminternal", action .. " his " .. itemType, false) end
 end
 
 function playerMeta:toggleWepRaised()
-    timer.Simple(1, function() self:setWepRaised(not self:isWepRaised(), true) end)
-    local weapon = self:GetActiveWeapon()
-    if IsValid(weapon) then
-        if self:isWepRaised() and weapon.OnRaised then
-            weapon:OnRaised()
-        elseif not self:isWepRaised() and weapon.OnLowered then
-            weapon:OnLowered()
-        end
-    end
+	timer.Simple(1, function() self:setWepRaised(not self:isWepRaised(), true) end)
+	local weapon = self:GetActiveWeapon()
+	if IsValid(weapon) then
+		if self:isWepRaised() and weapon.OnRaised then
+			weapon:OnRaised()
+		elseif not self:isWepRaised() and weapon.OnLowered then
+			weapon:OnLowered()
+		end
+	end
 end
