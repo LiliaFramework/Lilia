853e8e9fa989178710fa23dc86c7327e852eff52
Leonheart <33399712+bleonheart@users.noreply.github.com>
2024-05-20 02:19:05 +0100

    Update module.lua

--- lilia/modules/utilities/wepgenerator/module.lua
+++ lilia/modules/utilities/wepgenerator/module.lua
@@ -1,5 +1,6 @@
 ﻿--- Configuration for Easy Weapons Module.
 -- @configurations EasyWeapons
+
 --- This table defines the default settings for the EasyWeapons Module.
 -- @realm shared
 -- @table Configuration
