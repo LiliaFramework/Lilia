6e7825c05a5bf5ecf38a324bbccf2c6c5527eced
Leonheart <33399712+bleonheart@users.noreply.github.com>
2024-05-29 14:48:10 +0100

    Removed Undocumented Samples

--- lilia/modules/characters/inventory/docs/hooks.lua
+++
@@ -1,8 +0,0 @@
---- Hook Documentation for TEMPTEXT Module.
--- @hooksmodule WorkshopDownloader
-
---- Called when the third person mode is toggled.
---- @realm client
---- @bool state Indicates whether the third person mode is enabled (`true`) or disabled (`false`).
-function SampleFunction()
-end
