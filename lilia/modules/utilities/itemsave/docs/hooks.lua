3759d8ad71c94246444cf4d7cc27b3439312055a
Leonheart <33399712+bleonheart@users.noreply.github.com>
2024-05-29 14:41:28 +0100

    Documented Item Save

--- lilia/modules/utilities/itemsave/docs/hooks.lua
+++ lilia/modules/utilities/itemsave/docs/hooks.lua
@@ -1,8 +1,17 @@
---- Hook Documentation for TEMPTEXT Module.
--- @hooksmodule WorkshopDownloader
+--- Hook Documentation for Item Save Module.
+-- @hooksmodule ItemSave
 
---- Called when the third person mode is toggled.
---- @realm client
---- @bool state Indicates whether the third person mode is enabled (`true`) or disabled (`false`).
-function SampleFunction()
+--- Determines if saved items should be deleted on server restart.
+-- This hook is called to check whether saved items should be deleted when the server restarts.
+-- @realm server
+-- @treturn boolean True if saved items should be deleted, false otherwise.
+function ShouldDeleteSavedItems()
+end
+
+--- Called when saved items are loaded from the database.
+-- This hook is called after the saved items have been successfully loaded from the database.
+-- It can be used to perform additional actions with the loaded items.
+-- @realm server
+-- @param loadedItems A table containing the loaded item entities.
+function OnSavedItemLoaded(loadedItems)
 end
