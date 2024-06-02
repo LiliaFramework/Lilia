c1c20c60851f1bbf4366ac60ba5860b554e90c9a
Leonheart <33399712+bleonheart@users.noreply.github.com>
2024-06-02 11:38:30 +0100

    Documented F1 Menu

--- lilia/modules/frameworkui/f1menu/docs/hooks.lua
+++ lilia/modules/frameworkui/f1menu/docs/hooks.lua
@@ -1,8 +1,33 @@
---- Hook Documentation for TEMPTEXT Module.
--- @hooksmodule WorkshopDownloader
+--- Hook Documentation for F1 Menu Module.
+-- @hooksmodule F1Menu
 
---- Called when the third person mode is toggled.
---- @realm client
---- @bool state Indicates whether the third person mode is enabled (`true`) or disabled (`false`).
-function SampleFunction()
+--- Determines whether a specific character information should be displayed.
+-- @realm client
+-- @string name The name of the character information being checked.
+-- @treturn boolean Whether the character information should be displayed.
+function CanDisplayCharInfo(var)
+end
+
+--- Called when the player wants to view their inventory.
+-- @realm client
+-- @treturn boolean Whether the player is allowed to view their inventory.
+function CanPlayerViewInventory()
+end
+
+--- Called when the help menu is being built.
+-- @realm client
+-- @tab tabs Table containing the help menu tabs.
+function BuildHelpMenu(tabs)
+end
+
+--- Called when the menu buttons are being created.
+-- @realm client
+-- @tab tabs Table containing the menu buttons.
+function CreateMenuButtons(tabs)
+end
+
+--- Called after the player's inventory is drawn.
+-- @realm client
+-- @panel panel The panel containing the inventory.
+function PostDrawInventory(panel)
 end
---
+++ lilia/modules/frameworkui/mainmenu/docs/hooks.lua
@@ -0,0 +1,9 @@
+--- Hook Documentation for MainMenu Module.
+-- @hooksmodule MainMenu
+
+
+--- Called when the player wants to view their inventory.
+-- @realm client
+-- @treturn boolean Whether the player is allowed to view their inventory.
+function Temp()
+end
