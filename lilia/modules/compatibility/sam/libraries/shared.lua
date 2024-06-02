41f0f5979898310779ba120123257a6c05b7e858
Leonheart <33399712+bleonheart@users.noreply.github.com>
2024-06-02 10:47:24 +0100

    Linting

--- lilia/modules/compatibility/sam/libraries/shared.lua
+++ lilia/modules/compatibility/sam/libraries/shared.lua
@@ -23,7 +23,7 @@ function MODULE:PlayerSay(client, text)
 end
 
 lia.chat.register("asay", {
-    onCanSay = function(client) return true end,
+    onCanSay = function() return true end,
     onCanHear = function(_, client) return CAMI.PlayerHasAccess(client, "Staff Permissions - Read Admin Chat", nil) end,
     onChatAdd = function(client, text) if CAMI.PlayerHasAccess(client, "Staff Permissions - Read Admin Chat", nil) then chat.AddText(Color(255, 0, 0), "[Admin] ", speaker, " (" .. speaker:steamName() .. ") ", Color(0, 255, 0), ": " .. text) end end,
     font = "liaChatFont",
