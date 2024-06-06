e2e59b31d3f50fbb393b145299df1515c4431133
Leonheart <33399712+bleonheart@users.noreply.github.com>
2024-06-06 03:16:13 +0100

    Update database.lua

--- lilia/gamemode/config/database/database.lua
+++ lilia/gamemode/config/database/database.lua
@@ -1,16 +1,4 @@
-﻿--- Default database configuration for Lilia.
--- @configurationgeneral DatabaseConnection
-
---- This table defines the default settings for connecting to the database used in Lilia.
--- @table Configuration
--- @realm server
--- @field module Database module to be used (sqlite or mysqloo) | **string**
--- @field hostname Hostname of the database server | **string**
--- @field username Username for accessing the database | **string**
--- @field password Password for accessing the database | **string**
--- @field database Name of the database to connect to | **string**
--- @field port Port number for the database server | **number**
-DefaultDatabase = {
+﻿DefaultDatabase = {
     module = "sqlite",
     hostname = "127.0.0.1",
     username = "",
