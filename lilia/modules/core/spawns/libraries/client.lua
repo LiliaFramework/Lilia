d1735f4291917d0b509b3f3acddae24e64c97dba
Leonheart <33399712+bleonheart@users.noreply.github.com>
2024-07-02 15:55:32 +0100

    Made Screen Black wben dead

--- lilia/modules/core/spawns/libraries/client.lua
+++ lilia/modules/core/spawns/libraries/client.lua
@@ -7,8 +7,16 @@
 end
 
 function MODULE:HUDPaint()
-    if LocalPlayer():Alive() or LocalPlayer():GetNW2Int("deathTime", 0) > os.time() then return end
+    local client = LocalPlayer()
+    if client:Alive() or client:GetNW2Int("deathTime", 0) > os.time() then return end
     local xPos = ScrW() / 2
     local yPos = ScrH() / 2
     draw.DrawText(self.RespawnMessage, "liaHugeFont", xPos, yPos, Color(255, 255, 255, 255), TEXT_ALIGN_CENTER)
+end
+
+function MODULE:RenderScreenspaceEffects()
+    local client = LocalPlayer()
+    if client:Alive() or client:GetNW2Int("deathTime", 0) > os.time() then return end
+    surface.SetDrawColor(Color(0, 0, 0, 255))
+    surface.DrawRect(0, 0, ScrW(), ScrH())
 end
