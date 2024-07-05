de531f5ba81e3baf3c6d875dd9f57cd71048a92f
Leonheart <33399712+bleonheart@users.noreply.github.com>
2024-07-05 21:26:48 +0100

    Update server.lua

--- lilia/modules/core/interactionmenu/netcalls/server.lua
+++ lilia/modules/core/interactionmenu/netcalls/server.lua
@@ -1,26 +1,9 @@
-﻿netstream.Hook("transferMoneyFromP2P", function(client, value, target)
-    local amount = tonumber(value)
-    if not amount or amount <= 0 then
-        client:notify("Invalid amount specified.", NOT_CORRECT)
-        return
-    end
-
-    if not IsValid(client) or not IsValid(target) or not client:getChar() or not target:getChar() then
-        client:notify("Invalid client or target specified.", NOT_CORRECT)
-        return
-    end
-
-    if not client:getChar():hasMoney(amount) then
-        client:notify("You don't have enough money to transfer.", NOT_CORRECT)
-        return
-    end
-
+﻿netstream.Hook("transferMoneyFromP2P", function(client, amount, target)
+    if amount <= 0 and not client:getChar():hasMoney(amount) and not client or not target then return end
     target:getChar():giveMoney(amount)
     client:getChar():takeMoney(amount)
-    local clientDisplayName = hook.Run("GetDisplayedName", client) or client:getChar():getName()
-    local targetDisplayName = hook.Run("GetDisplayedName", target) or target:getChar():getName()
-    client:notify("You transferred " .. lia.currency.symbol .. amount .. " to " .. targetDisplayName, NOT_CORRECT)
-    target:notify("You received " .. lia.currency.symbol .. amount .. " from " .. clientDisplayName, NOT_CORRECT)
+    client:notify("You transfered " .. lia.currency.symbol .. amount .. " to " .. hook.Run("GetDisplayedName", target) or target:getChar():getName(), NOT_CORRECT)
+    target:notify("You received " .. lia.currency.symbol .. amount .. " from " .. hook.Run("GetDisplayedName", client) or client:getChar():getName(), NOT_CORRECT)
     lia.log.add(client, "moneyGiven", target:Name(), amount)
 end)
 
