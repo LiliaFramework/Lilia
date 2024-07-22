bde74cd2033288ad4de250dec0523aa0284590b6
Leonheart <33399712+bleonheart@users.noreply.github.com>
2024-07-23 00:34:39 +0100

    Update module.lua

--- lilia/modules/utilities/persistance/module.lua
+++ lilia/modules/utilities/persistance/module.lua
@@ -6,6 +6,5 @@ MODULE.discord = "@liliaplayer"
 MODULE.desc = "Adds a Module that implements persistance"
 MODULE.PersistingEntities = {}
 function entityMeta:IsLiliaPersistent()
-    print(self, self.IsLeonNPC, self.IsPersistent, table.HasValue(MODULE.PersistingEntities, self:GetClass()))
     return self.IsLeonNPC or self.IsPersistent or table.HasValue(MODULE.PersistingEntities, self:GetClass())
 end
