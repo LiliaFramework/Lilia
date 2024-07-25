82ffc2c424f5eebc4f1cc4becb34c31d0559fe42
Leonheart <33399712+bleonheart@users.noreply.github.com>
2024-07-25 02:33:49 +0100

    Update config.lua

--- lilia/modules/core/interactionmenu/docs/config.lua
+++ lilia/modules/core/interactionmenu/docs/config.lua
@@ -6,3 +6,4 @@
 -- @table Configuration
 -- @field MaxInteractionDistance Maximum distance for Interaction Menu to interact with other entities | **number**
 -- @field CarSearchRadius Search radius for Interaction Menu to find a car | **number**
+
