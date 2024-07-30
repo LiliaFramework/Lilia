375e0a7fbfa341cb5fd6b5137e220dd90aa7cfb3
Leonheart <33399712+bleonheart@users.noreply.github.com>
2024-07-30 16:45:24 +0100

    Update logs.lua

--- lilia/modules/utilities/logging/logs.lua
+++ lilia/modules/utilities/logging/logs.lua
@@ -203,7 +203,6 @@
         PLogs = "Toolgun",
         color = Color(52, 152, 219)
     },
-
     ["observerEnter"] = {
         func = function(client) return string.format("[%s] %s has entered observer. [CharID: %s]", client:SteamID(), client:Name(), client:getChar():getID()) end,
         category = "Staff Actions",
@@ -271,7 +270,7 @@
         color = Color(52, 152, 219)
     },
     ["roll"] = {
-        func = function(client, value) return string.format("%s rolled %d out of 100 [CharID: %s].", client:Name(), value,) end,
+        func = function(client, value) return string.format("%s rolled %d out of 100 [CharID: %s].", client:Name(), value, client:getChar():getID()) end,
         category = "Roll",
         PLogs = "Roll",
         color = Color(52, 152, 219)
