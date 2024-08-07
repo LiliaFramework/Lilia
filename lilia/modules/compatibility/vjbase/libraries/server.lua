cdae56c81b84c9700071a8baf9f1ff36c3249904
Leonheart <33399712+bleonheart@users.noreply.github.com>
2024-08-07 16:46:09 +0100

    Exploitable Net Message Notification

--- lilia/modules/compatibility/vjbase/libraries/server.lua
+++ lilia/modules/compatibility/vjbase/libraries/server.lua
@@ -1,4 +1,5 @@
 ﻿local vjThink = 0
+
 function MODULE:Think()
     if vjThink <= CurTime() then
         for k, v in pairs(self.VJBaseConsoleCommands) do
@@ -9,5 +10,14 @@ function MODULE:Think()
     end
 end
 
-hook.Remove("PlayerInitialSpawn", "VJBaseSpawn")
-hook.Remove("PlayerInitialSpawn", "drvrejplayerInitialSpawn")
+function MODULE:OnEntityCreated(entity)
+    if entity:GetClass() == "obj_vj_spawner_base" then entity:Remove() end
+end
+
+timer.Simple(10, function()
+    hook.Remove("PlayerInitialSpawn", "VJBaseSpawn")
+    hook.Remove("PlayerInitialSpawn", "drvrejplayerInitialSpawn")
+    concommand.Remove("vj_cleanup")
+end)
+
+lia.log.addType("unprotectedVJNetCall", function(client) return string.format("%s is trying to abuse unprotected VJ net calls.", client:Name()) end, "Exploits", Color(255, 0, 0), "Exploits")
