283fd0982c17f8814301b49bb6ba84a420d08635
Leonheart <33399712+bleonheart@users.noreply.github.com>
2024-08-10 23:21:25 +0100

    Upped SuperAdmin Permissions to Override Certain Things

--- lilia/modules/core/permissions/libraries/server.lua
+++ lilia/modules/core/permissions/libraries/server.lua
@@ -1,56 +1,58 @@
 ﻿local GM = GM or GAMEMODE
+
 function GM:PlayerSpawnEffect(client)
-    return client:isStaffOnDuty() or client:HasPrivilege("Spawn Permissions - Can Spawn Effects") or client:getChar():hasFlags("L")
+    return client:IsSuperAdmin() or client:isStaffOnDuty() or client:HasPrivilege("Spawn Permissions - Can Spawn Effects") or client:getChar():hasFlags("L")
 end
 
 function GM:PlayerSpawnNPC(client)
-    return client:isStaffOnDuty() or client:HasPrivilege("Spawn Permissions - Can Spawn NPCs") or client:getChar():hasFlags("n")
+    return client:IsSuperAdmin() or client:isStaffOnDuty() or client:HasPrivilege("Spawn Permissions - Can Spawn NPCs") or client:getChar():hasFlags("n")
 end
 
 function GM:PlayerSpawnProp(client)
-    return client:isStaffOnDuty() or client:HasPrivilege("Spawn Permissions - Can Spawn Props") or client:getChar():hasFlags("e")
+    return client:IsSuperAdmin() or client:isStaffOnDuty() or client:HasPrivilege("Spawn Permissions - Can Spawn Props") or client:getChar():hasFlags("e")
 end
 
 function GM:PlayerSpawnRagdoll(client)
-    return client:isStaffOnDuty() or client:HasPrivilege("Spawn Permissions - Can Spawn Ragdolls") or client:getChar():hasFlags("r")
+    return client:IsSuperAdmin() or client:isStaffOnDuty() or client:HasPrivilege("Spawn Permissions - Can Spawn Ragdolls") or client:getChar():hasFlags("r")
 end
 
 function GM:PlayerSpawnSENT(client)
-    return client:isStaffOnDuty() or client:HasPrivilege("Spawn Permissions - Can Spawn SENTs") or client:getChar():hasFlags("E")
+    return client:IsSuperAdmin() or client:isStaffOnDuty() or client:HasPrivilege("Spawn Permissions - Can Spawn SENTs") or client:getChar():hasFlags("E")
 end
 
 function GM:PlayerSpawnSWEP(client)
-    return client:isStaffOnDuty() or client:HasPrivilege("Spawn Permissions - Can Spawn SWEPs") or client:getChar():hasFlags("z")
+    return client:IsSuperAdmin() or client:isStaffOnDuty() or client:HasPrivilege("Spawn Permissions - Can Spawn SWEPs") or client:getChar():hasFlags("z")
 end
 
 function GM:PlayerSpawnVehicle(client)
-    return client:isStaffOnDuty() or client:HasPrivilege("Spawn Permissions - Can Spawn Cars") or client:getChar():hasFlags("C")
+    return client:IsSuperAdmin() or client:isStaffOnDuty() or client:HasPrivilege("Spawn Permissions - Can Spawn Cars") or client:getChar():hasFlags("C")
 end
 
 function GM:PlayerGiveSWEP(client)
-    return client:isStaffOnDuty() or client:HasPrivilege("Spawn Permissions - Can Spawn SWEPs") or client:getChar():hasFlags("W")
+    return client:IsSuperAdmin() or client:isStaffOnDuty() or client:HasPrivilege("Spawn Permissions - Can Spawn SWEPs") or client:getChar():hasFlags("W")
 end
 
 function GM:PlayerNoClip(client)
-    return (not client:isStaffOnDuty() and client:HasPrivilege("Staff Permissions - No Clip Outside Staff Character")) or client:isStaffOnDuty()
+    return (not client:isStaffOnDuty() and client:HasPrivilege("Staff Permissions - No Clip Outside Staff Character")) or client:IsSuperAdmin() or client:isStaffOnDuty()
 end
 
 function GM:OnPhysgunReload(_, client)
-    return client:HasPrivilege("Staff Permissions - Can Physgun Reload")
+    return client:IsSuperAdmin() or client:HasPrivilege("Staff Permissions - Can Physgun Reload")
 end
 
 function GM:CanTool(client, _, tool)
     local privilege = "Staff Permissions - Access Tool " .. tool:gsub("^%l", string.upper)
-    if (client:isStaffOnDuty() or client:getChar():hasFlags("t")) and client:HasPrivilege(privilege) then return true end
+    if client:IsSuperAdmin() or ((client:isStaffOnDuty() or client:getChar():hasFlags("t")) and client:HasPrivilege(privilege)) then return true end
 end
 
 function GM:CanProperty(client, property, entity)
     if entity:GetCreator() == client and (property == "remover" or property == "collision") then return true end
-    if client:HasPrivilege("Staff Permissions - Access Property " .. property:gsub("^%l", string.upper)) and client:isStaffOnDuty() then return true end
+    if client:IsSuperAdmin() or client:HasPrivilege("Staff Permissions - Access Property " .. property:gsub("^%l", string.upper)) and client:isStaffOnDuty() then return true end
     return false
 end
 
 function GM:PlayerSpawnObject(client)
+    if client:IsSuperAdmin() then return true end
     if not client.NextSpawn then client.NextSpawn = CurTime() end
     if client:HasPrivilege("Spawn Permissions - No Spawn Delay") then return true end
     if client.NextSpawn >= CurTime() then
@@ -63,6 +65,7 @@ function GM:PlayerSpawnObject(client)
 end
 
 function GM:PhysgunPickup(client, entity)
+    if client:IsSuperAdmin() then return true end
     if entity:GetCreator() == client and (entity:isProp() or entity:isItem()) then return true end
     if client:HasPrivilege("Staff Permissions - Physgun Pickup") then
         if entity:IsVehicle() then
