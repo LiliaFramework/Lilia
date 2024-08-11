3aa7b0468b054df269da06c99ac7d5798f65ae7b
Leonheart <33399712+bleonheart@users.noreply.github.com>
2024-08-11 10:30:09 +0100

    Moved FIle

--- lilia/modules/utilities/thirdperson/libraries/client.lua
+++ lilia/modules/utilities/thirdperson/libraries/client.lua
@@ -1,4 +1,8 @@
-﻿local view, traceData, traceData2, aimOrigin, crouchFactor, ft, curAng, diff, fm, sm
+﻿
+--- The Player Meta for the Third Person Module.
+-- @playermeta ThirdPerson
+local playerMeta = FindMetaTable("Player")
+local view, traceData, traceData2, aimOrigin, crouchFactor, ft, curAng, diff, fm, sm
 local ThirdPerson = CreateClientConVar("tp_enabled", 0, true)
 local ThirdPersonVerticalView = CreateClientConVar("tp_vertical", 10, true)
 local ThirdPersonHorizontalView = CreateClientConVar("tp_horizontal", 0, true)
@@ -151,6 +155,20 @@ function MODULE:PrePlayerDraw(drawnClient)
         drawnClient:DrawShadow(true)
         drawnClient.IsHidden = false
     end
+end
+
+--- Checks if a player can override the view to third-person.
+-- This function checks various conditions to determine if the player can switch to a third-person view.
+-- @realm shared
+-- @treturn boolean True if the player can override the view to third-person, false otherwise.
+function playerMeta:CanOverrideView()
+    local ragdoll = Entity(self:getLocalVar("ragdoll", 0))
+    local isInVehicle = self:hasValidVehicle()
+    if IsValid(lia.gui.char) then return false end
+    if isInVehicle then return false end
+    if not F1MenuCore.F1ThirdPersonEnabled and IsValid(lia.gui.menu) then return false end
+    if hook.Run("ShouldDisableThirdperson", self) == true then return false end
+    return ThirdPerson:GetBool() and ThirdPersonCore.ThirdPersonEnabled and (IsValid(self) and self:getChar() and not IsValid(ragdoll))
 end
 
 concommand.Add("tp_toggle", function()
