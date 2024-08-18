19e0308fbfb0880996f4871511a6c0afde622da7
Leonheart <33399712+bleonheart@users.noreply.github.com>
2024-08-18 11:03:25 +0100

    Added PlayerShouldPermaKill Hook

--- lilia/modules/characters/permakill/commands/server.lua
+++ lilia/modules/characters/permakill/commands/server.lua
@@ -5,11 +5,11 @@
     onRun = function(client, arguments)
         local target = lia.command.findPlayer(client, arguments[1])
         local tcharacter = target:getChar()
-        if tcharacter:getData("canbepermakilled", false) then
-            tcharacter:setData("canbepermakilled", false)
+        if tcharacter:getData("PermaKillFlagged", false) then
+            tcharacter:setData("PermaKillFlagged", false)
             client:notify("You have toggled this character's PK State to False.")
         else
-            tcharacter:setData("canbepermakilled", true)
+            tcharacter:setData("PermaKillFlagged", true)
             client:notify("You have toggled this character's PK State to True. He will be PK'ed the next time he dies!")
         end
     end
--- lilia/modules/characters/permakill/libraries/server.lua
+++ lilia/modules/characters/permakill/libraries/server.lua
@@ -1,5 +1,9 @@
 ﻿function MODULE:PlayerDeath(client, inflictor, attacker)
     local character = client:getChar()
     if (attacker == client or inflictor:IsWorld() or attacker:GetClass() == "worldspawn") and not self.PKWorld then return end
-    if character:getData("canbepermakilled", false) then character:ban() end
+    if hook.Run("PlayerShouldPermaKill", client, inflictor, attacker) == true then character:ban() end
+end
+
+function MODULE:PlayerShouldPermaKill(client, inflictor, attacker)
+    return character:getData("PermaKillFlagged", false)
 end
