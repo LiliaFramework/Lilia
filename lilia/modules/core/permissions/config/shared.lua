e02094d94d2c503478fcc04c044b8a58f9accbef
Leonheart <33399712+bleonheart@users.noreply.github.com>
2024-08-31 00:35:57 +0100

    Update shared.lua

--- lilia/modules/core/permissions/config/shared.lua
+++ lilia/modules/core/permissions/config/shared.lua
@@ -1,3 +1,4 @@
 ﻿MODULE.SpawnMenuLimit = false
 MODULE.NoClipESPEntities = {"lia_bodygrouper", "lia_vendor",}
 MODULE.DefaultUserTools = {"adv_duplicator", "duplicator", "advdupe2", "remover",}
+MODULE.DisallowedTools = {"rope", "light", "lamp", "dynamite", "physprop", "faceposer", "stacker",}
