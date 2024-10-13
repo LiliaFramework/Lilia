1d3c97404f920aa480bcbddb7f96765d5db91c31
Samael <33399712+bleonheart@users.noreply.github.com>
2024-10-13 16:09:26 +0100

    Update shared.lua

--- lilia/modules/core/administration/submodules/sam/libraries/shared.lua
+++ lilia/modules/core/administration/submodules/sam/libraries/shared.lua
@@ -11,14 +11,16 @@ function sam.player.send_message(client, msg, tbl)
         if sam.isconsole(client) then
             local result = sam.format_message(msg, tbl)
             sam.print(unpack(result, 1, result.__cnt))
-        else
+        elseif client then
             return sam.netstream.Start(client, "send_message", msg, tbl)
         end
     else
-        local prefix_result = sam.format_message(sam.config.get("ChatPrefix", ""))
-        local prefix_n = #prefix_result
-        local result = sam.format_message(msg, tbl, prefix_result, prefix_n)
-        if MODULE:CanReadNotifications(client) then chat.AddText(unpack(result, 1, result.__cnt)) end
+        if client and MODULE:CanReadNotifications(client) then
+            local prefix_result = sam.format_message(sam.config.get("ChatPrefix", ""))
+            local prefix_n = #prefix_result
+            local result = sam.format_message(msg, tbl, prefix_result, prefix_n)
+            chat.AddText(unpack(result, 1, result.__cnt))
+        end
     end
 end
 
