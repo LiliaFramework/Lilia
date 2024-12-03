f0df986468b6cd8a602c3630973119447f2f8d7f
Samael <33399712+bleonheart@users.noreply.github.com>
2024-12-03 17:20:19 +0000

    Added GamemodeVersion & WatermarkLogo to HUD config

--- lilia/modules/frameworkui/hud/libraries/cl_watermark.lua
+++ lilia/modules/frameworkui/hud/libraries/cl_watermark.lua
@@ -1,15 +1,23 @@
 function MODULE:DrawWatermark()
+    if not self:ShouldDrawWatermark() then return end
     local w, h = 64, 64
-    surface.SetMaterial(Material(SCHEMA.Logo))
-    surface.SetDrawColor(255, 255, 255, 80)
-    surface.DrawTexturedRect(5, ScrH() - h - 5, w, h)
-    surface.SetTextColor(255, 255, 255, 80)
-    surface.SetFont("WB_XLarge")
-    local _, th = surface.GetTextSize(SCHEMA.Version)
-    surface.SetTextPos(15 + w, ScrH() + 15 - h / 2 - th / 2)
-    surface.DrawText(SCHEMA.Version)
+    local watermarkLogo = Material(self.WatermarkLogo, "smooth")
+    local versionText = tostring(self.GamemodeVersion)
+    if isstring(self.WatermarkLogo) and self.WatermarkLogo ~= "" then
+        surface.SetMaterial(watermarkLogo)
+        surface.SetDrawColor(255, 255, 255, 80)
+        surface.DrawTexturedRect(5, ScrH() - h - 5, w, h)
+    end
+
+    if versionText ~= "" then
+        surface.SetFont("WB_XLarge")
+        local _, textHeight = surface.GetTextSize(versionText)
+        surface.SetTextColor(255, 255, 255, 80)
+        surface.SetTextPos(15 + w, ScrH() - h / 2 - textHeight / 2)
+        surface.DrawText(versionText)
+    end
 end
 
 function MODULE:ShouldDrawWatermark()
-    return SCHEMA.Version and isstring(SCHEMA.Version) and SCHEMA.Logo and isstring(SCHEMA.Logo) and self.WatermarkEnabled
+    return self.WatermarkEnabled and isstring(self.GamemodeVersion) and self.GamemodeVersion ~= "" and isstring(self.WatermarkLogo) and self.WatermarkLogo ~= ""
 end
