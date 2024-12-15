017bce8ed8332889a82d307720d03c3b661ff084
Samael <33399712+bleonheart@users.noreply.github.com>
2024-12-15 21:11:55 +0000

    Chatbox autocompletes command to which you can access

--- lilia/modules/frameworkui/chatbox/derma/cl_chatbox.lua
+++ lilia/modules/frameworkui/chatbox/derma/cl_chatbox.lua
@@ -28,24 +28,26 @@ function PANEL:Init()
             local text = entry:GetText()
             if text:sub(1, 1) == "/" then
                 local arguments = self.arguments or {}
-                local command = string.PatternSafe(arguments[1] or ""):lower()
+                local userCommand = string.PatternSafe(arguments[1] or ""):lower()
                 lia.util.drawBlur(this)
                 surface.SetDrawColor(0, 0, 0, 200)
                 surface.DrawRect(0, 0, w, h)
                 local i = 0
                 local color = lia.config.Color
-                for k, v in SortedPairs(lia.command.list) do
-                    local k2 = "/" .. k
-                    local k = k:lower()
-                    if k2:lower():match(command) then
-                        local x = lia.util.drawText((v.realCommand and "/" .. v.realCommand or k2) .. "  ", 4, i * 20, color)
-                        if k == command and v.syntax then
+                for cmdName, cmdData in SortedPairs(lia.command.list) do
+                    local fullCmd = "/" .. cmdName
+                    local cmdNameLower = cmdName:lower()
+                    local hasAccess, _ = lia.command.hasAccess(LocalPlayer(), cmdNameLower)
+                    if fullCmd:lower():match(userCommand) and hasAccess then
+                        local displayCommand = cmdData.realCommand and "/" .. cmdData.realCommand or fullCmd
+                        local x = lia.util.drawText(displayCommand .. "  ", 4, i * 20, color)
+                        if cmdNameLower == userCommand and cmdData.syntax then
                             local i2 = 0
-                            for argument in v.syntax:gmatch("([%[<][%w_]+[%s][%w_]+[%]>])") do
+                            for argument in cmdData.syntax:gmatch("([%[<][%w_]+[%s][%w_]+[%]>])") do
                                 i2 = i2 + 1
-                                local color = Color(200, 200, 200, 100)
-                                if i2 == (#arguments - 1) then color = color_white end
-                                x = x + lia.util.drawText(argument .. "  ", x, i * 20, color)
+                                local argColor = Color(200, 200, 200, 100)
+                                if i2 == (#arguments - 1) then argColor = color_white end
+                                x = x + lia.util.drawText(argument .. "  ", x, i * 20, argColor)
                             end
                         end
 
