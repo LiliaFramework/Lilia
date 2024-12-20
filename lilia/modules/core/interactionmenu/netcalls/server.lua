dc3ad7134c25e75dce0283c12a7e111c4687ae3f
Samael <33399712+bleonheart@users.noreply.github.com>
2024-12-20 11:30:46 +0000

    Update server.lua

--- lilia/modules/core/interactionmenu/netcalls/server.lua
+++ lilia/modules/core/interactionmenu/netcalls/server.lua
@@ -1,13 +1,4 @@
-﻿netstream.Hook("transferMoneyFromP2P", function(client, amount, target, targetName, clientName)
-    if amount <= 0 or not client:getChar():hasMoney(amount) or not client or not target then return end
-    target:getChar():giveMoney(amount)
-    client:getChar():takeMoney(amount)
-    client:notify("You transferred " .. lia.currency.symbol .. amount .. " to " .. targetName)
-    target:notify("You received " .. lia.currency.symbol .. amount .. " from " .. clientName)
-    lia.log.add(client, "moneyGiven", target:Name(), amount)
-end)
-
-netstream.Hook("PIMRunOption", function(client, name)
+﻿netstream.Hook("PIMRunOption", function(client, name)
     local opt = PIM.Options[name]
     if opt then
         if opt.runServer then
@@ -31,5 +22,5 @@ netstream.Hook("PIMRunLocalOption", function(client, name)
     end
 end)
 
-lia.log.addType("P2PAction", function(client, message, name, entity) return string.format("[%s] %s ran a P2P Action '%s' on entity '%s'.", client:SteamID(), client:Name(), name, entity) end, "P2P Actions", Color(255, 165, 0))
-lia.log.addType("PersonalAction", function(client, message, name) return string.format("[%s] %s ran a Personal Action '%s'.", client:SteamID(), client:Name(), name) end, "Personal Actions", Color(255, 140, 0))
+lia.log.addType("P2PAction", function(client, name, entity) return string.format("[%s] %s ran a P2P Action '%s' on entity '%s'.", client:SteamID(), client:Name(), name, entity) end, "P2P Actions", Color(255, 165, 0))
+lia.log.addType("PersonalAction", function(client, name) return string.format("[%s] %s ran a Personal Action '%s'.", client:SteamID(), client:Name(), name) end, "Personal Actions", Color(255, 140, 0))
