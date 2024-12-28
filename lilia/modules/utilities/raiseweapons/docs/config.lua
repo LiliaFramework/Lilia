0e117b5014217804d5f727d80c30bd5c15d4231c
Samael <33399712+bleonheart@users.noreply.github.com>
2024-12-28 17:44:55 +0000

    Made Raise Weapons Optional

--- lilia/modules/utilities/raiseweapons/docs/config.lua
+++
@@ -1,9 +0,0 @@
-﻿--- Configuration for the Raised Weapons Module.
--- @configuration RaisedWeapons
-
---- This table defines the default settings for the RaisedWeapons Module.
--- @realm shared
--- @table Configuration
--- @field PermaRaisedWeapons Weapons that are always raised | **table**
--- @field WeaponToggleDelay Time Between Weapon Toggles | **number**
--- @field ShouldPrintMessage Wether or not weapon raising messages should print in chat | **boolean**
--- lilia/modules/utilities/raiseweapons/module.lua
+++
@@ -1,6 +0,0 @@
-﻿MODULE.name = "Raised Weapons"
-MODULE.author = "76561198312513285"
-MODULE.discord = "@liliaplayer"
-MODULE.version = "Stock"
-MODULE.desc = "Adds a safety system that holsters sweps."
-MODULE.identifier = "RaisedWeaponCore"
