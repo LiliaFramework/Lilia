28a4eaa0738ad4120f9a5711952c63b5d003ae3d
Samael <33399712+bleonheart@users.noreply.github.com>
2025-01-09 01:00:01 +0000

    Added PopulateAdminStick Hook

--- lilia/modules/core/administration/libraries/client.lua
+++ lilia/modules/core/administration/libraries/client.lua
@@ -595,118 +595,9 @@ function MODULE:OpenAdminStickUI(target)
 
                 botOption:SetIcon("icon16/comment.png")
             end
-        elseif target:isDoor() then
-            AdminMenu:AddOption("Toggle Door Hidden", function()
-                LocalPlayer():ConCommand('say /doortogglehidden')
-                AdminStickIsOpen = false
-            end):SetIcon("icon16/eye.png")
-
-            AdminMenu:AddOption("Toggle Door Ownable", function()
-                LocalPlayer():ConCommand('say /doortoggleownable')
-                AdminStickIsOpen = false
-            end):SetIcon("icon16/house.png")
-
-            AdminMenu:AddOption("Set Door Title", function()
-                Derma_StringRequest("Set Door Title", "Enter the title for this door:", "", function(text)
-                    if text and text ~= "" then LocalPlayer():ConCommand('say /doorsettitle "' .. text .. '"') end
-                    AdminStickIsOpen = false
-                end)
-            end):SetIcon("icon16/tag.png")
-
-            local factionsAssignedRaw = target:getNetVar("factions", "[]")
-            local factionsAssigned = util.JSONToTable(factionsAssignedRaw) or {}
-            local addFactionMenu = AdminMenu:AddSubMenu("Add Faction to Door")
-            for _, faction in pairs(lia.faction.teams) do
-                if not factionsAssigned[faction.index] then
-                    addFactionMenu:AddOption(faction.name, function()
-                        LocalPlayer():ConCommand('say /dooraddfaction "' .. faction.uniqueID .. '"')
-                        AdminStickIsOpen = false
-                    end)
-                end
-            end
-
-            if table.Count(factionsAssigned) > 0 then
-                local removeFactionMenu = AdminMenu:AddSubMenu("Remove Faction from Door")
-                for id, _ in pairs(factionsAssigned) do
-                    for _, faction in pairs(lia.faction.teams) do
-                        if faction.index == id then
-                            removeFactionMenu:AddOption(faction.name, function()
-                                LocalPlayer():ConCommand('say /doorremovefaction "' .. faction.uniqueID .. '"')
-                                AdminStickIsOpen = false
-                            end)
-                        end
-                    end
-                end
-            else
-                AdminMenu:AddOption("No factions assigned to this door"):SetEnabled(false)
-            end
-
-            AdminMenu:AddOption("View Door Info", function()
-                LocalPlayer():ConCommand('say /doorinfo')
-                AdminStickIsOpen = false
-            end):SetIcon("icon16/information.png")
-
-            AdminMenu:AddOption("Force Lock Door", function()
-                LocalPlayer():ConCommand('say /doorforcelock')
-                AdminStickIsOpen = false
-            end):SetIcon("icon16/lock.png")
-
-            AdminMenu:AddOption("Force Unlock Door", function()
-                LocalPlayer():ConCommand('say /doorforceunlock')
-                AdminStickIsOpen = false
-            end):SetIcon("icon16/lock_open.png")
-
-            AdminMenu:AddOption("Reset Door Data", function()
-                LocalPlayer():ConCommand('say /doorresetdata')
-                AdminStickIsOpen = false
-            end):SetIcon("icon16/arrow_refresh.png")
-
-            AdminMenu:AddOption("Set Door Parent", function()
-                LocalPlayer():ConCommand('say /doorsetparent')
-                AdminStickIsOpen = false
-            end):SetIcon("icon16/link.png")
-
-            AdminMenu:AddOption("Set Door Child", function()
-                LocalPlayer():ConCommand('say /doorsetchild')
-                AdminStickIsOpen = false
-            end):SetIcon("icon16/link_add.png")
-
-            AdminMenu:AddOption("Remove Child Link", function()
-                LocalPlayer():ConCommand('say /doorremovechild')
-                AdminStickIsOpen = false
-            end):SetIcon("icon16/link_break.png")
-
-            AdminMenu:AddOption("Set Door Class", function()
-                Derma_StringRequest("Set Door Class", "Enter a class name:", "", function(text)
-                    LocalPlayer():ConCommand('say /doorsetclass ' .. text)
-                    AdminStickIsOpen = false
-                end)
-            end):SetIcon("icon16/user_suit.png")
-
-            AdminMenu:AddOption("Save Doors", function()
-                LocalPlayer():ConCommand('say /savedoors')
-                AdminStickIsOpen = false
-            end):SetIcon("icon16/disk.png")
-        elseif target:GetClass() == "lia_vendor" then
-            AdminMenu:AddOption("Restock Vendor", function()
-                LocalPlayer():ConCommand("say /restockvendor")
-                AdminStickIsOpen = false
-            end):SetIcon("icon16/arrow_refresh.png")
-
-            AdminMenu:AddOption("Restock Vendor Money", function()
-                Derma_StringRequest("Restock Vendor Money", "Enter amount:", "", function(amount)
-                    LocalPlayer():ConCommand("say /restockvendormoney " .. amount)
-                    AdminStickIsOpen = false
-                end)
-            end):SetIcon("icon16/money_add.png")
-        elseif target:GetClass() ~= "worldspawn" then
-            local entityOption = AdminMenu:AddOption("Get Entity Name", function()
-                LocalPlayer():ConCommand("say /entityInfo")
-                AdminStickIsOpen = false
-            end)
-
-            entityOption:SetIcon("icon16/eye.png")
         end
+
+        hook.Run("PopulateAdminStick", AdminMenu, target)
     end
 
     function AdminMenu:OnClose()
@@ -870,13 +761,13 @@ function MODULE:TicketFrame(requester, message, claimed)
 
     closeButton.DoClick = function() frm:Close() end
     frm:ShowCloseButton(false)
-    frm:SetPos(xpos, ypos + (130 * #TicketFrames))
-    frm:MoveTo(xpos, ypos + (130 * #TicketFrames), 0.2, 0, 1, function() surface.PlaySound("garrysmod/balloon_pop_cute.wav") end)
+    frm:SetPos(xpos, ypos + 130 * #TicketFrames)
+    frm:MoveTo(xpos, ypos + 130 * #TicketFrames, 0.2, 0, 1, function() surface.PlaySound("garrysmod/balloon_pop_cute.wav") end)
     function frm:OnRemove()
         if TicketFrames then
             table.RemoveByValue(TicketFrames, frm)
             for k, v in ipairs(TicketFrames) do
-                v:MoveTo(xpos, ypos + (130 * (k - 1)), 0.1, 0, 1, function() end)
+                v:MoveTo(xpos, ypos + 130 * (k - 1), 0.1, 0, 1, function() end)
             end
         end
 
