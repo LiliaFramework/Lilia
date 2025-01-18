0c563357fbcf64ca754a741ea76d612537a9dfa5
Samael <33399712+bleonheart@users.noreply.github.com>
2025-01-18 04:35:28 +0000

    Moved SetupBotPlayer to a hook rather than a GM function

--- lilia/gamemode/core/hooks/server.lua
+++ lilia/gamemode/core/hooks/server.lua
@@ -376,7 +376,7 @@ end
 
 function GM:PlayerInitialSpawn(client)
     if client:IsBot() then
-        self:SetupBotPlayer(client)
+        hook.Run("SetupBotPlayer", client)
         return
     end
 
@@ -461,4 +461,42 @@ end
 function GM:PlayerDeathThink(client)
     if client:getChar() and not client:GetNW2Bool("IsDeadRestricted", false) then client:Spawn() end
     return false
+end
+
+function GM:SaveData()
+    local data = {}
+    for _, v in ents.Iterator() do
+        if v.IsLeonNPC or v.IsPersistent then
+            data[#data + 1] = {
+                pos = v:GetPos(),
+                class = v:GetClass(),
+                model = v:GetModel(),
+                angles = v:GetAngles(),
+            }
+        end
+    end
+
+    self:setData(data)
+end
+
+local function IsEntityNearby(pos, class)
+    for _, ent in ipairs(ents.FindByClass(class)) do
+        if ent:GetPos():Distance(pos) <= 50 then return true end
+    end
+    return false
+end
+
+function GM:LoadData()
+    for _, v in pairs(self:getData() or {}) do
+        if not IsEntityNearby(v.pos, v.class) then
+            local ent = ents.Create(v.class)
+            if IsValid(ent) then
+                if v.pos then ent:SetPos(v.pos) end
+                if v.angles then ent:SetAngles(v.angles) end
+                if v.model then ent:SetModel(v.model) end
+                ent:Spawn()
+                ent:Activate()
+            end
+        end
+    end
 end
