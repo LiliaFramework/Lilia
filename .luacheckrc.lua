ea1211495695493c4133c1800d4b399dbb004917
Samael <33399712+bleonheart@users.noreply.github.com>
2025-01-18 23:00:42 +0000

    Readded Lua Checker whitelist

---
+++ .luacheckrc.lua
@@ -0,0 +1,9 @@
+exclude_files = {
+    "lilia/modules/utilities/logging/logs.lua",
+    "lilia/gamemode/core/libraries/thirdparty/**/*.lua",
+    "lilia/modules/**/docs/*.lua",
+    "lilia/lilia.txt",
+    "lilia/lilia.txt",
+    "docs/*.lua",
+    "docs/**/*.lua",
+}
