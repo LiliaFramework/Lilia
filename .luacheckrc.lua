dea0d9594111081ba1ec00581873662217567516
Samael <33399712+bleonheart@users.noreply.github.com>
2025-01-18 23:32:09 +0000

    Update .luacheckrc.lua

--- .luacheckrc.lua
+++ .luacheckrc.lua
@@ -1,7 +1,4 @@
 exclude_files = {
-    "lilia/modules/utilities/logging/logs.lua",
     "lilia/gamemode/core/libraries/thirdparty/*.lua",
-    "lilia/modules/**/docs/*.lua",
-    "lilia/lilia.txt",
     "lilia/lilia.txt",
 }
