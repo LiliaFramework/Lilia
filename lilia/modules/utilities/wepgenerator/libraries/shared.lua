a23b951266f4124d0b44c22797b12c761478ea07
Samael <33399712+bleonheart@users.noreply.github.com>
2025-01-18 04:34:49 +0000

    Changed Weapon Generator Formatting

--- lilia/modules/utilities/wepgenerator/libraries/shared.lua
+++ lilia/modules/utilities/wepgenerator/libraries/shared.lua
@@ -1,14 +1,35 @@
 ﻿local AutomaticWeaponRegister = true
 local NotifyWeaponRegister = false
-function MODULE:InitializedModules()
-    if AutomaticWeaponRegister then self:RegisterWeapons() end
-end
+local WeaponOverrides = {
+    ["weapon_pistol"] = {
+        name = "Custom Pistol",
+        desc = "A custom description for the pistol.",
+        model = "models/weapons/w_custom_pistol.mdl",
+        class = "weapon_pistol",
+        height = 2,
+        width = 2,
+        category = "weapons",
+        weaponCategory = "sidearm",
+        RequiredSkillLevels = nil,
+    },
+}
 
-function MODULE:RegisterWeapons()
+local RegisterWeaponsBlackList = {
+    sf2_tool = true,
+    weapon_fists = true,
+    weapon_medkit = true,
+    gmod_camera = true,
+    gmod_tool = true,
+    lightning_gun = true,
+    lia_hands = true,
+    lia_keys = true,
+}
+
+local function RegisterWeapons()
     for _, wep in ipairs(weapons.GetList()) do
         local className = wep.ClassName
-        if not className or className:find("_base") or self.RegisterWeaponsBlackList[className] then continue end
-        local override = self.WeaponOverrides and self.WeaponOverrides[className] or {}
+        if not className or className:find("_base") or RegisterWeaponsBlackList[className] then continue end
+        local override = WeaponOverrides and WeaponOverrides[className] or {}
         local ITEM = lia.item.register(className, "base_weapons", nil, nil, true)
         ITEM.name = override.name or wep.PrintName or className
         ITEM.desc = override.desc or "A Weapon"
@@ -21,4 +42,8 @@ function MODULE:RegisterWeapons()
         ITEM.category = override.category or "Weapons"
         if ITEM.name ~= className and NotifyWeaponRegister then LiliaInformation("Generated weapon: " .. ITEM.name) end
     end
+end
+
+function MODULE:InitializedModules()
+    if AutomaticWeaponRegister then RegisterWeapons() end
 end
