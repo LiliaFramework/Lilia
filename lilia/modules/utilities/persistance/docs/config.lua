e1e90147f176028b8cfc6a232b598273af74114c
Samael <33399712+bleonheart@users.noreply.github.com>
2025-01-18 04:36:04 +0000

    Removed Persistance

--- lilia/modules/utilities/persistance/docs/config.lua
+++
@@ -1,6 +0,0 @@
-﻿--- Configuration for the Persistance Module.
--- @configuration Persistance
---- This table defines the default settings for the Persistance Module.
--- @realm shared
--- @table Configuration
--- @field PersistingEntities The entities that should auto-save to the map | **table**
