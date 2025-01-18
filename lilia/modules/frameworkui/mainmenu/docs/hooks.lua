229d856f5ebb775d41ff2ac706d72c2837eddab3
Samael <33399712+bleonheart@users.noreply.github.com>
2025-01-18 04:33:34 +0000

    Removed Old Documentation

--- lilia/modules/frameworkui/mainmenu/docs/hooks.lua
+++
@@ -1,7 +0,0 @@
-﻿--- Hook Documentation for MainMenu Module.
--- @hooks MainMenu
---- Called when the player wants to view their inventory.
--- @realm client
--- @treturn bool Whether the player is allowed to view their inventory.
-function Temp()
-end
