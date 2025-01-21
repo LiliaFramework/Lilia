381246b28b25f21bf5883abe59452335156b06f5
Samael <33399712+bleonheart@users.noreply.github.com>
2025-01-21 19:54:07 +0000

    Removed Obsolete Code

--- lilia/modules/utilities/vendor/module.lua
+++ lilia/modules/utilities/vendor/module.lua
@@ -10,21 +10,3 @@ MODULE.CAMIPrivileges = {
     Description = "Allows access to edit vendors.",
   },
 }
-
-if CLIENT then
-  function MODULE:PopulateAdminStick(AdminMenu, target)
-    if IsValid(target) and target:GetClass() == "lia_vendor" then
-      AdminMenu:AddOption("Restock Vendor", function()
-        LocalPlayer():ConCommand("say /restockvendor")
-        AdminStickIsOpen = false
-      end):SetIcon("icon16/arrow_refresh.png")
-
-      AdminMenu:AddOption("Restock Vendor Money", function()
-        Derma_StringRequest("Restock Vendor Money", "Enter amount:", "", function(amount)
-          if amount and tonumber(amount) then LocalPlayer():ConCommand("say /restockvendormoney " .. tonumber(amount)) end
-          AdminStickIsOpen = false
-        end)
-      end):SetIcon("icon16/money_add.png")
-    end
-  end
-end
