348a8ad9c30c9d1714ae8b52fce8e5e9dcc947c8
Samael <33399712+bleonheart@users.noreply.github.com>
2025-02-11 02:01:16 +0000

    Muted Toolgun

--- lilia/modules/core/performance/libraries/server.lua
+++ lilia/modules/core/performance/libraries/server.lua
@@ -1,3 +1,12 @@
 ﻿function MODULE:PropBreak(_, entity)
     if IsValid(entity) and IsValid(entity:GetPhysicsObject()) then constraint.RemoveAll(entity) end
 end
+
+function MODULE:EntityEmitSound(tab)
+    local sounds = {
+        ["weapons/airboat/airboat_gun_lastshot1.wav"] = true,
+        ["weapons/airboat/airboat_gun_lastshot2.wav"] = true
+    }
+
+    if sounds[tab.SoundName] then return false end
+end
