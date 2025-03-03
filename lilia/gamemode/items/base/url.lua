4cb8c97798343491705a0fe189ee0a8c65eabf9a
Samael <33399712+bleonheart@users.noreply.github.com>
2025-03-03 12:30:03 +0000

    URL Items

---
+++ lilia/gamemode/items/base/url.lua
@@ -0,0 +1,14 @@
+ITEM.name = "Generic Item"
+ITEM.desc = "Generic Description"
+ITEM.model = "models/props_interiors/pot01a.mdl"
+ITEM.url = ""
+ITEM.functions.use = {
+    name = "Open",
+    icon = "icon16/book_link.png",
+    onRun = function(item)
+        net.Start("sendURL")
+        net.WriteString(item.url)
+        net.Send(item.player)
+        return false
+    end,
+}
