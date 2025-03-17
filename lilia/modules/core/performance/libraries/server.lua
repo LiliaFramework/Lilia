13f9d3ca55b832afd71f055c2f037d96eb49a787
eon (bloodycop) <eon.bloodycop@gmail.com>
2025-03-17 21:44:26 +0200

    improve toolgun sound mute

--- lilia/modules/core/performance/libraries/server.lua
+++ lilia/modules/core/performance/libraries/server.lua
@@ -2,14 +2,13 @@
     if IsValid(entity) and IsValid(entity:GetPhysicsObject()) then constraint.RemoveAll(entity) end
 end
 
+MODULE.ToolGunSounds = {
+    ["weapons/airboat/airboat_gun_lastshot1.wav"] = true,
+    ["weapons/airboat/airboat_gun_lastshot2.wav"] = true
+}
 function MODULE:EntityEmitSound(tab)
-    local sounds = {
-        ["weapons/airboat/airboat_gun_lastshot1.wav"] = true,
-        ["weapons/airboat/airboat_gun_lastshot2.wav"] = true
-    }
-
     if IsValid(tab.Entity) and tab.Entity:IsPlayer() then
         local wep = tab.Entity:GetActiveWeapon()
-        if IsValid(wep) and wep:GetClass() == "gmod_tool" and sounds[tab.SoundName] then return false end
+        if IsValid(wep) and wep:GetClass() == "gmod_tool" and self.ToolGunSounds[tab.SoundName] then return false end
     end
 end
