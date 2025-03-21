ef936478abd14fa8d06e0e3e68267e493e56e247
Samael <33399712+bleonheart@users.noreply.github.com>
2025-03-21 15:30:19 +0000

    Updated Methods

--- lilia/modules/core/administration/submodules/permissions/libraries/server.lua
+++ lilia/modules/core/administration/submodules/permissions/libraries/server.lua
@@ -1,43 +1,201 @@
 ﻿local GM = GM or GAMEMODE
 local resetCalled = 0
+function GM:PlayerSpawnProp(client, model)
+    if self.BlackListedProps and self.BlackListedProps[model] and not client:hasPrivilege("Spawn Permissions - Can Spawn Blacklisted Props") then
+        client:notify("Blacklisted Prop!")
+        return false
+    end
+
+    local weapon = client:GetActiveWeapon()
+    if IsValid(weapon) and weapon:GetClass() == "gmod_tool" then
+        local toolobj = weapon:GetToolObject()
+        if toolobj and (client.AdvDupe2 and client.AdvDupe2.Entities or client.CurrentDupe and client.CurrentDupe.Entities or toolobj.Entities) then return true end
+    end
+
+    local canSpawn = client:IsSuperAdmin() or client:isStaffOnDuty() or client:hasPrivilege("Spawn Permissions - Can Spawn Props") or client:getChar():hasFlags("e")
+    if not canSpawn then client:notify("You do not have permission to spawn props.") end
+    return canSpawn
+end
+
+function GM:PlayerSpawnObject(client)
+    local activeWeapon = client:GetActiveWeapon()
+    if IsValid(activeWeapon) and activeWeapon:GetClass() == "gmod_tool" then
+        local toolobj = activeWeapon:GetToolObject()
+        if toolobj and not isbool(toolobj) and (client.AdvDupe2 and client.AdvDupe2.Entities or client.CurrentDupe and client.CurrentDupe.Entities or toolobj.Entities) then return true end
+    end
+
+    if client:IsSuperAdmin() then return true end
+    if not client.NextSpawn then client.NextSpawn = CurTime() end
+    if client:hasPrivilege("Spawn Permissions - No Spawn Delay") then return true end
+    if client.NextSpawn >= CurTime() then
+        client:notifyWarning("You can't spawn props that fast!")
+        return false
+    end
+
+    client.NextSpawn = CurTime() + 0.75
+    return true
+end
+
+function GM:CanProperty(client, property, entity)
+    local restrictedProperties = {
+        persist = true,
+        drive = true,
+        bonemanipulate = true
+    }
+
+    if restrictedProperties[property] then
+        client:notify("This is disabled to avoid issues with Lilia's Core Features")
+        return false
+    end
+
+    if entity:IsWorld() and IsValid(entity) then
+        if client:hasPrivilege("Staff Permissions - Can Property World Entities") then return true end
+        client:notify("You do not have permission to modify world entities.")
+        return false
+    end
+
+    local entityClass = entity:GetClass()
+    if (self.RemoverBlockedEntities and self.RemoverBlockedEntities[entityClass]) or (self.RestrictedEnts and self.RestrictedEnts[entityClass]) then
+        if client:hasPrivilege("Staff Permissions - Use Entity Properties on Blocked Entities") then return true end
+        client:notify("You do not have permission to modify properties on this entity.")
+        return false
+    end
+
+    if entity:GetCreator() == client and (property == "remover" or property == "collision") then return true end
+    if client:IsSuperAdmin() or (client:hasPrivilege("Staff Permissions - Access Property " .. property:gsub("^%l", string.upper)) and client:isStaffOnDuty()) then return true end
+    client:notify("You do not have permission to modify this property.")
+    return false
+end
+
+function GM:PhysgunPickup(client, entity)
+    local entityClass = entity:GetClass()
+    if (client:hasPrivilege("Staff Permissions - Physgun Pickup") or client:isStaffOnDuty()) and self.RestrictedEnts and self.RestrictedEnts[entityClass] then
+        if not client:hasPrivilege("Staff Permissions - Physgun Pickup on Restricted Entities") then
+            client:notify("You do not have permission to pick up restricted entities with the physgun.")
+            return false
+        end
+        return true
+    end
+
+    if client:IsSuperAdmin() then return true end
+    if entity:GetCreator() == client and (entity:isProp() or entity:isItem()) then return true end
+    if client:hasPrivilege("Staff Permissions - Physgun Pickup") then
+        if entity:IsVehicle() then
+            if not client:hasPrivilege("Staff Permissions - Physgun Pickup on Vehicles") then
+                client:notify("You do not have permission to pick up vehicles with the physgun.")
+                return false
+            end
+            return true
+        elseif entity:IsPlayer() then
+            if entity:hasPrivilege("Staff Permissions - Can't be Grabbed with PhysGun") or not client:hasPrivilege("Staff Permissions - Can Grab Players") then
+                client:notify("You do not have permission to pick up this player with the physgun.")
+                return false
+            end
+            return true
+        elseif entity:IsWorld() or entity:CreatedByMap() then
+            if not client:hasPrivilege("Staff Permissions - Can Grab World Props") then
+                client:notify("You do not have permission to pick up world props with the physgun.")
+                return false
+            end
+            return true
+        end
+        return true
+    end
+
+    client:notify("You do not have permission to pick up this entity with the physgun.")
+    return false
+end
+
+function GM:PlayerSpawnVehicle(client, _, name)
+    if self.RestrictedVehicles and self.RestrictedVehicles[name] and not client:hasPrivilege("Spawn Permissions - Can Spawn Restricted Cars") then
+        client:notifyWarning("You can't spawn this vehicle since it's restricted!")
+        return false
+    end
+
+    if not client:hasPrivilege("Spawn Permissions - No Car Spawn Delay") then client.NextVehicleSpawn = SysTime() + lia.config.get("PlayerSpawnVehicleDelay", 30) end
+    local canSpawn = client:isStaffOnDuty() or client:hasPrivilege("Spawn Permissions - Can Spawn Cars") or client:getChar():hasFlags("C")
+    if not canSpawn then client:notify("You do not have permission to spawn vehicles.") end
+    return canSpawn
+end
+
+function GM:PlayerNoClip(client, state)
+    if client:isStaffOnDuty() or (client:hasPrivilege("Staff Permissions - No Clip Outside Staff Character") and not client:isStaffOnDuty()) then
+        if state then
+            client:SetNoDraw(true)
+            client:SetNotSolid(true)
+            client:DrawWorldModel(false)
+            client:DrawShadow(false)
+            client:SetNoTarget(true)
+            client.liaObsData = {client:GetPos(), client:EyeAngles()}
+            hook.Run("OnPlayerObserve", client, state)
+        else
+            if client.liaObsData then
+                if client:GetInfoNum("lia_obstpback", 0) > 0 then
+                    local position, angles = client.liaObsData[1], client.liaObsData[2]
+                    timer.Simple(0, function()
+                        client:SetPos(position)
+                        client:SetEyeAngles(angles)
+                        client:SetVelocity(Vector(0, 0, 0))
+                    end)
+                end
+
+                client.liaObsData = nil
+            end
+
+            client:SetNoDraw(false)
+            client:SetNotSolid(false)
+            client:DrawWorldModel(true)
+            client:DrawShadow(true)
+            client:SetNoTarget(false)
+            hook.Run("OnPlayerObserve", client, state)
+        end
+        return true
+    end
+
+    client:notify("You do not have permission to noclip.")
+    return false
+end
+
 function GM:PlayerSpawnEffect(client)
-    return client:IsSuperAdmin() or client:isStaffOnDuty() or client:hasPrivilege("Spawn Permissions - Can Spawn Effects") or client:getChar():hasFlags("L")
+    local canSpawn = client:IsSuperAdmin() or client:isStaffOnDuty() or client:hasPrivilege("Spawn Permissions - Can Spawn Effects") or client:getChar():hasFlags("L")
+    if not canSpawn then client:notify("You do not have permission to spawn effects.") end
+    return canSpawn
 end
 
 function GM:PlayerSpawnNPC(client)
-    return client:IsSuperAdmin() or client:isStaffOnDuty() or client:hasPrivilege("Spawn Permissions - Can Spawn NPCs") or client:getChar():hasFlags("n")
-end
-
-function GM:PlayerSpawnProp(client)
-    return client:IsSuperAdmin() or client:isStaffOnDuty() or client:hasPrivilege("Spawn Permissions - Can Spawn Props") or client:getChar():hasFlags("e")
+    local canSpawn = client:IsSuperAdmin() or client:isStaffOnDuty() or client:hasPrivilege("Spawn Permissions - Can Spawn NPCs") or client:getChar():hasFlags("n")
+    if not canSpawn then client:notify("You do not have permission to spawn NPCs.") end
+    return canSpawn
 end
 
 function GM:PlayerSpawnRagdoll(client)
-    return client:IsSuperAdmin() or client:isStaffOnDuty() or client:hasPrivilege("Spawn Permissions - Can Spawn Ragdolls") or client:getChar():hasFlags("r")
+    local canSpawn = client:IsSuperAdmin() or client:isStaffOnDuty() or client:hasPrivilege("Spawn Permissions - Can Spawn Ragdolls") or client:getChar():hasFlags("r")
+    if not canSpawn then client:notify("You do not have permission to spawn ragdolls.") end
+    return canSpawn
 end
 
 function GM:PlayerSpawnSENT(client)
-    return client:IsSuperAdmin() or client:isStaffOnDuty() or client:hasPrivilege("Spawn Permissions - Can Spawn SENTs") or client:getChar():hasFlags("E")
+    local canSpawn = client:IsSuperAdmin() or client:isStaffOnDuty() or client:hasPrivilege("Spawn Permissions - Can Spawn SENTs") or client:getChar():hasFlags("E")
+    if not canSpawn then client:notify("You do not have permission to spawn SENTs.") end
+    return canSpawn
 end
 
 function GM:PlayerSpawnSWEP(client)
-    return client:IsSuperAdmin() or client:isStaffOnDuty() or client:hasPrivilege("Spawn Permissions - Can Spawn SWEPs") or client:getChar():hasFlags("z")
-end
-
-function GM:PlayerSpawnVehicle(client)
-    return client:isStaffOnDuty() or client:hasPrivilege("Spawn Permissions - Can Spawn Cars") or client:getChar():hasFlags("C")
+    local canSpawn = client:IsSuperAdmin() or client:isStaffOnDuty() or client:hasPrivilege("Spawn Permissions - Can Spawn SWEPs") or client:getChar():hasFlags("z")
+    if not canSpawn then client:notify("You do not have permission to spawn SWEPs.") end
+    return canSpawn
 end
 
 function GM:PlayerGiveSWEP(client)
-    return client:isStaffOnDuty() or client:hasPrivilege("Spawn Permissions - Can Spawn SWEPs") or client:getChar():hasFlags("W")
-end
-
-function GM:PlayerNoClip(client)
-    return not client:isStaffOnDuty() and client:hasPrivilege("Staff Permissions - No Clip Outside Staff Character") or client:isStaffOnDuty()
+    local canGive = client:IsSuperAdmin() or client:isStaffOnDuty() or client:hasPrivilege("Spawn Permissions - Can Spawn SWEPs") or client:getChar():hasFlags("W")
+    if not canGive then client:notify("You do not have permission to give SWEPs.") end
+    return canGive
 end
 
 function GM:OnPhysgunReload(_, client)
-    return client:hasPrivilege("Staff Permissions - Can Physgun Reload")
+    local canReload = client:hasPrivilege("Staff Permissions - Can Physgun Reload")
+    if not canReload then client:notify("You do not have permission to reload the physgun.") end
+    return canReload
 end
 
 function GM:CanTool(client, _, tool)
@@ -48,7 +206,7 @@ function GM:CanTool(client, _, tool)
         dynamite = true,
         physprop = true,
         faceposer = true,
-        stacker = true,
+        stacker = true
     }
 
     local function CheckDuplicationScale(client, entities)
@@ -65,9 +223,17 @@ function GM:CanTool(client, _, tool)
         return true
     end
 
+    if DisallowedTools[tool] and not client:IsSuperAdmin() then
+        client:notify("You are not allowed to use the " .. tool .. " tool.")
+        return false
+    end
+
     local privilege = "Staff Permissions - Access Tool " .. tool:gsub("^%l", string.upper)
-    if DisallowedTools[tool] and not client:IsSuperAdmin() then return false end
-    if not (client:IsSuperAdmin() or ((client:isStaffOnDuty() or client:getChar():hasFlags("t")) and client:hasPrivilege(privilege))) then return false end
+    if not (client:IsSuperAdmin() or ((client:isStaffOnDuty() or client:getChar():hasFlags("t")) and client:hasPrivilege(privilege))) then
+        client:notify("You do not have permission to access tool " .. tool .. ".")
+        return false
+    end
+
     local weapon = client:GetActiveWeapon()
     local toolobj = IsValid(weapon) and weapon:GetToolObject() or nil
     local entity = client:getTracedEntity()
@@ -75,57 +241,41 @@ function GM:CanTool(client, _, tool)
         local entClass = entity:GetClass()
         if tool == "remover" then
             if self.RemoverBlockedEntities and self.RemoverBlockedEntities[entClass] then
-                return client:hasPrivilege("Staff Permissions - Can Remove Blocked Entities")
+                if not client:hasPrivilege("Staff Permissions - Can Remove Blocked Entities") then
+                    client:notify("You do not have permission to remove blocked entities.")
+                    return false
+                end
+                return true
             elseif entity:IsWorld() then
-                return client:hasPrivilege("Staff Permissions - Can Remove World Entities")
+                if not client:hasPrivilege("Staff Permissions - Can Remove World Entities") then
+                    client:notify("You do not have permission to remove world entities.")
+                    return false
+                end
+                return true
             end
             return true
         end
 
-        if (tool == "permaall" or tool == "permaprops" or tool == "blacklistandremove") and (string.StartWith(entClass, "lia_") or (self.CanNotPermaProp and self.CanNotPermaProp[entClass]) or entity:isLiliaPersistent() or entity:CreatedByMap()) then return false end
-        if (tool == "adv_duplicator" or tool == "advdupe2" or tool == "duplicator" or tool == "blacklistandremove") and ((self.DuplicatorBlackList and self.DuplicatorBlackList[entClass]) or entity.NoDuplicate) then return false end
-        if tool == "weld" and entClass == "sent_ball" then return false end
+        if (tool == "permaall" or tool == "permaprops" or tool == "blacklistandremove") and (string.StartWith(entClass, "lia_") or (self.CanNotPermaProp and self.CanNotPermaProp[entClass]) or entity:isLiliaPersistent() or entity:CreatedByMap()) then
+            client:notify("You cannot use " .. tool .. " on this entity.")
+            return false
+        end
+
+        if (tool == "adv_duplicator" or tool == "advdupe2" or tool == "duplicator" or tool == "blacklistandremove") and ((self.DuplicatorBlackList and self.DuplicatorBlackList[entClass]) or entity.NoDuplicate) then
+            client:notify("This entity cannot be duplicated using " .. tool .. ".")
+            return false
+        end
+
+        if tool == "weld" and entClass == "sent_ball" then
+            client:notify("You cannot weld this entity with the weld tool.")
+            return false
+        end
     end
 
     if tool == "duplicator" and client.CurrentDupe and not CheckDuplicationScale(client, client.CurrentDupe.Entities) then return false end
     if tool == "advdupe2" and client.AdvDupe2 and not CheckDuplicationScale(client, client.AdvDupe2.Entities) then return false end
     if tool == "adv_duplicator" and toolobj and toolobj.Entities and not CheckDuplicationScale(client, toolobj.Entities) then return false end
     return true
-end
-
-function GM:CanProperty(client, property, entity)
-    if entity:GetCreator() == client and (property == "remover" or property == "collision") then return true end
-    if client:IsSuperAdmin() or client:hasPrivilege("Staff Permissions - Access Property " .. property:gsub("^%l", string.upper)) and client:isStaffOnDuty() then return true end
-    return false
-end
-
-function GM:PlayerSpawnObject(client)
-    if client:IsSuperAdmin() then return true end
-    if not client.NextSpawn then client.NextSpawn = CurTime() end
-    if client:hasPrivilege("Spawn Permissions - No Spawn Delay") then return true end
-    if client.NextSpawn >= CurTime() then
-        client:notifyWarning("You can't spawn props that fast!")
-        return false
-    end
-
-    client.NextSpawn = CurTime() + 0.75
-    return true
-end
-
-function GM:PhysgunPickup(client, entity)
-    if client:IsSuperAdmin() then return true end
-    if entity:GetCreator() == client and (entity:isProp() or entity:isItem()) then return true end
-    if client:hasPrivilege("Staff Permissions - Physgun Pickup") then
-        if entity:IsVehicle() then
-            return client:hasPrivilege("Staff Permissions - Physgun Pickup on Vehicles")
-        elseif entity:IsPlayer() then
-            return not entity:hasPrivilege("Staff Permissions - Can't be Grabbed with PhysGun") and client:hasPrivilege("Staff Permissions - Can Grab Players")
-        elseif entity:IsWorld() or entity:CreatedByMap() then
-            return client:hasPrivilege("Staff Permissions - Can Grab World Props")
-        end
-        return true
-    end
-    return false
 end
 
 function GM:PlayerSpawnedNPC(client, entity)
@@ -178,7 +328,11 @@ end)
 
 local function handleDatabaseWipe(commandName)
     concommand.Add(commandName, function(client)
-        if IsValid(client) then return end
+        if IsValid(client) then
+            client:notify("This command can only be run from the server console.")
+            return
+        end
+
         if resetCalled < RealTime() then
             resetCalled = RealTime() + 3
             MsgC(Color(255, 0, 0), "[Lilia] TO CONFIRM DATABASE RESET, RUN '" .. commandName .. "' AGAIN in 3 SECONDS.\n")
--- lilia/modules/core/protection/libraries/server.lua
+++ lilia/modules/core/protection/libraries/server.lua
@@ -59,8 +59,27 @@ end
 
 local function ApplyPunishment(client, infraction, kick, ban, time)
     local bantime = time or 0
-    if kick then client:Kick("Kicked for " .. infraction .. ".") end
-    if ban then client:Ban(bantime, "Banned for " .. infraction .. ".") end
+    if kick then
+        if sam then
+            sam.player.kick(client, "Kicked for " .. infraction)
+        elseif ULib and ULib.kick then
+            ULib.kick(client, "Kicked for " .. infraction)
+        else
+            client:Kick("Kicked for " .. infraction)
+        end
+    end
+
+    if ban then
+        if sam then
+            sam.player.ban(client, bantime, "Banned for " .. infraction)
+        elseif ULib and ULib.ban then
+            ULib.ban(client, bantime, "Banned for " .. infraction)
+        elseif client.Ban then
+            client:Ban(bantime, "Banned for " .. infraction .. ".")
+        else
+            client:Kick("Banned for " .. infraction .. ".")
+        end
+    end
 end
 
 function MODULE:PlayerAuthed(client, steamid)
@@ -76,19 +95,19 @@ function MODULE:PlayerAuthed(client, steamid)
 
     local steamID64 = util.SteamIDTo64(steamid)
     local ownerSteamID64 = client:OwnerSteamID64()
-    local steamName = client:steamName()
+    local steamName = client:SteamName()
     local steamID = client:SteamID()
     if KnownCheaters[steamID64] or KnownCheaters[ownerSteamID64] then
-        client:Ban("You are banned from this server for using third-party cheats.\nIf you believe this is a mistake, please appeal by contacting the owner with this message.")
+        ApplyPunishment(client, "using third-party cheats", false, true, 0)
         NotifyAdmin(string.format("%s (%s) was banned for cheating or using an alt of a cheater.", steamName, steamID))
         return
     end
 
     if lia.config.get("AltsDisabled", false) and ownerSteamID64 ~= steamID64 then
-        client:Kick("Sorry! We do not allow family-shared accounts on this server!")
+        ApplyPunishment(client, "family sharing (alts are disabled)", true, false)
         NotifyAdmin(string.format("%s (%s) was kicked for family sharing.", steamName, steamID))
     elseif WhitelistCore and WhitelistCore.BlacklistedSteamID64[ownerSteamID64] then
-        client:Ban("You are using an account whose family share is blacklisted from this server!")
+        ApplyPunishment(client, "using a family-shared account that is blacklisted", false, true, 0)
         NotifyAdmin(string.format("%s (%s) was banned for using a family-shared account that is blacklisted.", steamName, steamID))
     end
 end
@@ -168,7 +187,6 @@ function MODULE:ShouldCollide(ent1, ent2)
     local isBlockedEntity2 = blockedEntities[class2]
     local isPlayerColliding = ent1:IsPlayer() or ent2:IsPlayer()
     if isPlayerColliding and isBlockedEntity1 and isBlockedEntity2 then return false end
-    if self.BlockedCollideEntities[class1] and self.BlockedCollideEntities[class2] then return false end
     return true
 end
 
@@ -182,52 +200,6 @@ end
 
 function MODULE:PhysgunDrop(_, entity)
     if entity:isProp() and entity:isItem() then timer.Simple(5, function() if IsValid(entity) and entity:GetCollisionGroup() == COLLISION_GROUP_PASSABLE_DOOR then entity:SetCollisionGroup(COLLISION_GROUP_NONE) end end) end
-end
-
-function MODULE:PlayerSpawnProp(client, model)
-    if self.BlackListedProps[model] and not client:hasPrivilege("Spawn Permissions - Can Spawn Blacklisted Props") then
-        client:notify("Blacklisted Prop!")
-        return false
-    end
-
-    local weapon = client:GetActiveWeapon()
-    if IsValid(weapon) and weapon:GetClass() == "gmod_tool" then
-        local toolobj = weapon:GetToolObject()
-        if toolobj ~= nil and (client.AdvDupe2 and client.AdvDupe2.Entities or client.CurrentDupe and client.CurrentDupe.Entities or toolobj.Entities) then return true end
-    end
-end
-
-function MODULE:PlayerSpawnObject(client)
-    if IsValid(client:GetActiveWeapon()) and client:GetActiveWeapon():GetClass() == "gmod_tool" then
-        local toolobj = client:GetActiveWeapon():GetToolObject()
-        if not isbool(toolobj) and client.AdvDupe2 and client.AdvDupe2.Entities or client.CurrentDupe and client.CurrentDupe.Entities or toolobj.Entities then return true end
-    end
-end
-
-function MODULE:PlayerSpawnedNPC(_, entity)
-    if not lia.config.get("NPCsDropWeapons", false) then entity:SetKeyValue("spawnflags", "8192") end
-end
-
-function MODULE:CanProperty(client, property, entity)
-    local restrictedProperties = {
-        persist = true,
-        drive = true,
-        bonemanipulate = true,
-    }
-
-    if restrictedProperties[property] then
-        client:notify("This is disabled to avoid issues with Lilia's Core Features")
-        return false
-    end
-
-    if entity:IsWorld() and IsValid(entity) then return client:hasPrivilege("Staff Permissions - Can Property World Entities") end
-    local entityClass = entity:GetClass()
-    if self.RemoverBlockedEntities[entityClass] or self.RestrictedEnts[entityClass] then return client:hasPrivilege("Staff Permissions - Use Entity Properties on Blocked Entities") end
-end
-
-function MODULE:PhysgunPickup(client, entity)
-    local entityClass = entity:GetClass()
-    if (client:hasPrivilege("Staff Permissions - Physgun Pickup") or client:isStaffOnDuty()) and self.RestrictedEnts[entityClass] then return client:hasPrivilege("Staff Permissions - Physgun Pickup on Restricted Entities") end
 end
 
 function MODULE:OnPhysgunFreeze(_, physObj, entity, client)
@@ -256,69 +228,8 @@ function MODULE:OnPhysgunFreeze(_, physObj, entity, client)
     return true
 end
 
-function MODULE:PlayerSpawnVehicle(client, _, name)
-    if self.RestrictedVehicles[name] and not client:hasPrivilege("Spawn Permissions - Can Spawn Restricted Cars") then
-        client:notifyWarning("You can't spawn this vehicle since it's restricted!")
-        return false
-    end
-
-    if not client:hasPrivilege("Spawn Permissions - No Car Spawn Delay") then client.NextVehicleSpawn = SysTime() + lia.config.get("PlayerSpawnVehicleDelay", 30) end
-end
-
-function MODULE:PlayerNoClip(client, state)
-    if not client:isStaffOnDuty() and client:hasPrivilege("Staff Permissions - No Clip Outside Staff Character") or client:isStaffOnDuty() then
-        if state then
-            client:SetNoDraw(true)
-            client:SetNotSolid(true)
-            client:DrawWorldModel(false)
-            client:DrawShadow(false)
-            client:SetNoTarget(true)
-            client.liaObsData = {client:GetPos(), client:EyeAngles()}
-            hook.Run("OnPlayerObserve", client, state)
-        else
-            if client.liaObsData then
-                if client:GetInfoNum("lia_obstpback", 0) > 0 then
-                    local position, angles = client.liaObsData[1], client.liaObsData[2]
-                    timer.Simple(0, function()
-                        client:SetPos(position)
-                        client:SetEyeAngles(angles)
-                        client:SetVelocity(Vector(0, 0, 0))
-                    end)
-                end
-
-                client.liaObsData = nil
-            end
-
-            client:SetNoDraw(false)
-            client:SetNotSolid(false)
-            client:DrawWorldModel(true)
-            client:DrawShadow(true)
-            client:SetNoTarget(false)
-            hook.Run("OnPlayerObserve", client, state)
-        end
-    end
-end
-
 function MODULE:PlayerSpawn(client)
     if not client:getChar() then return end
     net.Start("VerifyCheats")
     net.Send(client)
-end
-
-function net.Incoming(length, client)
-    local i = net.ReadHeader()
-    local strName = util.NetworkIDToString(i)
-    if not strName then
-        lia.log.add(client, "invalidNet")
-        return
-    end
-
-    local func = net.Receivers[strName:lower()]
-    if not func then
-        lia.log.add(client, "invalidNet")
-        return
-    end
-
-    length = length - 16
-    func(length, client)
 end
