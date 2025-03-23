5ebc408259609a4d924bcbc7779445798818c3df
Samael <33399712+bleonheart@users.noreply.github.com>
2025-03-23 20:54:06 +0000

    Registered Missing Privilege

--- modules/core/administration/submodules/adminstick/module.lua
+++ modules/core/administration/submodules/adminstick/module.lua
@@ -3,3 +3,10 @@ MODULE.author = "76561198312513285"
 MODULE.discord = "@liliaplayer"
 MODULE.version = "Stock"
 MODULE.desc = "Adds a module that facilitates Adminstration"
+MODULE.CAMIPrivileges = {
+    {
+        Name = "Staff Permissions - Use Admin Stick",
+        MinAccess = "superadmin",
+        Description = "Allows access to using Admin Stick."
+    },
+}
