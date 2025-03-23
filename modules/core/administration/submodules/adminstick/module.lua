c7a5f1124a4530481122c1132bbfcc044509c356
Samael <33399712+bleonheart@users.noreply.github.com>
2025-03-23 20:54:56 +0000

    New Folder Format

---
+++ modules/core/administration/submodules/adminstick/module.lua
@@ -0,0 +1,12 @@
+﻿MODULE.name = "Admin Stick"
+MODULE.author = "76561198312513285"
+MODULE.discord = "@liliaplayer"
+MODULE.version = "Stock"
+MODULE.desc = "Adds a module that facilitates Adminstration"
+MODULE.CAMIPrivileges = {
+    {
+        Name = "Staff Permissions - Use Admin Stick",
+        MinAccess = "superadmin",
+        Description = "Allows access to using Admin Stick."
+    },
+}
