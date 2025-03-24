2cbf15765e78e9f40d82c8d0936e13a21e542f77
Samael <33399712+bleonheart@users.noreply.github.com>
2025-03-24 01:46:50 +0000

    Removed Deprecated Compatibility

---
+++ modules/compatibility/simfphys/module.lua
@@ -0,0 +1,13 @@
+﻿MODULE.name = "Simfphys"
+MODULE.author = "76561198312513285"
+MODULE.discord = "@liliaplayer"
+MODULE.version = "Stock"
+MODULE.desc = "Adds Simfphys Compatibility"
+MODULE.enabled = simfphys ~= nil
+MODULE.CAMIPrivileges = {
+	{
+		Name = "Staff Permissions - Can Edit Simfphys Cars",
+		MinAccess = "superadmin",
+		Description = "Allows access to Editting Simfphys Cars.",
+	}
+}
--- modules/compatibility/streamradios/module.lua
+++
@@ -1,6 +0,0 @@
-﻿MODULE.name = "3D Stream Radios"
-MODULE.author = "76561198312513285"
-MODULE.discord = "@liliaplayer"
-MODULE.version = "Stock"
-MODULE.desc = "Adds - 3D Stream Radios Compatibility"
-MODULE.enabled = StreamRadioLib ~= nil
