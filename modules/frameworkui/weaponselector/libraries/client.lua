9728702147c46ef0613bf79734d4a56b293fc3fc
Samael <33399712+bleonheart@users.noreply.github.com>
2025-03-26 03:51:14 +0000

    Redundancies

--- modules/frameworkui/weaponselector/libraries/client.lua
+++ modules/frameworkui/weaponselector/libraries/client.lua
@@ -89,7 +89,7 @@ function MODULE:onIndexChanged()
     infoAlpha = 0
     if IsValid(weapon) then
         local textParts = {}
-        local activeColor = lia.config.get("Color") -- Cache color here, too
+        local activeColor = lia.config.get("Color")
         for _, key in ipairs({"Author", "Contact", "Purpose", "Instructions"}) do
             if weapon[key] and weapon[key]:find("%S") then table.insert(textParts, string.format("<font=liaItemBoldFont><color=%d,%d,%d>%s</font></color>\n%s\n", activeColor.r, activeColor.g, activeColor.b, L(key), weapon[key])) end
         end
