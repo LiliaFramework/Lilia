85a391d697c851d072575b761dd1e0a718524a4a
Samael <33399712+bleonheart@users.noreply.github.com>
2025-03-26 00:23:55 +0000

    Revert "Merged Metas for Ease of Use"
    This reverts commit 4e8e491b32d59bba4a682922f27a5b9a04339fa1.

---
+++ modules/characters/attributes/meta/sh_character.lua
@@ -0,0 +1,77 @@
+﻿local characterMeta = lia.meta.character
+function characterMeta:getMaxStamina()
+    local maxStamina = hook.Run("CharMaxStamina", self) or lia.config.get("DefaultStamina", 100)
+    return maxStamina
+end
+
+function characterMeta:getStamina()
+    local stamina = self:getPlayer():getLocalVar("stamina", 100) or lia.config.get("DefaultStamina", 100)
+    return stamina
+end
+
+function characterMeta:getAttrib(key, default)
+    local att = self:getAttribs()[key] or default or 0
+    local boosts = self:getBoosts()[key]
+    if boosts then
+        for _, v in pairs(boosts) do
+            att = att + v
+        end
+    end
+    return att
+end
+
+function characterMeta:getBoost(attribID)
+    local boosts = self:getBoosts()
+    return boosts[attribID]
+end
+
+function characterMeta:getBoosts()
+    return self:getVar("boosts", {})
+end
+
+if SERVER then
+    function characterMeta:updateAttrib(key, value)
+        local client = self:getPlayer()
+        local attribute = lia.attribs.list[key]
+        if not attribute then return end
+        local attrib = self:getAttribs()
+        local currentLevel = attrib[key] or 0
+        local maxLevel = hook.Run("GetAttributeMax", client, key) or math.huge
+        attrib[key] = math.min(currentLevel + value, maxLevel)
+        if IsValid(client) then
+            netstream.Start(client, "attrib", self:getID(), key, attrib[key])
+            if attribute.setup then attribute.setup(attrib[key]) end
+        end
+    end
+
+    function characterMeta:setAttrib(key, value)
+        local client = self:getPlayer()
+        local attribute = lia.attribs.list[key]
+        if attribute then
+            local attrib = self:getAttribs()
+            attrib[key] = value
+            if IsValid(client) then
+                netstream.Start(client, "attrib", self:getID(), key, attrib[key])
+                if attribute.setup then attribute.setup(attrib[key]) end
+            end
+        end
+
+        hook.Run("OnCharAttribUpdated", client, self, key, value)
+    end
+
+    function characterMeta:addBoost(boostID, attribID, boostAmount)
+        local boosts = self:getVar("boosts", {})
+        boosts[attribID] = boosts[attribID] or {}
+        boosts[attribID][boostID] = boostAmount
+        hook.Run("OnCharAttribBoosted", self:getPlayer(), self, attribID, boostID, boostAmount)
+        return self:setVar("boosts", boosts, nil, self:getPlayer())
+    end
+
+    function characterMeta:removeBoost(boostID, attribID)
+        local boosts = self:getVar("boosts", {})
+        boosts[attribID] = boosts[attribID] or {}
+        boosts[attribID][boostID] = nil
+        hook.Run("OnCharAttribBoosted", self:getPlayer(), self, attribID, boostID, true)
+        return self:setVar("boosts", boosts, nil, self:getPlayer())
+    end
+end
---
+++ modules/characters/attributes/meta/sh_player.lua
@@ -0,0 +1,36 @@
+﻿local playerMeta = FindMetaTable("Player")
+function playerMeta:hasSkillLevel(skill, level)
+    local currentLevel = self:getChar():getAttrib(skill, 0)
+    return currentLevel >= level
+end
+
+function playerMeta:meetsRequiredSkills(requiredSkillLevels)
+    if not requiredSkillLevels then return true end
+    for skill, level in pairs(requiredSkillLevels) do
+        if not self:hasSkillLevel(skill, level) then return false end
+    end
+    return true
+end
+
+if SERVER then
+    function playerMeta:restoreStamina(amount)
+        local current = self:getLocalVar("stamina", 0)
+        local maxStamina = self:getChar():getMaxStamina()
+        local value = math.Clamp(current + amount, 0, maxStamina)
+        self:setLocalVar("stamina", value)
+        if value >= maxStamina * 0.5 and self:getNetVar("brth", false) then
+            self:setNetVar("brth", nil)
+            hook.Run("PlayerStaminaGained", self)
+        end
+    end
+
+    function playerMeta:consumeStamina(amount)
+        local current = self:getLocalVar("stamina", 0)
+        local value = math.Clamp(current - amount, 0, self:getChar():getMaxStamina())
+        self:setLocalVar("stamina", value)
+        if value == 0 and not self:getNetVar("brth", false) then
+            self:setNetVar("brth", true)
+            hook.Run("PlayerStaminaLost", self)
+        end
+    end
+end
