acf455e82ed0b86e50ccccff276fb763e0a47f8b
Samael <33399712+bleonheart@users.noreply.github.com>
2025-03-31 00:29:20 +0100

    Re-Added wepgenerator

---
+++ modules/utilities/wepgenerator/module.lua
@@ -0,0 +1,6 @@
+﻿MODULE.name = "Easy Weapons"
+MODULE.author = "76561198312513285"
+MODULE.discord = "@liliaplayer"
+MODULE.version = "1.0"
+MODULE.desc = "Adds an Easy Way of Making Weapons."
+MODULE.Public = true
