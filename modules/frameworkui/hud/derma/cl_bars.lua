b7ee70303153c8a87885feb95037c2da9dec208e
Samael <33399712+bleonheart@users.noreply.github.com>
2025-04-26 20:44:12 +0100

    Moved the Derma

--- modules/frameworkui/hud/derma/cl_bars.lua
+++
@@ -1,9 +0,0 @@
-﻿lia.bar.add(function()
-    local client = LocalPlayer()
-    return client:Health() / client:GetMaxHealth()
-end, Color(200, 50, 40), 1, "health")
-
-lia.bar.add(function()
-    local client = LocalPlayer()
-    return client:Armor() / client:GetMaxArmor()
-end, Color(30, 70, 180), 3, "armor")
--- modules/utilities/vendor/derma/cl_vendor_trader.lua
+++
@@ -1,7 +0,0 @@
-﻿local PANEL = {}
-function PANEL:Init()
-    self.items = self:Add("DScrollPanel")
-    self.items:Dock(FILL)
-end
-
-vgui.Register("VendorTrader", PANEL, "DPanel")
