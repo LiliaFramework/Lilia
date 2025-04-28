30c99496555aca8b204e6c6441b496e12eda6460
Samael <33399712+bleonheart@users.noreply.github.com>
2025-04-28 09:46:44 +0100

    Third Party Stuff

--- gamemode/core/libraries/thirdparty/sh_curtime.lua
+++ gamemode/core/libraries/thirdparty/sh_curtime.lua
@@ -1,38 +1,21 @@
-local SyncDelay = 30
-local SyncType = "net"
-if SyncType == "NW2" then
-    if SERVER then
-        timer.Create("CurTime-Sync", SyncDelay, -1, function() Entity(0):SetNW2Float("CurTime-Sync", CurTime()) end)
-    else
-        hook.Add("InitPostEntity", "CurTime-Sync", function()
-            local SyncTime = 0
-            Entity(0):SetNW2VarProxy("CurTime-Sync", function(_, _, _, ServerCurTime) SyncTime = OldCurTime() - ServerCurTime end)
-            OldCurTime = OldCurTime or CurTime
-            function CurTime()
-                return OldCurTime() - SyncTime
-            end
+if SERVER then
+    timer.Create("CurTime-Sync", 30, -1, function()
+        net.Start("CurTime-Sync", true)
+        net.WriteFloat(CurTime())
+        net.Broadcast()
+    end)
+else
+    hook.Add("InitPostEntity", "CurTime-Sync", function()
+        local SyncTime = 0
+        net.Receive("CurTime-Sync", function()
+            local ServerCurTime = net.ReadFloat()
+            if not ServerCurTime then return end
+            SyncTime = OldCurTime() - ServerCurTime
         end)
-    end
-elseif SyncType == "net" then
-    if SERVER then
-        timer.Create("CurTime-Sync", SyncDelay, -1, function()
-            net.Start("CurTime-Sync", true)
-            net.WriteFloat(CurTime())
-            net.Broadcast()
-        end)
-    else
-        hook.Add("InitPostEntity", "CurTime-Sync", function()
-            local SyncTime = 0
-            net.Receive("CurTime-Sync", function()
-                local ServerCurTime = net.ReadFloat()
-                if not ServerCurTime then return end
-                SyncTime = OldCurTime() - ServerCurTime
-            end)
 
-            OldCurTime = OldCurTime or CurTime
-            function CurTime()
-                return OldCurTime() - SyncTime
-            end
-        end)
-    end
+        OldCurTime = OldCurTime or CurTime
+        function CurTime()
+            return OldCurTime() - SyncTime
+        end
+    end)
 end
