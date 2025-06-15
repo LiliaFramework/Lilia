c379808fcaf7cd93d6446c2ad5782f075e070fe9
Samael <33399712+bleonheart@users.noreply.github.com>
2025-06-15 16:54:13 +0100

    Update vjbase.lua

--- gamemode/core/libraries/compatibility/vjbase.lua
+++ gamemode/core/libraries/compatibility/vjbase.lua
@@ -1,19 +1,4 @@
 ﻿local exploitable_nets = {"VJSay", "vj_fireplace_turnon1", "vj_npcmover_sv_create", "vj_npcmover_sv_startmove", "vj_npcmover_removesingle", "vj_npcmover_removeall", "vj_npcspawner_sv_create", "vj_npcrelationship_sr_leftclick", "vj_testentity_runtextsd", "vj_fireplace_turnon2"}
-local console_settings = {
-    vj_npc_processtime = "1",
-    vj_npc_corpsefade = "1",
-    vj_npc_corpsefadetime = "5",
-    vj_npc_nogib = "1",
-    vj_npc_nosnpcchat = "1",
-    vj_npc_slowplayer = "1",
-    vj_npc_noproppush = "1",
-    vj_npc_nothrowgrenade = "1",
-    vj_npc_fadegibstime = "5",
-    vj_npc_knowenemylocation = "1",
-    vj_npc_dropweapon = "0",
-    vj_npc_plypickupdropwep = "0",
-}
-
 local function handle_exploitable_net(client, name)
     if not IsValid(client) or not client:IsPlayer() then return end
     client:ChatPrint(L("unauthorizedNetMessage", name))
@@ -27,18 +12,10 @@ for _, name in ipairs(exploitable_nets) do
 end
 
 function optimizeVJ()
-    local count = #player.GetAll()
-    RunConsoleCommand("vj_npc_processtime", 1 + count / 40)
+    RunConsoleCommand("vj_npc_processtime", 1 + #player.GetAll() / 40)
 end
 
-timer.Create("vjbase_console_commands", 180, 0, function()
-    for cmd, val in pairs(console_settings) do
-        RunConsoleCommand(cmd, val)
-    end
-
-    optimizeVJ()
-end)
-
+timer.Create("vjbase_console_commands", 180, 0, function() optimizeVJ() end)
 hook.Add("OnEntityCreated", "vjbase_entity_handler", function(ent)
     timer.Simple(0, function()
         if not IsValid(ent) then return end
