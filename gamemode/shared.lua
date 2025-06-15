42f8e9cefc3f28d9cbc3eca9a327a1e1af8eea1f
Samael <33399712+bleonheart@users.noreply.github.com>
2025-06-15 04:43:52 +0100

    Update shared.lua

--- gamemode/shared.lua
+++ gamemode/shared.lua
@@ -1,11 +1,5 @@
 GM.Name = "Lilia"
 GM.version = "0.0.00"
-GM.version = "0.0.00"
-GM.version = "0.0.00"
-GM.version = "0.0.00"
-GM.version = "0.0.00"
-GM.version = "0.0.00"
-GM.version = "0.0.00"
 GM.Author = "Samael"
 GM.Website = "https://discord.gg/esCRH5ckbQ"
 include("core/loader.lua")
