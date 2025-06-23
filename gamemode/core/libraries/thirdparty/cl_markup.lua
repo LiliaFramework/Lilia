834c6c918b3c9945810db25708c600973aff9f85
Samael <33399712+bleonheart@users.noreply.github.com>
2025-06-23 17:23:33 +0100

    Add documentation for lia.markup.parse

--- gamemode/core/libraries/thirdparty/cl_markup.lua
+++ gamemode/core/libraries/thirdparty/cl_markup.lua
@@ -324,6 +324,27 @@ function MarkupObject:draw(xOffset, yOffset, halign, valign, alphaoverride)
     end
 end
 
+--[[
+    lia.markup.parse(text, maxwidth)
+
+    Description:
+        Parses the provided markup text and returns a markup object
+        representing the formatted content. When maxwidth is provided,
+        the text will automatically wrap at that width.
+
+    Parameters:
+        text (string) – String containing markup to be parsed.
+        maxwidth (number|nil) – Optional maximum width for wrapping.
+
+    Realm:
+        Client
+
+    Returns:
+        MarkupObject – The parsed markup object with size information.
+
+    Example Usage:
+        local object = lia.markup.parse("<color=255,0,0>Hello</color>", 200)
+]]
 function parse(ml, maxwidth)
     colour_stack = {
         {
