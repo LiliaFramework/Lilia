7b2d6c6b4e92bf1cb6afb65aa9ae3a9b82a68cd1
Samael <33399712+bleonheart@users.noreply.github.com>
2025-06-29 07:23:56 +0100

    Added Linux Bug Fix

--- gamemode/core/libraries/util.lua
+++ gamemode/core/libraries/util.lua
@@ -173,6 +173,77 @@ function lia.util.findFaction(client, name)
     return nil
 end
 
+if system.IsLinux() then
+    local cache = {}
+    local function GetSoundPath(path, gamedir)
+        if not gamedir then
+            path = "sound/" .. path
+            gamedir = "GAME"
+        end
+        return path, gamedir
+    end
+
+    local function f_IsWAV(f)
+        f:Seek(8)
+        return f:Read(4) == "WAVE"
+    end
+
+    local function f_SampleDepth(f)
+        f:Seek(34)
+        local bytes = {}
+        for i = 1, 2 do
+            bytes[i] = f:ReadByte(1)
+        end
+
+        local num = bit.lshift(bytes[2], 8) + bit.lshift(bytes[1], 0)
+        return num
+    end
+
+    local function f_SampleRate(f)
+        f:Seek(24)
+        local bytes = {}
+        for i = 1, 4 do
+            bytes[i] = f:ReadByte(1)
+        end
+
+        local num = bit.lshift(bytes[4], 24) + bit.lshift(bytes[3], 16) + bit.lshift(bytes[2], 8) + bit.lshift(bytes[1], 0)
+        return num
+    end
+
+    local function f_Channels(f)
+        f:Seek(22)
+        local bytes = {}
+        for i = 1, 2 do
+            bytes[i] = f:ReadByte(1)
+        end
+
+        local num = bit.lshift(bytes[2], 8) + bit.lshift(bytes[1], 0)
+        return num
+    end
+
+    local function f_Duration(f)
+        return (f:Size() - 44) / (f_SampleDepth(f) / 8 * f_SampleRate(f) * f_Channels(f))
+    end
+
+    liaSoundDuration = liaSoundDuration or SoundDuration
+    function SoundDuration(str)
+        local path, gamedir = GetSoundPath(str)
+        local f = file.Open(path, "rb", gamedir)
+        if not f then return 0 end
+        local ret
+        if cache[str] then
+            ret = cache[str]
+        elseif f_IsWAV(f) then
+            ret = f_Duration(f)
+        else
+            ret = liaSoundDuration(str)
+        end
+
+        f:Close()
+        return ret
+    end
+end
+
 if SERVER then
     function lia.util.CreateTableUI(client, title, columns, data, options, characterID)
         if not IsValid(client) or not client:IsPlayer() then return end
