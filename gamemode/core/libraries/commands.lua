ec4e38ea5c946c1b6475184db553acadcdf2eb49
Samael <33399712+bleonheart@users.noreply.github.com>
2025-06-30 19:06:22 +0100

    Update commands.lua

--- gamemode/core/libraries/commands.lua
+++ gamemode/core/libraries/commands.lua
@@ -16,7 +16,6 @@ function lia.command.add(command, data)
             CAMI.RegisterPrivilege({
                 Name = privilegeName,
                 MinAccess = superAdminOnly and "superadmin" or "admin",
-                Description = data.description
             })
         end
     end
@@ -242,7 +241,7 @@ else
         local command = lia.command.list[cmdKey]
         if not command then return end
         local firstKey = istable(fields) and next(fields)
-        if not fields or isstring(fields) or (firstKey and isnumber(firstKey)) then
+        if not fields or isstring(fields) or firstKey and isnumber(firstKey) then
             local args = fields
             if isstring(args) then args = lia.command.extractArgs(args) end
             local parsed, valid = lia.command.parseSyntaxFields(command.syntax)
@@ -259,6 +258,7 @@ else
                 end
             end
         end
+
         local numFields = table.Count(fields)
         local frameW, frameH = 600, 200 + numFields * 75
         local frame = vgui.Create("DFrame")
