65e21735bb5f74a52363751de87c68764d356185
Samael <33399712+bleonheart@users.noreply.github.com>
2025-07-03 18:23:23 +0100

    Update panel.lua

--- gamemode/core/meta/panel.lua
+++ gamemode/core/meta/panel.lua
@@ -9,18 +9,18 @@ function panelMeta:liaListenForInventoryChanges(inventory)
     self.liaHookID[id] = hookID
     self.liaToRemoveHooks = self.liaToRemoveHooks or {}
     self.liaToRemoveHooks[id] = {}
-    local function listenForInventoryChange(name, panelHook)
-        panelHook = panelHook or name
-        hook.Add(name, hookID, function(inventory, ...)
+    local function listenForInventoryChange(eventName, panelHookName)
+        panelHookName = panelHookName or eventName
+        hook.Add(eventName, hookID, function(inv, ...)
             if not IsValid(self) then return end
-            if not isfunction(self[panelHook]) then return end
+            if not isfunction(self[panelHookName]) then return end
             local args = {...}
-            args[#args + 1] = inventory
-            self[panelHook](self, unpack(args))
-            if name == "InventoryDeleted" and self.deleteInventoryHooks then self:deleteInventoryHooks(id) end
+            args[#args + 1] = inv
+            self[panelHookName](self, unpack(args))
+            if eventName == "InventoryDeleted" then self:liaDeleteInventoryHooks(id) end
         end)
 
-        table.insert(self.liaToRemoveHooks[id], name)
+        table.insert(self.liaToRemoveHooks[id], eventName)
     end
 
     listenForInventoryChange("InventoryInitialized")
