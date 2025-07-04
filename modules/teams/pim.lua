4a60966bf657773c6759e78b1f8df0a5abad5c85
Samael <33399712+bleonheart@users.noreply.github.com>
2025-07-04 08:18:20 +0100

    CanInviteToFaction hook

--- modules/teams/pim.lua
+++ modules/teams/pim.lua
@@ -1,19 +1,20 @@
 ﻿AddInteraction("Invite To Faction", {
     runServer = true,
     shouldShow = function(client, target)
-        local clientChar = client:getChar()
-        local targetChar = target:getChar()
-        if not clientChar or not targetChar then return false end
-        return clientChar:hasFlags("Z") and clientChar:getFaction() ~= targetChar:getFaction()
+        local cChar = client:getChar()
+        local tChar = target:getChar()
+        if not cChar or not tChar then return false end
+        if cChar:hasFlags("Z") then return true end
+        return hook.Run("CanInviteToFaction", client, target) ~= false and cChar:getFaction() ~= tChar:getFaction()
     end,
     onRun = function(client, target)
         if not SERVER then return end
-        local inviterChar = client:getChar()
-        local targetChar = target:getChar()
-        if not inviterChar or not targetChar then return end
-        local factionIndex = client:Team()
+        local iChar = client:getChar()
+        local tChar = target:getChar()
+        if not iChar or not tChar then return end
+        local faction
         for _, fac in pairs(lia.faction.teams) do
-            if fac.index == factionIndex then faction = fac end
+            if fac.index == client:Team() then faction = fac end
         end
 
         if not faction then
@@ -27,19 +28,19 @@
                 return
             end
 
-            if hook.Run("CanCharBeTransfered", targetChar, faction, targetChar:getFaction()) == false then return end
-            local oldFaction = targetChar:getFaction()
-            targetChar.vars.faction = faction.uniqueID
-            targetChar:setFaction(factionIndex)
-            targetChar:kickClass()
-            local defaultClass = lia.faction.getDefaultClass(factionIndex)
-            if defaultClass then targetChar:joinClass(defaultClass.index) end
+            if hook.Run("CanCharBeTransfered", tChar, faction, tChar:getFaction()) == false then return end
+            local oldFaction = tChar:getFaction()
+            tChar.vars.faction = faction.uniqueID
+            tChar:setFaction(faction.index)
+            tChar:kickClass()
+            local defClass = lia.faction.getDefaultClass(faction.index)
+            if defClass then tChar:joinClass(defClass.index) end
             hook.Run("OnTransferred", target)
             if faction.OnTransferred then faction:OnTransferred(target, oldFaction) end
             hook.Run("PlayerLoadout", target)
             client:notifyLocalized("transferSuccess", target:Name(), faction.name)
             if client ~= target then target:notifyLocalized("transferNotification", faction.name, client:Name()) end
-            targetChar:takeFlags("Z")
+            tChar:takeFlags("Z")
         end)
     end
 })
