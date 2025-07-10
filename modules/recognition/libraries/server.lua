25be4152eaa12631c0564d48f79ce05f80f69683
Samael <33399712+bleonheart@users.noreply.github.com>
2025-07-10 06:22:43 +0100

    More Recognitions Commands

---
+++ modules/recognition/libraries/server.lua
@@ -0,0 +1,21 @@
+local rangeMap = {
+    whisper = "w",
+    normal = "ic",
+    talk = "ic",
+    yell = "y"
+}
+
+function MODULE:ForceRecognizeRange(ply, range)
+    local char = ply:getChar()
+    if not (char and ply:Alive()) then return end
+    local key = rangeMap[range] or "ic"
+    local cls = lia.chat.classes[key]
+    if not cls then return end
+    for _, v in player.Iterator() do
+        if v ~= ply and v:getChar() and cls.onCanHear(ply, v) and v:getChar():recognize(char) then lia.log.add(ply, "charRecognize", v:getChar():getID(), "FORCED") end
+    end
+
+    net.Start("rgnDone")
+    net.Send(ply)
+    hook.Run("OnCharRecognized", ply)
+end
