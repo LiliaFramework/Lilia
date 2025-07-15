62aaa6bdaffe4595a93f1a577ebe5ccd6c16ca08
Samael <33399712+bleonheart@users.noreply.github.com>
2025-07-15 16:01:30 +0100

    Way too many changes lel

--- gamemode/shared.lua
+++ gamemode/shared.lua
@@ -1,9 +1,5 @@
 GM.Name = "Lilia"
-<<<<<<< Updated upstream
-GM.version = 6.089
-=======
 GM.version = 6.081
->>>>>>> Stashed changes
 GM.Author = "Samael"
 GM.Website = "https://discord.gg/esCRH5ckbQ"
 include("core/libraries/core.lua")
