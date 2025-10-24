6961879fccbf63c60e9b877505dac9a384fdc076
Samael <33399712+bleonheart@users.noreply.github.com>
2025-10-24 19:30:58 +0100

    Dumb Check

--- gamemode/core/derma/panels/buttons.lua
+++ gamemode/core/derma/panels/buttons.lua
@@ -42,7 +42,7 @@ function PANEL:SetIcon(icon, icon_size)
         self.icon = icon
     else
         local mat = Material(icon)
-        if mat and mat:IsValid() then
+        if mat then
             self.icon = mat
         else
             self.icon = nil
