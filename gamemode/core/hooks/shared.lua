f1adcb5d873563991a197a53a0b7f845de933753
Samael <33399712+bleonheart@users.noreply.github.com>
2025-10-27 03:31:44 +0000

    Update shared.lua

--- gamemode/core/hooks/shared.lua
+++ gamemode/core/hooks/shared.lua
@@ -11,13 +11,3 @@ function GM:GetModelGender(model)
     local isFemale = model:find("alyx") or model:find("mossman") or model:find("female")
     return isFemale and "female" or "male"
 end
-
-function GM:OnReloaded()
-    print("Client reload triggered!")
-    lia.reloadInProgress = true
-    lia.module.initialize()
-    lia.config.load()
-    chat.AddText(Color(0, 255, 0), "[Lilia] ", Color(255, 255, 255), L("gamemodeHotreloadedSuccessfully"))
-    lia.faction.formatModelData()
-    lia.reloadInProgress = false
-end
