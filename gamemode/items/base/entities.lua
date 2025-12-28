5db5814d6b91be43ff834301d5d477b147e73c85
Samael <33399712+bleonheart@users.noreply.github.com>
2025-12-28 20:31:25 +0000

    Placing entity items without being in inv

--- gamemode/items/base/entities.lua
+++ gamemode/items/base/entities.lua
@@ -7,13 +7,9 @@ ITEM.functions.Place = {
     name = "placeDownEntity",
     onRun = function(item)
         local client = item.player
-        if not IsValid(item.entity) then
-            client:notifyError("You must place the entity down first!")
-            return false
-        end
-
         local entity = ents.Create(item.entityid)
-        entity:SetPos(item.entity:GetPos())
+        local pos = IsValid(item.entity) and item.entity:GetPos() or client:getItemDropPos()
+        entity:SetPos(pos)
         entity:Spawn()
         item:remove()
         return true
