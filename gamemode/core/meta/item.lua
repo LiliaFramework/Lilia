ce6fecab09f3782e7b6cbba7af94c3b94a911a94
Samael <33399712+bleonheart@users.noreply.github.com>
2026-03-14 03:59:35 +0000

    Lia Item Exception

--- gamemode/core/meta/item.lua
+++ gamemode/core/meta/item.lua
@@ -835,6 +835,7 @@ if SERVER then
             end
 
             local entity = ents.Create("lia_item")
+            if not IsValid(entity) then return nil end
             entity:Spawn()
             entity:SetPos(position)
             entity:SetAngles(angles or angle_zero)
