2b7b9832d4721d866f899c4e6ad7aab0dd309128
Samael <33399712+bleonheart@users.noreply.github.com>
2026-06-13 22:41:47 +0100

    Fixed Bug W/ URL item

--- gamemode/items/base/url.lua
+++ gamemode/items/base/url.lua
@@ -1,13 +1,13 @@
-﻿ITEM.name = "urlName"
+ITEM.name = "urlName"
 ITEM.desc = "urlDesc"
 ITEM.model = "models/props_interiors/pot01a.mdl"
 ITEM.url = ""
 ITEM.functions.use = {
     name = "open",
     icon = "icon16/book_link.png",
-    onRun = function(item)
+    onClick = function(item)
         local url = item.url
-        if CLIENT then gui.OpenURL(url) end
-        return false
+        if isstring(url) and url ~= "" then gui.OpenURL(url) end
     end,
+    onRun = function() return false end,
 }
